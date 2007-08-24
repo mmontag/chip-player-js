795243b3e00ffde5322dee93cbbff19be9383cab
Claudio Matsuoka <cmatsuoka@users.sf.net>
2007-08-24 00:28:10 +0000

    Move archimedes sample conversion to a common file

--- src/loaders/arch_load.c
+++ src/loaders/arch_load.c
@@ -1,7 +1,7 @@
 /* Archimedes Tracker module loader for xmp
  * Copyright (C) 2007 Claudio Matsuoka
  *
- * $Id: arch_load.c,v 1.5 2007-08-23 13:47:40 cmatsuoka Exp $
+ * $Id: arch_load.c,v 1.6 2007-08-24 00:28:10 cmatsuoka Exp $
  *
  * This file is part of the Extended Module Player and is distributed
  * under the terms of the GNU General Public License. See doc/COPYING
@@ -14,32 +14,12 @@
 
 #include "load.h"
 #include "iff.h"
-#include "period.h"
+#include "archimedes.h"
 
 static int year, month, day;
 static int pflag, sflag, max_ins;
 static uint8 ster[8], rows[64];
 static int8 *sbuf[36];
-
-
-static int8 table[128] = {
-	/*   0 */	  0,   0,   0,   0,   0,   0,   0,   0,
-	/*   8 */	  0,   0,   0,   0,   0,   0,   0,   0,
-	/*  16 */	  0,   0,   0,   0,   0,   0,   0,   0,
-	/*  24 */	  1,   1,   1,   1,   1,   1,   1,   1,
-	/*  32 */	  1,   1,   1,   1,   2,   2,   2,   2,
-	/*  40 */	  2,   2,   2,   2,   3,   3,   3,   3,
-	/*  48 */	  3,   3,   4,   4,   4,   4,   5,   5,
-	/*  56 */	  5,   5,   6,   6,   6,   6,   7,   7,
-	/*  64 */	  7,   8,   8,   9,   9,  10,  10,  11,
-	/*  72 */	 11,  12,  12,  13,  13,  14,  14,  15,
-	/*  80 */	 15,  16,  17,  18,  19,  20,  21,  22,
-	/*  88 */	 23,  24,  25,  26,  27,  28,  29,  30,
-	/*  96 */	 31,  33,  34,  36,  38,  40,  42,  44,
-	/* 104 */	 46,  48,  50,  52,  54,  56,  58,  60,
-	/* 112 */	 62,  65,  68,  72,  77,  80,  84,  91,
-	/* 120 */	 95,  98, 103, 109, 114, 120, 126, 127
-};
 
 static void fix_effect(struct xxm_event *e)
 {
@@ -186,31 +166,6 @@ static void get_patt(int size, FILE *f)
 	reportv(0, ".");
 }
 
-/*
- * From the Audio File Formats (version 2.5)
- * Submitted-by: Guido van Rossum <guido@cwi.nl>
- * Last-modified: 27-Aug-1992
- *
- * The Acorn Archimedes uses a variation on U-LAW with the bit order
- * reversed and the sign bit in bit 0.  Being a 'minority' architecture,
- * Arc owners are quite adept at converting sound/image formats from
- * other machines, and it is unlikely that you'll ever encounter sound in
- * one of the Arc's own formats (there are several).
- */
-
-static void convert(int8 *buf, int len)
-{
-	int i;
-	uint8 x;
-
-	for (i = 0; i < len; i++) {
-		x = buf[i];
-		buf[i] = table[x >> 1];
-		if (x & 0x01)
-			buf[i] *= -1;
-	}
-}
-
 static void get_samp(int size, FILE *f)
 {
 	static int i = 0;
@@ -256,7 +211,7 @@ static void get_samp(int size, FILE *f)
 
 	sbuf[i] = malloc(xxs[i].len);
 	fread(sbuf[i], 1, xxs[i].len, f);
-	convert(sbuf[i], xxs[i].len);
+	arc2linear(sbuf[i], xxs[i].len);
 
 	xmp_drv_loadpatch(NULL, xxi[i][0].sid, xmp_ctl->c4rate,XMP_SMP_NOLOAD,
 					&xxs[xxi[i][0].sid], (char *)sbuf[i]);
