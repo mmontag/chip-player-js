01a022e2237e9c6aa5f4321b2b297f0f3012652d
Claudio Matsuoka <cmatsuoka@users.sf.net>
2007-09-01 01:23:28 +0000

    MED2 loader WIP

--- src/loaders/med2_load.c
+++ src/loaders/med2_load.c
@@ -5,7 +5,7 @@
  * under the terms of the GNU General Public License. See doc/COPYING
  * for more information.
  *
- * $Id: med2_load.c,v 1.3 2007-08-31 22:22:10 cmatsuoka Exp $
+ * $Id: med2_load.c,v 1.4 2007-09-01 01:23:28 cmatsuoka Exp $
  */
 
 /*
@@ -31,7 +31,7 @@ int med2_load(FILE * f)
 {
 	int i, j;
 	struct xxm_event *event;
-	uint32 ver, flags, mask, m;
+	uint32 ver, mask;
 
 	LOAD_INIT();
 
@@ -58,77 +58,71 @@ int med2_load(FILE * f)
 
 	/* read instrument volumes */
 	mask = read32b(f);
-	for (i = 0, m = mask; i < 32; i++, m <<= 1) {
-		xxi[i][0].vol = m & MASK ? read8(f) : 0;
+	for (i = 0; i < 32; i++, mask <<= 1) {
+		xxi[i][0].vol = mask & MASK ? read8(f) : 0;
 		xxi[i][0].pan = 0x80;
 		xxi[i][0].fin = 0;
 		xxi[i][0].sid = i;
 	}
 
-	fseek(f, 8, SEEK_CUR);	/* skip something */
-
-	xxh->pat = read16b(f);
-	xxh->len = read16b(f);
-	xxh->chn = 4;
-	xxh->trk = xxh->chn * xxh->pat;
-
-	fread(xxo, 1, xxh->len, f);
-
 	/* read instrument loops */
-	for (i = 0; i < 32; i++) {
-		xxs[i].lps = xxih[i].name[0] ? read16l(f) : 0;
+	mask = read32b(f);
+	for (i = 0; i < 32; i++, mask <<= 1) {
+		xxs[i].lps = mask & MASK ? read16b(f) : 0;
 	}
 
 	/* read instrument loop length */
-	for (i = 0; i < 32; i++) {
-		uint32 lsiz = xxih[i].name[0] ? read16l(f) : 0;
+	mask = read32b(f);
+	for (i = 0; i < 32; i++, mask <<= 1) {
+		uint32 lsiz = mask & MASK ? read16b(f) : 0;
 		xxs[i].len = xxs[i].lps + lsiz;
 		xxs[i].lpe = xxs[i].lps + lsiz;
 		xxs[i].flg = lsiz > 1 ? WAVE_LOOPING : 0;
 		xxih[i].nsm = !!(xxs[i].len);
 	}
-	
-#if 0
+
+	xxh->chn = 4;
 	xxh->pat = read16b(f);
-	fread(xxo, 1, 100, f);
 	xxh->len = read16b(f);
+	fread(xxo, 1, xxh->len, f);
 	xxh->tpo = read16b(f);
+
 	xxh->trk = xxh->chn * xxh->pat;
 
 	if (ver == MAGIC_MED2) {
 		/* MED2 header */
-		strcpy(xmp_ctl->type, "MED2");
-		strcpy(tracker_name, "MED 1.11/1.12");
-		flags = read16b(f);
-		read16b(f);		/* sliding (5 or 6) */
-		read32b(f);		/* jump mask */
-		fseek(f, 16, SEEK_CUR);	/* skip rgb */
+		strcpy(xmp_ctl->type, "MED2 (MED 1.11)");
 	} else {
 		/* MED3 header */
-		strcpy(xmp_ctl->type, "MED3");
-		strcpy(tracker_name, "MED 2.00");
-		read8(f);	/* playtranspose */
-		flags = read8(f);
-		read16b(f);		/* sliding (5 or 6) */
-		read32b(f);		/* jump mask */
-		fseek(f, 16, SEEK_CUR);	/* skip rgb */
-		fseek(f, 32, SEEK_CUR);	/* skip MIDI channels */
-		fseek(f, 32, SEEK_CUR);	/* skip MIDI presets */
-	}
-#endif
+		strcpy(xmp_ctl->type, "MED3 (MED 2.00)");
 
+		/* read midi channels */
+		mask = read32b(f);
+		for (i = 0; i < 32; i++, mask <<= 1) {
+			if (mask & MASK)
+				read8(f);
+		}
+	
+		/* read midi programs */
+		mask = read32b(f);
+		for (i = 0; i < 32; i++, mask <<= 1) {
+			if (mask & MASK)
+				read8(f);
+		}
+	}
+	
 	MODULE_INFO();
 
 	reportv(0, "Instruments    : %d\n", xxh->ins);
-	reportv(1, "     Instrument name                  Len  LBeg LEnd L Vol Fin\n");
+	reportv(1, "     Instrument name                  Len  LBeg LEnd L Vol\n");
 
 	for (i = 0; i < xxh->ins; i++) {
 		if ((V(1)) && (strlen((char *)xxih[i].name) || xxs[i].len > 2))
-			report("[%2X] %-32.32s %04x %04x %04x %c V%02x %+d\n",
+			report("[%2X] %-32.32s %04x %04x %04x %c V%02x\n",
 			       i, xxih[i].name, xxs[i].len, xxs[i].lps,
 			       xxs[i].lpe,
 			       xxs[i].flg & WAVE_LOOPING ? 'L' : ' ',
-			       xxi[i][0].vol, (char)xxi[i][0].fin >> 4);
+			       xxi[i][0].vol);
 	}
 
 	PATTERN_INIT();
@@ -136,11 +130,29 @@ int med2_load(FILE * f)
 	/* Load and convert patterns */
 	reportv(0, "Stored patterns: %d ", xxh->pat);
 
-#if 0
 	for (i = 0; i < xxh->pat; i++) {
+		uint8 b;
+		uint16 sz;
+		uint32 x;
+
 		PATTERN_ALLOC(i);
 		xxp[i]->rows = 64;
 		TRACK_ALLOC(i);
+
+		read8(f);
+
+		b = read8(f);
+		sz = read16b(f);
+
+		if (b & 0x10) {
+			x = 0;
+		} else if (b & 0x01) {
+			x = 0xffffffff;
+		} else {
+			x = read32b(f);
+		}
+	
+#if 0
 		for (j = 0; j < (64 * 4); j++) {
 			event = &EVENT(i, j % 4, j / 4);
 			fread(mod_event, 1, 4, f);
@@ -153,12 +165,10 @@ int med2_load(FILE * f)
 				cvt_pt_event(event, mod_event);
 			}
 		}
+#endif
 
-		if (V(0))
-			report(".");
+		reportv(0, ".");
 	}
-
-	xxh->flg |= XXM_FLG_MODRNG;
 
 	/* Load samples */
 
@@ -169,12 +179,9 @@ int med2_load(FILE * f)
 			continue;
 		xmp_drv_loadpatch(f, xxi[i][0].sid, xmp_ctl->c4rate, 0,
 				  &xxs[xxi[i][0].sid], NULL);
-		if (V(0))
-			report(".");
+		reportv(0, ".");
 	}
-	if (V(0))
-		report("\n");
-#endif
+	reportv(0, "\n");
 
 	return 0;
 }
