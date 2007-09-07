f7525ce8883aae2e1fa8a482661a89c27b93f155
Claudio Matsuoka <cmatsuoka@users.sf.net>
2007-09-07 21:11:48 +0000

    Vastly improved MED4 loader

--- src/loaders/med4_load.c
+++ src/loaders/med4_load.c
@@ -5,7 +5,7 @@
  * under the terms of the GNU General Public License. See doc/COPYING
  * for more information.
  *
- * $Id: med4_load.c,v 1.5 2007-09-07 12:10:26 cmatsuoka Exp $
+ * $Id: med4_load.c,v 1.6 2007-09-07 21:11:48 cmatsuoka Exp $
  */
 
 /*
@@ -25,20 +25,65 @@
 #define MAGIC_MEDV	MAGIC4('M','E','D','V')
 
 
+static int read4_ctl;
+
+static void fix_effect(struct xxm_event *event)
+{
+	switch (event->fxt) {
+	case 0x00:	/* arpeggio */
+	case 0x01:	/* slide up */
+	case 0x02:	/* slide down */
+		break;
+	case 0x03:	/* vibrato */
+		event->fxt = FX_VIBRATO;
+		break;
+	case 0x0c:	/* set volume (BCD) */
+		event->fxp = MSN(event->fxp) * 10 +
+					LSN(event->fxp);
+		break;
+	case 0x0d:	/* volume slides */
+		event->fxt = FX_VOLSLIDE;
+		break;
+	case 0x0f:	/* tempo/break */
+		if (event->fxp == 0)
+			event->fxt = FX_BREAK;
+		if (event->fxp == 0xff) {
+			event->fxp = event->fxt = 0;
+			event->vol = 1;
+		} else if (event->fxp == 0xfe) {
+			event->fxp = event->fxt = 0;
+		} else if (event->fxp == 0xf1) {
+			event->fxt = FX_EXTENDED;
+			event->fxp = (EX_RETRIG << 4) | 3;
+		} else if (event->fxp == 0xf2) {
+			event->fxt = FX_EXTENDED;
+			event->fxp = (EX_CUT << 4) | 3;
+		} else if (event->fxp == 0xf3) {
+			event->fxt = FX_EXTENDED;
+			event->fxp = (EX_DELAY << 4) | 3;
+		} else if (event->fxp > 10) {
+			event->fxt = FX_S3M_BPM;
+			event->fxp = event->fxp * 4 - 7;
+		}
+		break;
+	default:
+		event->fxp = event->fxt = 0;
+	}
+}
+
 static inline uint8 read4(FILE *f)
 {
 	static uint8 b = 0;
-	static int ctl = 0;
 	uint8 ret;
 
-	if (ctl & 0x01) {
+	if (read4_ctl & 0x01) {
 		ret = b & 0x0f;
 	} else {
 		b = read8(f);
 		ret = b >> 4;
 	}
 
-	ctl ^= 0x01;
+	read4_ctl ^= 0x01;
 
 	return ret;
 }
@@ -102,21 +147,19 @@ int med4_load(FILE * f)
 			buf[j] = read8(f);
 		buf[j] = 0;
 
-		if (c == 0x4f)
-			read8(f);
+		xxi[i][0].vol = c & 0x20 ? 0x40 : read8(f);
+
 		if (c == 0x4c) {
 			read32b(f);
-			read8(f);
 		}
 		if (c == 0x4d) {
 			read32b(f);
-			read8(f);
 		}
 		if (c == 0x6c)
 			read32b(f);
 
 		copy_adjust(xxih[i].name, buf, 32);
-//printf("%d = %s\n", i, xxih[i].name);
+		//printf("%d = %s\n", i, xxih[i].name);
 	}
 
 	xxh->chn = 4;
@@ -157,37 +200,34 @@ int med4_load(FILE * f)
 		xxp[i]->rows = 64;
 		TRACK_ALLOC(i);
 
+		//printf("\n===== PATTERN %d =====\n", i);
+		//printf("offset = %lx\n", ftell(f));
+
 		size = read8(f);	/* pattern control block */
-		read16b(f);		/* 0x3f 0x04 */
+		x = read16b(f);		/* 0x043f */
 		plen = read16b(f);
 		ctl = read8(f);
 
-		printf("size = %02x\n", size);
-		printf("pen  = %04x\n", plen);
+		linemsk0 = ctl & 0x80 ? ~0 : ctl & 0x40 ? 0 : read32b(f);
+		fxmsk0   = ctl & 0x20 ? ~0 : ctl & 0x10 ? 0 : read32b(f);
+		linemsk1 = ctl & 0x08 ? ~0 : ctl & 0x04 ? 0 : read32b(f);
+		fxmsk1   = ctl & 0x02 ? ~0 : ctl & 0x01 ? 0 : read32b(f);
+
+		/*printf("size = %02x\n", size);
+		printf("x    = %02x\n", x);
+		printf("plen = %04x\n", plen);
 		printf("ctl  = %02x\n", ctl);
-
-		linemsk0 = fxmsk0 = ctl & 0x80 ? 0xffffffff : 0x00000000;
-		linemsk1 = fxmsk1 = ctl & 0x80 ? 0xffffffff : 0x00000000;
-
-		if (~ctl & 0x80) {
-			if (~ctl & 0x40)
-				linemsk0 = read32b(f);
-			if (~ctl & 0x10)
-				fxmsk0   = read32b(f);
-		}
-		if (~ctl & 0x08) {
-			if (~ctl & 0x04)
-				linemsk1 = read32b(f);
-			if (~ctl & 0x01)
-				fxmsk1   = read32b(f);
-		}
-
 		printf("linemsk0 = %08x\n", linemsk0);
 		printf("fxmsk0   = %08x\n", fxmsk0);
 		printf("linemsk1 = %08x\n", linemsk1);
-		printf("fxmsk1   = %08x\n", fxmsk1);
+		printf("fxmsk1   = %08x\n", fxmsk1);*/
 
-		read8(f);		/* 0xff */
+		assert(x == 0x043f);
+
+		x = read8(f);		/* 0xff */
+		//printf("blk end  = %02x\n\n", x);
+
+		read4_ctl = 0;
 
 		for (j = 0; j < 32; j++, linemsk0 <<= 1, fxmsk0 <<= 1) {
 			if (linemsk0 & 0x80000000) {
@@ -214,11 +254,12 @@ int med4_load(FILE * f)
 						x = read12b(f);
 						event->fxt = x >> 8;
 						event->fxp = x & 0xff;
+						fix_effect(event);
 					}
 				}
 			}
 
-			printf("%03d ", j);
+			/*printf("%03d ", j);
 			for (k = 0; k < 4; k++) {
 				event = &EVENT(i, k, j);
 				if (event->note)
@@ -228,7 +269,7 @@ int med4_load(FILE * f)
 				printf(" %1x%1x%02x ",
 					event->ins, event->fxt, event->fxp);
 			}
-			printf("\n");
+			printf("\n");*/
 		}
 
 		for (j = 32; j < 64; j++, linemsk1 <<= 1, fxmsk1 <<= 1) {
@@ -254,9 +295,22 @@ int med4_load(FILE * f)
 						x = read12b(f);
 						event->fxt = x >> 8;
 						event->fxp = x & 0xff;
+						fix_effect(event);
 					}
 				}
 			}
+
+			/*printf("%03d ", j);
+			for (k = 0; k < 4; k++) {
+				event = &EVENT(i, k, j);
+				if (event->note)
+					printf("%03d", event->note);
+				else
+					printf("---");
+				printf(" %1x%1x%02x ",
+					event->ins, event->fxt, event->fxp);
+			}
+			printf("\n");*/
 		}
 
 		reportv(0, ".");
@@ -269,14 +323,21 @@ int med4_load(FILE * f)
 	reportv(1, "\n     Instrument name                  Len  LBeg LEnd L Vol");
 
 	mask = read32b(f);
+
+	read16b(f);
+
+	//printf("instrument mask: %08x\n", mask);
+
+	mask <<= 1;	/* no instrument #0 */
 	for (i = 0; i < 32; i++, mask <<= 1) {
 		if (~mask & 0x80000000)
 			continue;
 
-		xxs[i].len = read32b(f);
-		if (read16b(f))		/* type */
-			continue;
+		read16b(f);
+		read16b(f);
+		xxs[i].len = read16b(f);
 
+		xxi[i][0].sid = i;
 		xxih[i].nsm = !!(xxs[i].len);
 
 		reportv(1, "\n[%2X] %-32.32s %04x %04x %04x %c V%02x ",
