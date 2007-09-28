fa5eca39f5ca27d36182452081726773758557cd
Claudio Matsuoka <cmatsuoka@users.sf.net>
2007-09-28 12:55:33 +0000

    DBM loader WIP

--- src/loaders/dbm_load.c
+++ src/loaders/dbm_load.c
@@ -1,7 +1,7 @@
 /* DigiBoosterPRO module loader for xmp
  * Copyright (C) 1999-2007 Claudio Matsuoka
  *
- * $Id: dbm_load.c,v 1.2 2007-09-28 01:25:01 cmatsuoka Exp $
+ * $Id: dbm_load.c,v 1.3 2007-09-28 12:55:33 cmatsuoka Exp $
  *
  * This file is part of the Extended Module Player and is distributed
  * under the terms of the GNU General Public License. See doc/COPYING
@@ -21,37 +21,43 @@
 
 #define MAGIC_DBM0	MAGIC4('D','B','M','0')
 
+static int have_song;
 
-static void get_info(int size, uint8 *buffer)
+static void get_info(int size, FILE *f)
 {
-	xxh->ins = readmem16b(buffer);
-	xxh->smp = readmem16b(buffer + 2);
-	xxh->pat = readmem16b(buffer + 6);
-	xxh->chn = readmem16b(buffer + 8);
+	xxh->ins = read16b(f);
+	xxh->smp = read16b(f);
+	xxh->pat = read16b(f);
+	xxh->chn = read16b(f);
 
 	xxh->trk = xxh->pat * xxh->chn;
 
 	INSTRUMENT_INIT();
 }
 
-static void get_song(int size, uint8 *buffer)
+static void get_song(int size, FILE *f)
 {
+	char buffer[50];
+
+	if (have_song)
+		return;
+
+	have_song = 1;
+
+	fread(buffer, 44, 1, f);
 	if (V(0) && *buffer)
-		report("Song name      : %-44.44s\n", buffer);
+		report("Song name      : %s\n", buffer);
 
-	buffer += 44;
-
-	xxh->len = readmem16b(buffer);
-
+	xxh->len = read16b(f);
 	reportv(0, "Song length    : %d patterns\n", xxh->len);
-
-	memcpy(xxo, buffer + 2, xxh->len);
+	fread(xxo, xxh->len, 1, f);
 }
 
-static void get_inst(int size, uint8 *buffer)
+static void get_inst(int size, FILE *f)
 {
 	int i;
 	int c2spd;
+	uint8 buffer[50];
 
 	reportv(0, "Instruments    : %d", xxh->ins);
 
@@ -59,14 +65,15 @@ static void get_inst(int size, uint8 *buffer)
 		xxi[i] = calloc(sizeof(struct xxm_instrument), 1);
 
 		xxih[i].nsm = 1;
+		fread(buffer, 30, 1, f);
 		copy_adjust(xxih[i].name, buffer, 30);
-		xxi[i][0].sid = readmem16b(buffer + 30);
-		xxi[i][0].vol = readmem16b(buffer + 32);
-		c2spd = readmem32b(buffer + 34);
-		xxs[i].lps = readmem32b(buffer + 38);
-		xxs[i].lpe = xxs[i].lps + readmem32b(buffer + 42);
+		xxi[i][0].sid = read16b(f);
+		xxi[i][0].vol = read16b(f);
+		c2spd = read32b(f);
+		xxs[i].lps = read32b(f);
+		xxs[i].lpe = xxs[i].lps + read32b(f);
 		/*xxs[i].flg = inst[i].looplen > 2 ? WAVE_LOOPING : 0; */
-		xxi[i][0].pan = readmem16b(buffer + 46);
+		xxi[i][0].pan = read16b(f);
 
 		if (V(1) && (*xxih[i].name || (xxs[i].len > 1))) {
 			report("\n[%2X] %-30.30s %05x %05x %c %02x %02x", i,
@@ -74,63 +81,60 @@ static void get_inst(int size, uint8 *buffer)
 				xxs[i].flg & WAVE_LOOPING ? 'L' : ' ',
 				xxi[i][0].vol, xxi[i][0].pan);
 		}
-
-		buffer += 48;
 	}
 }
 
-static void get_patt(int size, uint8 *buffer)
+static void get_patt(int size, FILE *f)
 {
 	int i, c, r, n, rows, sz;
 	struct xxm_event *event, dummy;
-	uint8 *p = buffer;
+	uint8 x;
 
 	PATTERN_INIT();
 
 	reportv(0, "\nStored patterns: %d ", xxh->pat);
 
 	for (i = 0; i < xxh->pat; i++) {
-		rows = readmem16b(p);
-		p += 2;
+		rows = read16b(f);
 
 		PATTERN_ALLOC(i);
 		xxp[i]->rows = rows;
 		TRACK_ALLOC(i);
 
-		sz = readmem32b(p);
-		p += 4;
+		sz = read32b(f);
 
 		r = 0;
 		c = -1;
 
 		while (sz >= 0) {
-			n = *p++;
+			n = read8(f);
 			sz--;
 			if (n == 0) {
 				r++;
 				c = -1;
 				continue;
 			}
-			if (c >= *p)
+			x = read8(f);
+			if (c >= x)
 				r++;
-			c = *p++;
+			c = read8(f);
 			sz--;
 			event = c >= xxh->chn ? &dummy : &EVENT(i, c, r);
 			if (n & 0x01) {
-				event->note = MSN(*p) * 12 + LSN(*p);
-				p++;
+				x = read8(f);
+				event->note = MSN(x) * 12 + LSN(x);
 				sz--;
 			}
 			if (n & 0x02)
-				sz--, event->ins = *p++ + 1;
+				sz--, event->ins = read8(f) + 1;
 			if (n & 0x04)
-				sz--, event->fxt = *p++;
+				sz--, event->fxt = read8(f);
 			if (n & 0x08)
-				sz--, event->fxp = *p++;
+				sz--, event->fxp = read8(f);
 			if (n & 0x10)
-				sz--, event->f2t = *p++;
+				sz--, event->f2t = read8(f);
 			if (n & 0x20)
-				sz--, event->f2p = *p++;
+				sz--, event->f2p = read8(f);
 		}
 		reportv(0, ".");
 	}
@@ -171,6 +175,7 @@ int dbm_load(FILE *f)
 	if (read32b(f) != MAGIC_DBM0)
 		return -1;
 
+	have_song = 0;
 	version = read16b(f);
 
 	fseek(f, 10, SEEK_CUR);
@@ -187,7 +192,7 @@ int dbm_load(FILE *f)
 #endif
 
 	strncpy(xmp_ctl->name, name, XMP_DEF_NAMESIZE);
-	strcpy(xmp_ctl->type, "DBM0");
+	strcpy(xmp_ctl->type, "DBM0 (DigiBooster Pro)");
 	sprintf(tracker_name, "DigiBooster Pro %d.%0x",
 		version >> 8, version & 0xff);
 
