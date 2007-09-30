f4752247a94568f4dae18d4662d4dfbf87a5e37d
Claudio Matsuoka <cmatsuoka@users.sf.net>
2007-09-30 16:27:14 +0000

    Don't try to load OXM as XM

--- src/misc/load.c
+++ src/misc/load.c
@@ -1,7 +1,7 @@
 /* Extended Module Player
  * Copyright (C) 1996-2007 Claudio Matsuoka and Hipolito Carraro Jr
  *
- * $Id: load.c,v 1.24 2007-09-26 19:27:24 cmatsuoka Exp $
+ * $Id: load.c,v 1.25 2007-09-30 16:27:14 cmatsuoka Exp $
  *
  * This file is part of the Extended Module Player and is distributed
  * under the terms of the GNU General Public License. See doc/COPYING
@@ -35,7 +35,9 @@ int decrunch_sqsh (FILE *, FILE *);
 int decrunch_pp (FILE *, FILE *);
 int decrunch_mmcmp (FILE *, FILE *);
 int decrunch_umx (FILE *, FILE *);
+int decrunch_oxm (FILE *, FILE *);
 int decrunch_pw (FILE *, FILE *);
+int test_oxm(FILE *);
 int pw_check(unsigned char *, int);
 
 #define BUILTIN_PP	0x01
@@ -46,6 +48,7 @@ int pw_check(unsigned char *, int);
 #define BUILTIN_ARCFS	0x06
 #define BUILTIN_S404	0x07
 #define BUILTIN_UMX	0x08
+#define BUILTIN_OXM	0x09
 
 
 static int decrunch(FILE **f, char **s)
@@ -58,11 +61,11 @@ static int decrunch(FILE **f, char **s)
 
     packer = cmd = NULL;
     builtin = res = 0;
-    temp = strdup ("/tmp/xmp_XXXXXX");
+    temp = strdup("/tmp/xmp_XXXXXX");
 
-    fseek (*f, 0, SEEK_SET);
+    fseek(*f, 0, SEEK_SET);
     b = calloc(1, PW_TEST_CHUNK);
-    fread (b, 1, PW_TEST_CHUNK, *f);
+    fread(b, 1, PW_TEST_CHUNK, *f);
 
     if (b[0] == 'P' && b[1] == 'K') {
 	packer = "Zip";
@@ -122,6 +125,9 @@ static int decrunch(FILE **f, char **s)
     } else if (b[0] == 0xc1 && b[1] == 0x83 && b[2] == 0x2a && b[3] == 0x9e) {
 	packer = "UMX";
 	builtin = BUILTIN_UMX;
+    } else if (test_oxm(*f) == 0) {
+	packer = "oggmod";
+	builtin = BUILTIN_OXM;
     } else {
 	int extra;
 	int s = PW_TEST_CHUNK;
@@ -226,6 +232,9 @@ static int decrunch(FILE **f, char **s)
 	    break;
 	case BUILTIN_UMX:
 	    res = decrunch_umx(*f, t);
+	    break;
+	case BUILTIN_OXM:
+	    res = decrunch_oxm(*f, t);
 	    break;
 	case BUILTIN_PW:
 	    res = decrunch_pw(*f, t);
