f568e3ad537fa7fe3572f20ad2eaf5972ad26520
Claudio Matsuoka <cmatsuoka@users.sf.net>
2007-10-03 22:39:44 +0000

    Updated Douglas Carmichael's email address

--- src/main/main.c
+++ src/main/main.c
@@ -1,7 +1,7 @@
 /* Extended Module Player
  * Copyright (C) 1996-2007 Claudio Matsuoka and Hipolito Carraro Jr
  *
- * $Id: main.c,v 1.12 2007-10-03 21:12:16 cmatsuoka Exp $
+ * $Id: main.c,v 1.13 2007-10-03 22:39:44 cmatsuoka Exp $
  *
  * This file is part of the Extended Module Player and is distributed
  * under the terms of the GNU General Public License. See doc/COPYING
@@ -17,7 +17,7 @@
  */
 
 /*
- * Wed, 10 Jun 1998 15:42:25 -0500  Douglas Carmichael <dcarmich@mcs.com>
+ * Wed, 10 Jun 1998 15:42:25 -0500 Douglas Carmichael <dcarmich@dcarmichael.net>
  * Support for real-time priority in FreeBSD.
  */
 
