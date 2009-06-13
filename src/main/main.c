a628ef786df6602232605bd38ffbea84f9e9c0cb
Claudio Matsuoka <cmatsuoka@gmail.com>
2009-06-13 19:09:13 -0300

    Remove CVS Id
    Signed-off-by: Claudio Matsuoka <cmatsuoka@gmail.com>

--- src/main/main.c
+++ src/main/main.c
@@ -1,7 +1,5 @@
 /* Extended Module Player
- * Copyright (C) 1996-2008 Claudio Matsuoka and Hipolito Carraro Jr
- *
- * $Id: main.c,v 1.40 2008-10-20 02:29:16 cmatsuoka Exp $
+ * Copyright (C) 1996-2009 Claudio Matsuoka and Hipolito Carraro Jr
  *
  * This file is part of the Extended Module Player and is distributed
  * under the terms of the GNU General Public License. See doc/COPYING
