d5d4ecab64030c23c4200caf30171228b3a79ed8
Claudio Matsuoka <cmatsuoka@gmail.com>
2009-06-20 19:39:23 -0300

    Fix Amiga driver copyright notice
    Signed-off-by: Claudio Matsuoka <cmatsuoka@gmail.com>

--- src/drivers/amiga.c
+++ src/drivers/amiga.c
@@ -1,6 +1,5 @@
 /* Amiga AHI driver for Extended Module Player
  * Copyright (C) 2007 Lorence Lombardo
- * Copyright (C) 1996-2007 Claudio Matsuoka and Hipolito Carraro Jr
  *
  * This file is part of the Extended Module Player and is distributed
  * under the terms of the GNU General Public License. See doc/COPYING
