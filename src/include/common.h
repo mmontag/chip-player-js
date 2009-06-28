017087c783f9e8da095b271e49a8207f01a26322
Claudio Matsuoka <cmatsuoka@gmail.com>
2009-06-28 19:05:16 -0300

    Set default amplification back to 0
    Detected some distortion when playing some Jeronen Tel modules with
    lots of channels, more investigation is needed.
    
    Signed-off-by: Claudio Matsuoka <cmatsuoka@gmail.com>

--- src/include/common.h
+++ src/include/common.h
@@ -100,7 +100,7 @@ typedef signed long long int64;
 /* [Amiga] PAL color carrier frequency (PCCF) = 4.43361825 MHz */
 /* [Amiga] CPU clock = 1.6 * PCCF = 7.0937892 MHz */
 
-#define DEFAULT_AMPLIFY	1
+#define DEFAULT_AMPLIFY	0
 
 /* Global flags */
 #define PATTERN_BREAK	0x0001 
