2a593ae6c32892b9a078b46504b9a6b202f0401c
Claudio Matsuoka <cmatsuoka@gmail.com>
2009-07-30 16:48:34 -0300

    Avoid crash if a module has a tempo of zero
    from	Andrew Church <achurch@achurch.org>
    to	cmatsuoka@gmail.com
    date	Thu, Jul 30, 2009 at 8:58 PM
    
    Avoids a crash if a module specifies a tempo of zero. (I'm afraid I
    can't remember what module caused this; it may well have been a
    corrupt file.)
    
    Signed-off-by: Claudio Matsuoka <cmatsuoka@gmail.com>

--- src/player/player.c
+++ src/player/player.c
@@ -904,7 +904,8 @@ int _xmp_player_frame(struct xmp_context *ctx)
 			f->end_point = p->xmp_scan_num;
 
 		f->ord = p->pos;
-		p->tempo = m->xxo_info[f->ord].tempo;
+		if (m->xxo_info[f->ord].tempo)
+			p->tempo = m->xxo_info[f->ord].tempo;
 		p->xmp_bpm = m->xxo_info[f->ord].bpm;
 		p->tick_time = m->rrate / p->xmp_bpm;
 		m->volume = m->xxo_info[f->ord].gvl;
