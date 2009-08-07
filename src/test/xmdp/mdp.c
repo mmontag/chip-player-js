dd3bcd64b14f66a56e9b1b58f7e73c1f8cdff94a
Claudio Matsuoka <cmatsuoka@gmail.com>
2009-08-07 14:58:46 -0300

    Update xmdp to be single-threaded and use SDL
    Signed-off-by: Claudio Matsuoka <cmatsuoka@gmail.com>

--- src/test/xmdp/mdp.c
+++ src/test/xmdp/mdp.c
@@ -1,7 +1,7 @@
 /* xmdp Copyright (C) 1997-2009 Claudio Matsuoka and Hipolito Carraro Jr
  * Original MDP.EXE for DOS by Future Crew
  *
- * 1.2.0: Ported to xmp 3.0
+ * 1.2.0: Ported to xmp 3.0 and sdl
  * 1.1.0: Raw keys handled by Russel Marks' rawkey functions
  *        Links to libxmp 1.1.0, shm stuff removed
  * 1.0.2: execlp() bug fixed by Takashi Iwai
@@ -16,27 +16,10 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
-#include <signal.h>
-#include <sys/ipc.h>
-#include <sys/shm.h>
-#include <sys/types.h>
-#include <sys/wait.h>
-
-#define USE_X11
-
-#ifdef USE_X11
-#   undef RAW_KEYS
-#   include <X11/Xlib.h>
-#   include <X11/Xutil.h>
-#   include <X11/extensions/XShm.h>
-#else /* SVGAlib */
-#   include <vga.h>
-#endif
-
-#include "xmp.h"
+#include <SDL/SDL.h>
 #include "mdp.h"
+#include "xmp.h"
 #include "font.h"
-
 
 #define rightmsg(f,x,y,s,c,b)						\
     writemsg (f, (x) - writemsg (f,0,0,s,-1,0), y, s, c, b)
@@ -54,30 +37,11 @@ extern char *mdp_font2[];
 extern struct font_header font1;
 extern struct font_header font2;
 
-static struct channel_info *ci;
-static int shmid;
-static pid_t pid;
+static SDL_Surface *screen;
+static struct channel_info ci[40];
 static struct xmp_module_info mi;
 
-#ifdef USE_X11
-static Display *display;
-static Visual *visual;
-static int screen;
-static Screen *scrptr;
-static Colormap colormap;
-static XSetWindowAttributes attributes;
-static unsigned long attribute_mask;
-static int depth;
-static GC gc;
-static XImage *ximage;
-static Window window, root;
-static XShmSegmentInfo shminfo;
-static XColor color[16];
-static unsigned long __color;
-#endif
-
-static int palette[] =
-{
+static int palette[] = {
     0x00, 0x00, 0x00,	/*  0 */	0x3f, 0x3f, 0x25,	/*  1 */
     0x3f, 0x3b, 0x12,	/*  2 */	0x3f, 0x34, 0x00,	/*  3 */
     0x3f, 0x29, 0x00,	/*  4 */	0x3f, 0x1f, 0x00,	/*  5 */
@@ -88,67 +52,56 @@ static int palette[] =
     0x10, 0x18, 0x20,	/* 14 */	0xff, 0xff, 0xff	/* 15 */
 };
 
+static SDL_Color color[16];
+static Uint32 mapped_color[16];
+static int __color;
 static xmp_context ctx;
 
-void signal_handler ()
+static inline void setcolor(int c)
 {
-#ifdef RAW_KEYS
-    printf("Switching to normal keyboard mode\n");
-    rawmode_exit();
-#endif
-#ifdef USE_X11
-    XShmDetach(display,&shminfo);
-    XDestroyImage(ximage);
-    shmctl(shminfo.shmid,IPC_RMID,NULL);
-    XCloseDisplay(display);
-    shmdt(shminfo.shmaddr);
-#else
-    vga_setmode(TEXT);
-#endif
-
-    kill (pid, SIGTERM);
-    waitpid (pid, NULL, 0);
-
-    shmctl (shmid, IPC_RMID, NULL);
-    shmdt ((char *)ci);
-
-    exit(0);
+	__color = c;
 }
 
-
-#ifdef USE_X11
-
-/* This function is from Russel Marks' ZX-81 emulator */
-void notify (int *argc, char **argv)
+static inline void put_pixel(int x, int y, int c)
 {
-    XWMHints hints;
-    XSizeHints sizehints;
-    XClassHint classhint;
-    XTextProperty appname, iconname;
-    char *apptext = "xmdp";
-    char *icontext = "xmdp";
+	Uint32 pixel;
+	Uint8 *bits, bpp;
 
-    XStringListToTextProperty (&apptext, 1, &appname);
-    XStringListToTextProperty (&icontext, 1, &iconname);
-    sizehints.flags = PSize | PMinSize | PMaxSize;
-    sizehints.min_width = sizehints.max_width = 640;
-    sizehints.min_height = sizehints.max_height = 480;
-    hints.flags = StateHint | InputHint;
-    hints.initial_state = NormalState;
-    hints.input = 1;
-    classhint.res_name = "xmpd";
-    classhint.res_class = "Xmdp";
-    XSetWMProperties (display, window, &appname, &iconname, argv,
-	*argc, &sizehints, &hints, &classhint);
+	pixel = mapped_color[c];
+
+	bpp = screen->format->BytesPerPixel;
+	bits = ((Uint8 *) screen->pixels) + y * screen->pitch + x * bpp;
+
+	/* Set the pixel */
+	switch (bpp) {
+	case 1:
+		*(Uint8 *)(bits) = pixel;
+		break;
+	case 2:
+		*((Uint16 *) (bits)) = (Uint16) pixel;
+		break;
+	case 3:{
+		Uint8 r, g, b;
+		r = (pixel >> screen->format->Rshift) & 0xff;
+		g = (pixel >> screen->format->Gshift) & 0xff;
+		b = (pixel >> screen->format->Bshift) & 0xff;
+		*((bits) + screen->format->Rshift / 8) = r;
+		*((bits) + screen->format->Gshift / 8) = g;
+		*((bits) + screen->format->Bshift / 8) = b;
+		}
+		break;
+	case 4:
+		*((Uint32 *)(bits)) = (Uint32)pixel;
+		break;
+	}
 }
-
 
 void drawhline (int x, int y, int w)
 {
     int i;
 
     for (i = 0; i < w; i++)
-	XPutPixel (ximage, x + i, y, __color);
+	put_pixel(x + i, y, __color);
 }
 
 
@@ -157,26 +110,52 @@ void drawvline (int x, int y, int h)
     int i;
 
     for (i = 0; i < h; i++)
-	XPutPixel (ximage, x, y + i, __color);
+	put_pixel(x, y + i, __color);
 }
 
-#endif /* USE_X11 */
 
-
-void setpalette ()
+void drawpixel(int x, int y)
 {
-    int i;
+    put_pixel(x, y, __color);
+}
 
-    for (i = 0; i < 16; i++) {
-#ifdef USE_X11
-	color[i].red = palette[i * 3] << 10;
-	color[i].green = palette[i * 3 + 1] << 10;
-	color[i].blue = palette[i * 3 + 2] << 10;
-	XAllocColor (display, colormap, &color[i]);
-#else
-	vga_setpalette (i, palette[i*3], palette[i*3 + 1], palette[i*3 + 2]);
+
+int init_video()
+{
+	int i, mode;
+    
+	if (SDL_Init (SDL_INIT_VIDEO /*| SDL_INIT_AUDIO*/) < 0) {
+		fprintf (stderr, "sdl: can't initialize: %s\n",
+			SDL_GetError ());
+		return -1;
+	}
+
+	mode = SDL_SWSURFACE | SDL_ANYFORMAT;
+
+#if 0
+	if (opt.fullscreen)
+		mode |= SDL_FULLSCREEN;
 #endif
-    }
+
+	if ((screen = SDL_SetVideoMode (640, 480, 8, mode)) == NULL) {
+		fprintf (stderr, "sdl: can't set video mode: %s\n",
+			SDL_GetError ());
+		return -1;
+	}
+	atexit(SDL_Quit);
+
+	SDL_WM_SetCaption("xmdp", "xmdp");
+
+	for (i = 0; i < 16; i++) {
+		color[i].r = palette[i * 3] << 2;
+		color[i].g = palette[i * 3 + 1] << 2;
+		color[i].b = palette[i * 3 + 2] << 2;
+		mapped_color[i] = SDL_MapRGB(screen->format,
+			color[i].r, color[i].g, color[i].b);
+	}
+	SDL_SetColors(screen, color, 0, 16);
+
+	return 0;
 }
 
 
@@ -215,17 +194,13 @@ void prepare_screen ()
 
 void draw_lines (int i, int a, int b, int c)
 {
-#ifdef USE_X11
     int y = a;
-#endif
 
     setcolor (c);
     i = i * 16 + 1;
     for (; a < b; a++)
 	drawhline (i, a, 13);
-#ifdef USE_X11
-    XShmPutImage (display, window, gc, ximage, i, y, i, y, 13, a - y + 1, 0);
-#endif
+    SDL_UpdateRect(screen, i, y, 13, a - y + 1);
 }
 
 
@@ -299,50 +274,45 @@ void draw_progress (int pos)
 	    drawvline (11 + i, 58, 7);
     }
     oldpos = pos;
-#ifdef USE_X11
-    XShmPutImage (display, window, gc, ximage, 11, 58, 11, 58, 127, 7, 0);
-#endif
+    SDL_UpdateRect(screen, 11, 58, 127, 7);
 }
 
 
 int writemsg (struct font_header *f, int x, int y, char *s, int c, int b)
 {
-    int w, x1 = 0, y1 = 0, i;
+    int w, x1 = 0, y1 = 0;
     char *p;
 
     if (!*s)
 	return 0;
+
     for (; *s; s++, x1++) {
 	for (w = 0; *((p = f->map[f->index[(int) *s] + w])); w++) {
 	    for (; *p; x1++) {
 		for (y1 = 0; *p; p++, y1++) {
 		    if (c >= 0) {
-			i = getpixel (x + x1, y - y1);
-			if ((*p == '#') && (i != c)) {
+			if ((*p == '#')) {
 			    setcolor (c);
-			    drawpixel (x + x1, y - y1);
+			    drawpixel(x + x1, y - y1);
 			} else if ((b != -1) && (*p != '#')) {
 			    setcolor (b);
-			    drawpixel (x + x1, y - y1);
+			    drawpixel(x + x1, y - y1);
 			}
 		    }
 		}
 		if ((b != -1) && (c != -1)) {
 		    setcolor (b);
 		    for (; y1 < f->h; y1++)
-			if ((i = getpixel (x + x1, y - y1)) != b)
-			    drawpixel (x + x1, y - y1);
+			drawpixel(x + x1, y - y1);
 		}
 	    }
 	    for (y1 = 0; (b != -1) && (c != -1) && (y1 < f->h); y1++)
-		if ((i = getpixel (x + x1, y - y1)) != b)
-		    drawpixel (x + x1, y - y1);
+		drawpixel(x + x1, y - y1);
 	}
     }
 
-#ifdef USE_X11
-    XShmPutImage (display, window, gc, ximage, x, y - y1, x, y - y1, x1, y1, 0);
-#endif
+    if (c != -1)
+	SDL_UpdateRect(screen, x, y - f->h + 1, x1, f->h);
 
     return x1;
 }
@@ -357,59 +327,35 @@ void shadowmsg (struct font_header *f, int x, int y, char *s, int c, int b)
 
 void process_events ()
 {
-#ifdef USE_X11
-    static XEvent event;
-#else
-    int i;
-#endif
+    SDL_Event event;
 
-#ifdef USE_X11
-    while (XEventsQueued (display, QueuedAfterReading)) {
-	XNextEvent (display, &event);
-	switch (event.type) {
-	case Expose:
-	    XShmPutImage (display, window, gc, ximage,
-		event.xexpose.x, event.xexpose.y,
-		event.xexpose.x, event.xexpose.y,
-		event.xexpose.width, event.xexpose.height, 0);
-	    break;
-	case KeyPress:
-	    switch (event.xkey.keycode) {
-#else
-    switch (i = GET_KEY ()) {
-#endif
-    case KEY_QUIT:
-	xmp_stop_module(ctx);
-	break;
+    while (SDL_PollEvent (&event) > 0) {
+	if (event.type == SDL_KEYDOWN) {
+	    switch ((int)event.key.keysym.sym) {
+	    case SDLK_F10:
+	    case SDLK_ESCAPE:
+		xmp_stop_module(ctx);
+		break;
 #if 0
-    case KEY_PAUSE:
-	xmp_pause_module ();
-	break;
+	    case SDLK_SPACE:
+		xmp_pause_module(ctx);
+		break;
 #endif
-    case KEY_LEFT:
-	xmp_ord_prev(ctx);
-	break;
-    case KEY_RIGHT:
-	xmp_ord_next(ctx);
-	break;
-    case KEY_MINUS:
-#if defined(USE_X11)||defined(RAW_KEYS)
-    case KEY_MINUS2:
-#endif
-	xmp_gvol_dec(ctx);
-	break;
-    case KEY_PLUS:
-#if defined(USE_X11)||defined(RAW_KEYS)
-    case KEY_PLUS2:
-#endif
-	xmp_gvol_inc(ctx);
-	break;
+	    case SDLK_LEFT:
+		xmp_ord_prev(ctx);
+		break;
+	    case SDLK_RIGHT:
+		xmp_ord_next(ctx);
+		break;
+	    case SDLK_MINUS:
+		xmp_gvol_dec(ctx);
+		break;
+	    case SDLK_PLUS:
+		xmp_gvol_inc(ctx);
+		break;
+	    }
+	}
     }
-#ifdef USE_X11
-	    break;
-	} /* switch */
-    } /* while */
-#endif
 }
 
 
@@ -433,10 +379,7 @@ static void event_callback(unsigned long i)
 	update_counter (global_vol, gvl, 68); 
 	draw_progress (ord * 126 / mi.len +
 	    (msg & 0xff) * 126 / mi.len / (msg >> 8));
-#ifdef USE_X11
 	process_events ();
-	XFlush (display);
-#endif
 	break;
     case XMP_ECHO_INS:
 	ins = msg &0xff;
@@ -461,27 +404,24 @@ static void event_callback(unsigned long i)
 
 void usage ()
 {
-    printf ("Usage: %s [-v] [-r refresh_rate] module\n", MDP);
+    printf ("Usage: xmdp [-v] [-r refresh_rate] module\n");
 }
 
 
 int main (int argc, char **argv)
 {
-    int o, rate;
+    int o;
     struct xmp_options opt;
 
     init_drivers();
     ctx = xmp_create_context();
     xmp_init(ctx, argc, argv);
 
-    while ((o = getopt (argc, argv, "vr:")) != -1) {
+    while ((o = getopt (argc, argv, "v")) != -1) {
 	switch (o) {
 	case 'v':
 	    printf ("Version %s\n", VERSION);
 	    exit (0);
-	case 'r':
-	    rate = atoi (optarg);
-	    break;
 	default:
 	    exit (-1);
 	}
@@ -505,61 +445,9 @@ int main (int argc, char **argv)
         }
     }
 
-#ifdef USE_X11
-    if ((display = XOpenDisplay (NULL)) == NULL) {
-	printf ("No connection to server - aborting\n");
-	exit (-1);
-    }
+    init_video();
 
-    screen = DefaultScreen (display);
-    scrptr = DefaultScreenOfDisplay (display);
-    visual = DefaultVisual (display, screen);
-    root = DefaultRootWindow (display);
-    depth = DefaultDepth (display, screen);
-    colormap = DefaultColormap (display, screen);
-    attribute_mask = CWEventMask;
-    attributes.event_mask |= ExposureMask | KeyPressMask;
-    window = XCreateWindow (display, root, 0, 0, 640, 480, 1,
-	DefaultDepthOfScreen (scrptr), InputOutput, CopyFromParent,
-	attribute_mask, &attributes);
-
-    if (!window) {
-	printf ("Can't create window!\n");
-	exit (-1);
-    }
-
-    notify (&argc, argv);
-    gc = XCreateGC (display, window, 0, NULL);
-    ximage = XShmCreateImage (display, visual, depth, ZPixmap,
-	NULL, &shminfo, 640, 480);
-
-    if (!ximage) {
-	printf ("Can't create image!\n");
-	exit (-1);
-    }
-
-    shminfo.shmid = shmget (IPC_PRIVATE, ximage->bytes_per_line *
-	(ximage->height + 1), IPC_CREAT | 0600);
-
-    if (shminfo.shmid == -1) {
-	printf ("Can't allocate X shared memory!\n");
-	exit (-1);
-    }
-
-    shminfo.shmaddr = ximage->data = shmat (shminfo.shmid, 0, 0);
-    shminfo.readOnly = 0;
-    XShmAttach (display, &shminfo);
-
-    XMapWindow (display, window);
-    XSetWindowBackground (display, window, BlackPixel (display, screen));
-    XClearWindow (display, window);
-    XFlush (display);
-#else
-    vga_init ();
-    vga_screenoff ();
-    vga_setmode (VGAMODE);
-#endif
-
+#if 0
     shmid = shmget (IPC_PRIVATE, 40 * sizeof (struct channel_info),
 	IPC_CREAT | 0600);
 
@@ -574,26 +462,15 @@ int main (int argc, char **argv)
         fprintf (stderr, "Error attaching shared memory segment\n");
         exit (-1);
     }
+#endif
 
-    signal (SIGSEGV, signal_handler);
-    signal (SIGQUIT, signal_handler);
-    signal (SIGTERM, signal_handler);
-    signal (SIGFPE, signal_handler);
-    signal (SIGINT, signal_handler);
-
-    setpalette ();
     prepare_screen ();
 
     /* Pipc->pctl.gvl = 64; */
     rightmsg (&font2, 500, 36, "Spacebar to pause", 14, -1);
     rightmsg (&font2, 500, 52, "+/- to change volume", 14, -1);
-#if(defined RAW_KEYS)||(defined USE_X11)
     rightmsg (&font2, 500, 20, "F10 to quit", 14, -1);
     rightmsg (&font2, 500, 68, "Arrows to change pattern", 14, -1);
-#else
-    rightmsg (&font2, 500, 10, "q to quit", 14, -1);
-    rightmsg (&font2, 500, 68, "j/k to change pattern", 14, -1);
-#endif
     writemsg (&font2, 540, 20, "Ord", 14, -1);
     writemsg (&font2, 540, 36, "Pat", 14, -1);
     writemsg (&font2, 540, 52, "Row", 14, -1);
@@ -603,28 +480,9 @@ int main (int argc, char **argv)
     writemsg (&font2, 580, 52, ":", 14, -1);
     writemsg (&font2, 580, 68, ":", 14, -1);
 
-#ifdef USE_X11
-    XShmPutImage (display, window, gc, ximage, 0, 0, 0, 0, 640, 480, 0);
-#else
-    vga_screenon ();
-#endif
-
-#ifdef RAW_KEYS
-    printf ("Switching to raw keyboard mode\n");
-    rawmode_init ();
-#endif
+    SDL_UpdateRect(screen, 0, 0, 640, 480);
 
     xmp_register_event_callback(ctx, event_callback);
-
-    if (!(pid = fork ())) {
-	while (42) {
-	    usleep (50000);
-	    draw_bars ();
-#ifdef USE_X11
-	    XFlush (display);
-#endif
-	}
-    }
 
     switch (xmp_load_module(ctx, argv[optind])) {
     case -1:
@@ -643,14 +501,18 @@ int main (int argc, char **argv)
 	break; }
     default:
 	xmp_get_module_info(ctx, &mi);
-	shadowmsg (&font1, 10, 26, mi.name, 15, -1);
-	xmp_play_module(ctx);
+	shadowmsg(&font1, 10, 26, mi.name, 15, -1);
+
+	xmp_player_start(ctx);
+	while (xmp_player_frame(ctx) == 0) {
+		xmp_play_buffer(ctx);
+		draw_bars();
+	}
+	xmp_player_end(ctx);
 	break;
     }
 
     xmp_close_audio(ctx);
-
-    signal_handler ();
 
     return 0;
 }
--- src/test/xmdp/mdp.h
+++ src/test/xmdp/mdp.h
@@ -2,54 +2,7 @@
  * Copyright (C) 1997 Claudio Matsuoka and Hipolito Carraro Jr
  */
 
-#include "rawkey.h"
-
-#ifdef USE_X11
-#undef RAW_KEYS
-#define MDP "xmdp"
-#define KEY_LEFT 100
-#define KEY_RIGHT 102
-#define KEY_QUIT 76
-#define KEY_MINUS 20
-#define KEY_MINUS2 1
-#define KEY_PLUS 21
-#define KEY_PLUS2 0
-#else /* SVGAlib */
-#include <vga.h>
-#define MDP "smdp"
-#ifdef RAW_KEYS
-#define GET_KEY() get_scancode()
-#define KEY_LEFT CURSOR_LEFT
-#define KEY_RIGHT CURSOR_RIGHT
-#define KEY_QUIT FUNC_KEY(10)
-#define KEY_MINUS 12
-#define KEY_MINUS2 GRAY_MINUS
-#define KEY_PLUS 10
-#define KEY_PLUS2 GRAY_PLUS
-#else /* SVGAlib, no raw keys */
-#define GET_KEY() vga_getkey()
-#define KEY_LEFT 'k'
-#define KEY_RIGHT 'j'
-#define KEY_QUIT 'q'
-#define KEY_MINUS '-'
-#define KEY_PLUS '+'
-#endif
-#endif
-
-#define VGAMODE G640x480x16
 #define MAX_TIMER 1600		/* Expire time */
-
-#ifdef USE_X11
-#define drawpixel(x,y) XPutPixel(ximage,x,y,__color)
-#define getpixel(x,y) XGetPixel(ximage,x,y)
-#define setcolor(x) __color=color[x].pixel
-#else
-#define drawpixel(x,y) vga_drawpixel(x,y)
-#define getpixel(x,y) vga_getpixel(x,y)
-#define setcolor(x) vga_setcolor(x)
-#define drawhline(x,y,w) vga_drawline(x,y,x+w-1,y)
-#define drawvline(x,y,h) vga_drawline(x,y,x,y+h-1)
-#endif
 
 struct channel_info {
     int timer;
@@ -58,5 +11,4 @@ struct channel_info {
     int bend;
     int y2, y3;
 };
-
 
