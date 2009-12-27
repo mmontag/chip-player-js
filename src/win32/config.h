d1104f83c0138610f6f72c6b5a44483bfcbf6502
Claudio Matsuoka <cmatsuoka@gmail.com>
2009-12-27 20:48:49 -0200

    Test MSVC version
    Signed-off-by: Claudio Matsuoka <cmatsuoka@gmail.com>

--- src/win32/config.h
+++ src/win32/config.h
@@ -13,31 +13,31 @@
 // 4018: signed/unsigned mismatch
 // 4244: conversion from XXX to YYY, possible loss of data
 // 4761: integral size mismatch in argument; conversion supplied
-#pragma warning( disable : 4018 4244 4761 )
+#pragma warning(disable: 4018 4244 4761)
 
 #define  strcasecmp  _strcmpi
 #define  snprintf    _snprintf
 
 #define S_ISDIR(a) ((a) & _S_IFDIR)
 
-// Additional definitions needed by MSVC 9.0 
-#if _MSC_VER > 0
+// Additional definitions needed by MSVC++ 9.0
+#if _MSC_VER >= 1500
 #define inline	__inline
 #define fileno	_fileno
 #else
-#define write        _write
+#define write	_write
 #endif
 
-#define open         _open
-#define lseek        _lseek
-#define close        _close
+#define open	_open
+#define lseek	_lseek
+#define close	_close
 
 #ifndef PATH_MAX
 #define PATH_MAX	260
 #endif
 
 #ifndef M_LN2
-#define M_LN2		0.69314718055994530942
+#define M_LN2	0.69314718055994530942
 #endif
 
 #endif
