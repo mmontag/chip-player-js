a400309c5096e8b8ed181bef291e62923c376a8e
Claudio Matsuoka <cmatsuoka@gmail.com>
2010-01-03 19:03:30 -0200

    Fuzzac loader code cleanup
    Signed-off-by: Claudio Matsuoka <cmatsuoka@gmail.com>

--- src/loaders/prowizard/fuzzac.c
+++ src/loaders/prowizard/fuzzac.c
@@ -30,227 +30,150 @@ struct pw_format pw_fuzz = {
 };
 
 
-#define ON  1
-#define OFF 2
-
-
 static int depack_fuzz(FILE *in, FILE *out)
 {
-	uint8 c1, c2, c3, c4, c5;
-	uint8 PatPos;
-	uint8 *tmp;
-	uint8 NbrTracks;
-	uint8 Pattern[1024];
-	uint8 PatternList[128];
-	uint8 Track_Numbers[128][16];
-	uint8 Track_Numbers_Real[128][4];
-	uint8 Track_Datas[4][256];
-	uint8 Status = ON;
-	long ssize = 0;
-	long i, j, k, l;
+	uint8 c1;
+	uint8 data[1024];
+	uint8 ord[128];
+	uint8 tidx[128][16];
+	uint8 tidx_real[128][4];
+	uint8 track[4][256];
+	uint8 status = 1;
+	int len, ntrk, npat;
+	int size, ssize = 0;
+	int lps, lsz;
+	int i, j, k, l;
 
-	memset(Track_Numbers, 0, 128 * 16);
-	memset(Track_Numbers_Real, 0, 128 * 4);
-	memset(PatternList, 0, 128);
+	memset(tidx, 0, 128 * 16);
+	memset(tidx_real, 0, 128 * 4);
+	memset(ord, 0, 128);
 
-	/* bypass ID */
-	fseek (in, 4, 0);	/* SEEK_SET */
+	read32b(in);			/* bypass ID */
+	read16b(in);			/* bypass 2 unknown bytes */
+	pw_write_zero(out, 20);		/* write title */
 
-	/* bypass 2 unknown bytes */
-	fseek (in, 2, 1);	/* SEEK_CUR */
-
-	/* write title */
-	tmp = (uint8 *) malloc (20);
-	memset(tmp, 0, 20);
-	fwrite (tmp, 20, 1, out);
-	free (tmp);
-
-	/*printf ( "Converting header ... " ); */
-	/*fflush ( stdout ); */
 	for (i = 0; i < 31; i++) {
-		c1 = 0x00;
-		for (j = 0; j < 22; j++) {	/*sample name */
-			fread (&c1, 1, 1, in);
-			fwrite (&c1, 1, 1, out);
-		}
-
-		fseek (in, 38, 1);	/* SEEK_CUR */
-
-		fread (&c1, 1, 1, in);	/* size */
-		fread (&c2, 1, 1, in);
-		ssize += (((c1 << 8) + c2) * 2);
-		fwrite (&c1, 1, 1, out);
-		fwrite (&c2, 1, 1, out);
-		fread (&c1, 1, 1, in);	/* loop start */
-		fread (&c2, 1, 1, in);
-		fread (&c3, 1, 1, in);	/* loop size */
-		fread (&c4, 1, 1, in);
-		fread (&c5, 1, 1, in);	/* finetune */
-		fwrite (&c5, 1, 1, out);
-		fread (&c5, 1, 1, in);	/* volume */
-		fwrite (&c5, 1, 1, out);
-		fwrite (&c1, 1, 1, out);
-		fwrite (&c2, 1, 1, out);
-
-		if ((c3 << 8) + c4 == 0x00)
-			c4 = 0x01;
-		fwrite (&c3, 1, 1, out);
-		fwrite (&c4, 1, 1, out);
+		pw_move_data(out, in, 22);	/*sample name */
+		fseek(in, 38, SEEK_CUR);
+		write16b(out, size = read16b(in));
+		ssize += size * 2;
+		lps = read16b(in);		/* loop start */
+		lsz = read16b(in);		/* loop size */
+		write8(out, read8(in));		/* finetune */
+		write8(out, read8(in));		/* volume */
+		write16b(out, lps);
+		write16b(out, lsz > 0 ? lsz : 1);
 	}
-	/*printf ( "ok\n" ); */
-	/*printf ( " - Whole sample size : %ld\n" , ssize ); */
 
-	/* read & write size of pattern list */
-	fread (&PatPos, 1, 1, in);
-	fwrite (&PatPos, 1, 1, out);
-	/*printf ( " - size of pattern list : %d\n" , PatPos ); */
-
-	/* read the number of tracks */
-	fread (&NbrTracks, 1, 1, in);
-
-	/* write noisetracker byte */
-	c1 = 0x7f;
-	fwrite (&c1, 1, 1, out);
-
+	write8(out, len = read8(in));	/* size of pattern list */
+	ntrk = read8(in);		/* read the number of tracks */
+	write8(out, 0x7f);		/* write noisetracker byte */
 
 	/* place file pointer at track number list address */
-	fseek (in, 2118, 0);	/* SEEK_SET */
+	fseek(in, 2118, SEEK_SET);
 
 	/* read tracks numbers */
 	for (i = 0; i < 4; i++) {
-		for (j = 0; j < PatPos; j++) {
-			fread (&Track_Numbers[j][i * 4], 1, 1, in);
-			fread (&Track_Numbers[j][i * 4 + 1], 1, 1, in);
-			fread (&Track_Numbers[j][i * 4 + 2], 1, 1, in);
-			fread (&Track_Numbers[j][i * 4 + 3], 1, 1, in);
-		}
+		for (j = 0; j < len; j++)
+			fread(&tidx[j][i * 4], 1, 4, in);
 	}
 
-
 	/* sort tracks numbers */
-	c5 = 0x00;
-	for (i = 0; i < PatPos; i++) {
+	npat = 0;
+	for (i = 0; i < len; i++) {
 		if (i == 0) {
-			PatternList[0] = c5;
-			c5 += 0x01;
+			ord[0] = npat++;
 			continue;
 		}
+
 		for (j = 0; j < i; j++) {
-			Status = ON;
+			status = 1;
 			for (k = 0; k < 4; k++) {
-				if (Track_Numbers[j][k * 4] !=
-					Track_Numbers[i][k * 4]) {
-					Status = OFF;
+				if (tidx[j][k * 4] !=
+					tidx[i][k * 4]) {
+					status = 0;
 					break;
 				}
 			}
-			if (Status == ON) {
-				PatternList[i] = PatternList[j];
+			if (status == 1) {
+				ord[i] = ord[j];
 				break;
 			}
 		}
-		if (Status == OFF) {
-			PatternList[i] = c5;
-			c5 += 0x01;
-		}
-		Status = ON;
+
+		if (status == 0)
+			ord[i] = npat++;
+
+		status = 1;
 	}
-	/* c5 is the Max pattern number */
 
-
-	/* create a real list of tracks numbers for the really existing patterns */
+	/* create a list of tracks numbers for the really existing patterns */
 	c1 = 0x00;
-	for (i = 0; i < PatPos; i++) {
+	for (i = 0; i < len; i++) {
 		if (i == 0) {
-			Track_Numbers_Real[c1][0] = Track_Numbers[i][0];
-			Track_Numbers_Real[c1][1] = Track_Numbers[i][4];
-			Track_Numbers_Real[c1][2] = Track_Numbers[i][8];
-			Track_Numbers_Real[c1][3] = Track_Numbers[i][12];
-			c1 += 0x01;
+			tidx_real[c1][0] = tidx[i][0];
+			tidx_real[c1][1] = tidx[i][4];
+			tidx_real[c1][2] = tidx[i][8];
+			tidx_real[c1][3] = tidx[i][12];
+			c1++;
 			continue;
 		}
+
 		for (j = 0; j < i; j++) {
-			Status = ON;
-			if (PatternList[i] == PatternList[j]) {
-				Status = OFF;
+			status = 1;
+			if (ord[i] == ord[j]) {
+				status = 0;
 				break;
 			}
 		}
-		if (Status == OFF)
+
+		if (status == 0)
 			continue;
-		Track_Numbers_Real[c1][0] = Track_Numbers[i][0];
-		Track_Numbers_Real[c1][1] = Track_Numbers[i][4];
-		Track_Numbers_Real[c1][2] = Track_Numbers[i][8];
-		Track_Numbers_Real[c1][3] = Track_Numbers[i][12];
-		c1 += 0x01;
-		Status = ON;
+
+		tidx_real[c1][0] = tidx[i][0];
+		tidx_real[c1][1] = tidx[i][4];
+		tidx_real[c1][2] = tidx[i][8];
+		tidx_real[c1][3] = tidx[i][12];
+		c1++;
+		status = 1;
 	}
 
-	/* write pattern list */
-	fwrite (PatternList, 128, 1, out);
-
-	/* write ID */
-	c1 = 'M';
-	c2 = '.';
-	c3 = 'K';
-	fwrite (&c1, 1, 1, out);
-	fwrite (&c2, 1, 1, out);
-	fwrite (&c3, 1, 1, out);
-	fwrite (&c2, 1, 1, out);
-
+	fwrite(ord, 128, 1, out);	/* write pattern list */
+	write32b(out, PW_MOD_MAGIC);	/* write ID */
 
 	/* pattern data */
-	/*printf ( "Processing the pattern datas ... " ); */
-	/*fflush ( stdout ); */
-	l = 2118 + (PatPos * 16);
-	for (i = 0; i < c5; i++) {
-		memset(Pattern, 0, 1024);
-		memset(Track_Datas, 0, 4 << 8);
-		fseek (in, l + (Track_Numbers_Real[i][0] << 8), 0);
-		fread (Track_Datas[0], 256, 1, in);
-		fseek (in, l + (Track_Numbers_Real[i][1] << 8), 0);
-		fread (Track_Datas[1], 256, 1, in);
-		fseek (in, l + (Track_Numbers_Real[i][2] << 8), 0);
-		fread (Track_Datas[2], 256, 1, in);
-		fseek (in, l + (Track_Numbers_Real[i][3] << 8), 0);
-		fread (Track_Datas[3], 256, 1, in);
+	l = 2118 + len * 16;
+
+	for (i = 0; i < npat; i++) {
+		memset(data, 0, 1024);
+		memset(track, 0, 4 << 8);
+
+		fseek(in, l + (tidx_real[i][0] << 8), SEEK_SET);
+		fread(track[0], 256, 1, in);
+
+		fseek(in, l + (tidx_real[i][1] << 8), SEEK_SET);
+		fread(track[1], 256, 1, in);
+
+		fseek(in, l + (tidx_real[i][2] << 8), SEEK_SET);
+		fread(track[2], 256, 1, in);
+
+		fseek(in, l + (tidx_real[i][3] << 8), SEEK_SET);
+		fread(track[3], 256, 1, in);
 
 		for (j = 0; j < 64; j++) {
-			Pattern[j * 16] = Track_Datas[0][j * 4];
-			Pattern[j * 16 + 1] = Track_Datas[0][j * 4 + 1];
-			Pattern[j * 16 + 2] = Track_Datas[0][j * 4 + 2];
-			Pattern[j * 16 + 3] = Track_Datas[0][j * 4 + 3];
-			Pattern[j * 16 + 4] = Track_Datas[1][j * 4];
-			Pattern[j * 16 + 5] = Track_Datas[1][j * 4 + 1];
-			Pattern[j * 16 + 6] = Track_Datas[1][j * 4 + 2];
-			Pattern[j * 16 + 7] = Track_Datas[1][j * 4 + 3];
-			Pattern[j * 16 + 8] = Track_Datas[2][j * 4];
-			Pattern[j * 16 + 9] = Track_Datas[2][j * 4 + 1];
-			Pattern[j * 16 + 10] = Track_Datas[2][j * 4 + 2];
-			Pattern[j * 16 + 11] = Track_Datas[2][j * 4 + 3];
-			Pattern[j * 16 + 12] = Track_Datas[3][j * 4];
-			Pattern[j * 16 + 13] = Track_Datas[3][j * 4 + 1];
-			Pattern[j * 16 + 14] = Track_Datas[3][j * 4 + 2];
-			Pattern[j * 16 + 15] = Track_Datas[3][j * 4 + 3];
+			memcpy(&data[j * 16     ], &track[0][j * 4], 4);
+			memcpy(&data[j * 16 + 4 ], &track[1][j * 4], 4);
+			memcpy(&data[j * 16 + 8 ], &track[2][j * 4], 4);
+			memcpy(&data[j * 16 + 12], &track[3][j * 4], 4);
+			data[j * 16 + 15] = track[3][j * 4 + 3];
 		}
-
-		fwrite (Pattern, 1024, 1, out);
-		/*printf ( "+" ); */
-		/*fflush ( stdout ); */
+		fwrite(data, 1024, 1, out);
 	}
-	/*printf ( "ok\n" ); */
 
 	/* sample data */
-	/*printf ( "Saving sample data ... " ); */
-	/*fflush ( stdout ); */
-	fseek (in, l + (NbrTracks << 8) + 4, SEEK_SET);
-	/* l : 2118 + npat*16 */
-	/* 4 : to bypass the "SEnd" unidentified ID */
-	tmp = (uint8 *) malloc (ssize);
-	fread (tmp, ssize, 1, in);
-	fwrite (tmp, ssize, 1, out);
-	free (tmp);
-	/*printf ( "ok\n" ); */
+	/* bypass the "SEnd" unidentified ID */
+	fseek(in, l + (ntrk << 8) + 4, SEEK_SET);
+	pw_move_data(out, in, ssize);
 
 	return 0;
 }
@@ -261,12 +184,12 @@ static int test_fuzz (uint8 *data, int s)
 	int j, k;
 	int start = 0, ssize = 0;
 
-	if (data[0]!='M' || data[1]!='1' || data[2]!='.' || data[3]!='0')
+	if (readmem32b(data) != MAGIC4('M','1','.','0'))
 		return -1;
 
 	/* test finetune */
 	for (k = 0; k < 31; k++) {
-		if (data[start + 72 + k * 68] > 0x0F)
+		if (data[start + 72 + k * 68] > 0x0f)
 			return -1;
 	}
 
@@ -278,11 +201,10 @@ static int test_fuzz (uint8 *data, int s)
 
 	/* test sample sizes */
 	for (k = 0; k < 31; k++) {
-		j = (data[start + 66 + k * 68] << 8) +
-			data[start + 67 + k * 68];
+		j = readmem16b(data + start + k * 68 + 66);
 		if (j > 0x8000)
 			return -1;
-		ssize += (j * 2);
+		ssize += j * 2;
 	}
 
 	/* test size of pattern list */
