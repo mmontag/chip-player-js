1fd029e003e7b8f3d1b611ae14da7cbc89f658f1
Claudio Matsuoka <cmatsuoka@gmail.com>
2010-05-12 20:57:33 -0300

    [Android] Add license text to Xmpoid.java
    Signed-off-by: Claudio Matsuoka <cmatsuoka@gmail.com>

--- src/android/project/src/org/helllabs/android/xmp/Xmpoid.java
+++ src/android/project/src/org/helllabs/android/xmp/Xmpoid.java
@@ -1,3 +1,24 @@
+/* Copyright (C) 2010 Claudio Matsuoka
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
 package org.helllabs.android.xmp;
 
 import java.io.File;
@@ -454,7 +475,8 @@ public class Xmpoid extends ListActivity {
 			
 			alertDialog.setTitle("About " + getString(R.string.app_name));
 			alertDialog.setMessage(getString(R.string.app_name) + " " +
-					getString(R.string.app_version));
+					getString(R.string.app_version) +
+					"\nFull source code available at http://sf.net/project/xmp");
 			alertDialog.setButton("Cool", new DialogInterface.OnClickListener() {
 				public void onClick(DialogInterface dialog, int which) {
 					alertDialog.dismiss();
