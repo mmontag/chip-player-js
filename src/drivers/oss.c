2b6ab9d00cd95eef165005cfe522a1ce4544ec73
Claudio Matsuoka <cmatsuoka@gmail.com>
2010-06-08 10:17:49 -0300

    Fix OSS driver fragment settings
    Signed-off-by: Claudio Matsuoka <cmatsuoka@gmail.com>

--- src/drivers/oss.c
+++ src/drivers/oss.c
@@ -168,8 +168,11 @@ static int init(struct xmp_context *ctx)
 	static char buf[80];
 #endif
 	char *token, **parm;
-	int i = 1024;
+	int i;
 
+	fragnum = 16;		/* default number of fragments */
+	i = 1024;		/* default size of fragment */
+	
 	parm_init();
 	chkparm2("frag", "%d,%d", &fragnum, &i);
 	chkparm1("dev", dev_audio[0] = token);
@@ -182,8 +185,6 @@ static int init(struct xmp_context *ctx)
 	for (fragsize = 0; i >>= 1; fragsize++) ;
 	if (fragsize < 4)
 		fragsize = 4;
-	if (fragnum > 1)
-		fragnum--;
 
 	for (i = 0; i < sizeof(dev_audio) / sizeof(dev_audio[0]); i++)
 		if ((audio_fd = open(dev_audio[i], O_WRONLY)) >= 0)
