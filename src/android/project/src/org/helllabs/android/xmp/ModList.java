d1b71e2c3aed4906b0a63efe00da0fb63690f114
Claudio Matsuoka <cmatsuoka@gmail.com>
2010-10-22 17:37:15 -0200

    [Android] Add playlist class and layout
    Signed-off-by: Claudio Matsuoka <cmatsuoka@gmail.com>

--- src/android/project/src/org/helllabs/android/xmp/ModList.java
+++ src/android/project/src/org/helllabs/android/xmp/ModList.java
@@ -44,7 +44,6 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.View.OnClickListener;
-import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.ListView;
 import org.helllabs.android.xmp.R;
---
+++ src/android/project/src/org/helllabs/android/xmp/PlayList.java
@@ -0,0 +1,13 @@
+package org.helllabs.android.xmp;
+
+import android.app.ListActivity;
+import android.os.Bundle;
+
+public class PlayList extends ListActivity {
+	@Override
+	public void onCreate(Bundle icicle) {
+		super.onCreate(icicle);
+		setContentView(R.layout.playlist);
+		
+	}
+}
