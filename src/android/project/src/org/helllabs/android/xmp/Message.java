b478451c97a74adf43d01bca23d1d7365bdeeb16
Claudio Matsuoka <cmatsuoka@gmail.com>
2010-10-25 13:25:50 -0200

    [Android] Add error dialog method
    Signed-off-by: Claudio Matsuoka <cmatsuoka@gmail.com>

--- src/android/project/src/org/helllabs/android/xmp/Message.java
+++ src/android/project/src/org/helllabs/android/xmp/Message.java
@@ -1,12 +1,23 @@
 package org.helllabs.android.xmp;
 
+import android.app.AlertDialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.widget.Toast;
 
 public class Message {
 	
-	public static void error(String s) {
+	public static void error(Context context, String message) {
+		AlertDialog alertDialog = new AlertDialog.Builder(context).create();
 		
+		alertDialog.setTitle("Error");
+		alertDialog.setMessage(message);
+		alertDialog.setButton("Dismiss", new DialogInterface.OnClickListener() {
+			public void onClick(DialogInterface dialog, int which) {
+				//
+			}
+		});
+		alertDialog.show();		
 	}
 	
 	public static void toast(Context context, String s) {
