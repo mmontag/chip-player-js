9f9247bb275977169c0cb888696295114c48ee1d
Claudio Matsuoka <cmatsuoka@gmail.com>
2011-08-06 10:34:58 -0300

    Cosmetic indenting changes in Audacious 3 code
    Signed-off-by: Claudio Matsuoka <cmatsuoka@gmail.com>

--- src/plugin/audacious3.c
+++ src/plugin/audacious3.c
@@ -36,11 +36,11 @@ static xmp_context ctx;
 
 /* config values used for the Preferences UI */
 struct {
-    gboolean bits16, bits8;
-    gboolean stereo, mono;
-    gboolean freq44, freq22, freq11;
-    gboolean fixloops, modrange, convert8bit, interpolation, filter;
-    gfloat panamp;
+	gboolean bits16, bits8;
+	gboolean stereo, mono;
+	gboolean freq44, freq22, freq11;
+	gboolean fixloops, modrange, convert8bit, interpolation, filter;
+	gfloat panamp;
 } guicfg;
 static void configure_init(void);
 
@@ -72,9 +72,9 @@ static void strip_vfs(char *s) {
 	int len;
 	char *c;
 
-    if (!s) {
-        return;
-    }
+	if (!s) {
+		return;
+	}
 	_D("%s", s);
 	if (!memcmp(s, "file://", 7)) {
 		len = strlen(s);
@@ -95,32 +95,35 @@ static void strip_vfs(char *s) {
 }
 
 
-static void stop(InputPlayback *playback) {
+static void stop(InputPlayback *playback)
+{
 	_D("*** stop!");
-    g_mutex_lock(seek_mutex);
-    if (!stop_flag) {
-        xmp_stop_module(ctx); 
-        stop_flag = TRUE;
-        playback->output->abort_write();
-        g_cond_signal(seek_cond);
-    }
-    g_mutex_unlock(seek_mutex);
+	g_mutex_lock(seek_mutex);
+	if (!stop_flag) {
+		xmp_stop_module(ctx); 
+		stop_flag = TRUE;
+		playback->output->abort_write();
+		g_cond_signal(seek_cond);
+	}
+	g_mutex_unlock(seek_mutex);
 }
 
 
-static void mseek(InputPlayback *playback, gint msec) {
-    g_mutex_lock(seek_mutex);
-    if (!stop_flag) {
-        jumpToTime = msec;
-        playback->output->abort_write();
-        g_cond_signal(seek_cond);
-        g_cond_wait(seek_cond, seek_mutex);
-    }
-    g_mutex_unlock(seek_mutex);
+static void mseek(InputPlayback *playback, gint msec)
+{
+	g_mutex_lock(seek_mutex);
+	if (!stop_flag) {
+		jumpToTime = msec;
+		playback->output->abort_write();
+		g_cond_signal(seek_cond);
+		g_cond_wait(seek_cond, seek_mutex);
+	}
+	g_mutex_unlock(seek_mutex);
 }
 
 
-static void seek_ctx(gint time) {
+static void seek_ctx(gint time)
+{
 	int i, t;
 	struct xmp_player_context *p = &((struct xmp_context *)ctx)->p;
 
@@ -142,25 +145,27 @@ static void seek_ctx(gint time) {
 }
 
 
-static void mod_pause(InputPlayback *playback, gboolean p) {
-    g_mutex_lock(seek_mutex);
-    if (!stop_flag) {
-        playback->output->pause(p);
-    }
-    g_mutex_unlock(seek_mutex);
+static void mod_pause(InputPlayback *playback, gboolean p)
+{
+	g_mutex_lock(seek_mutex);
+	if (!stop_flag) {
+		playback->output->pause(p);
+	}
+	g_mutex_unlock(seek_mutex);
 }
 
 
-static gboolean init(void) {
+static gboolean init(void)
+{
 	mcs_handle_t *cfg;
 
 	_D("Plugin init");
 	xmp_drv_register(&drv_smix);
 	ctx = xmp_create_context();
 
-    jumpToTime = -1;
-    seek_mutex = g_mutex_new();
-    seek_cond = g_cond_new();
+	jumpToTime = -1;
+	seek_mutex = g_mutex_new();
+	seek_cond = g_cond_new();
 
 	xmp_cfg.mixing_freq = 0;
 	xmp_cfg.convert8bit = 0;
@@ -188,39 +193,43 @@ static gboolean init(void) {
 		aud_cfg_db_close(cfg);
 	}
 
-    configure_init();
+	configure_init();
 
 	xmp_init(ctx, 0, NULL);
-    return TRUE;
+
+	return TRUE;
 }
 
 
-static void cleanup() {
-    g_cond_free(seek_cond);
-    g_mutex_free(seek_mutex);
+static void cleanup()
+{
+	g_cond_free(seek_cond);
+	g_mutex_free(seek_mutex);
 	xmp_free_context(ctx);
 }
 
 
-static int is_our_file_from_vfs(const char* _filename, VFSFile *vfsfile) {
-    gchar *filename = g_strdup(_filename);
-    gboolean ret;
+static int is_our_file_from_vfs(const char* _filename, VFSFile *vfsfile)
+{
+	gchar *filename = g_strdup(_filename);
+	gboolean ret;
 
 	_D("filename = %s", filename);
 	strip_vfs(filename);		/* Sorry, no VFS support */
 
 	ret = (xmp_test_module(ctx, filename, NULL) == 0);
 
-    g_free(filename);
+	g_free(filename);
 	return ret;
 }
 
 
-Tuple *probe_for_tuple(const gchar *_filename, VFSFile *fd) {
-    gchar *filename = g_strdup(_filename);
-    xmp_context ctx;
-    int len;
-    Tuple *tuple;
+Tuple *probe_for_tuple(const gchar *_filename, VFSFile *fd)
+{
+	gchar *filename = g_strdup(_filename);
+	xmp_context ctx;
+	int len;
+	Tuple *tuple;
 	struct xmp_module_info mi;
 	struct xmp_options *opt;
 
@@ -231,11 +240,12 @@ Tuple *probe_for_tuple(const gchar *_filename, VFSFile *fd) {
 	opt = xmp_get_options(ctx);
 	opt->skipsmp = 1;	/* don't load samples */
 	len = xmp_load_module(ctx, filename);
-    g_free(filename);
-    if (len < 0) {
-        xmp_free_context(ctx);
-        return NULL;
-    }
+	g_free(filename);
+
+	if (len < 0) {
+		xmp_free_context(ctx);
+		return NULL;
+	}
 
 	xmp_get_module_info(ctx, &mi);
 
@@ -245,8 +255,9 @@ Tuple *probe_for_tuple(const gchar *_filename, VFSFile *fd) {
 	tuple_associate_int(tuple, FIELD_LENGTH, NULL, len);
 
 	xmp_release_module(ctx);
-    xmp_free_context(ctx);
-    return tuple;
+	xmp_free_context(ctx);
+
+	return tuple;
 }
 
 
@@ -257,21 +268,21 @@ static gboolean play(InputPlayback *ipb, const gchar *_filename, VFSFile *file, 
 	int lret, fmt, nch;
 	void *data;
 	int size;
-    gchar *filename = g_strdup(_filename);
+	gchar *filename = g_strdup(_filename);
 	Tuple *tuple;
 	
 	_D("play: %s\n", filename);
-    if (file == NULL) {
-        return FALSE;
-    }
+	if (file == NULL) {
+		return FALSE;
+	}
 	opt = xmp_get_options(ctx);
 
 	strip_vfs(filename);  /* Sorry, no VFS support */
 
 	_D("play_file: %s", filename);
 
-    jumpToTime = (start_time > 0) ? start_time : -1;
-    stop_flag = FALSE;
+	jumpToTime = (start_time > 0) ? start_time : -1;
+	stop_flag = FALSE;
 
 	if ((f = fopen(filename,"rb")) == 0) {
 		goto PLAY_ERROR_1;
@@ -328,7 +339,7 @@ static gboolean play(InputPlayback *ipb, const gchar *_filename, VFSFile *file, 
 	_D("*** loading: %s", filename);
 
 	lret =  xmp_load_module(ctx, filename);
-    g_free(filename);
+	g_free(filename);
 
 	if (lret < 0) {
 		xmp_close_audio(ctx);
@@ -342,7 +353,7 @@ static gboolean play(InputPlayback *ipb, const gchar *_filename, VFSFile *file, 
 	tuple_associate_string(tuple, FIELD_TITLE, NULL, xmp_cfg.mod_info.name);
 	tuple_associate_string(tuple, FIELD_CODEC, NULL, xmp_cfg.mod_info.type);
 	tuple_associate_int(tuple, FIELD_LENGTH, NULL, lret);
-    ipb->set_tuple( ipb, tuple );
+	ipb->set_tuple(ipb, tuple);
 
 	ipb->set_params(ipb, xmp_cfg.mod_info.chn * 1000, opt->freq, channelcnt);
 	ipb->set_pb_ready(ipb);
@@ -350,73 +361,77 @@ static gboolean play(InputPlayback *ipb, const gchar *_filename, VFSFile *file, 
 	stop_flag = FALSE;
 	xmp_player_start(ctx);
 
-	while ( !stop_flag ) {
-        if (stop_time >= 0 && ipb->output->written_time () >= stop_time) {
-            goto DRAIN;
-        }
-        g_mutex_lock(seek_mutex);
-        if ( jumpToTime != -1 ) {
-            seek_ctx(jumpToTime);
-            ipb->output->flush(jumpToTime);
-            jumpToTime = -1;
-            g_cond_signal(seek_cond);
-        }
-        g_mutex_unlock(seek_mutex);
+	while (!stop_flag) {
+		if (stop_time >= 0 && ipb->output->written_time() >= stop_time) {
+			goto DRAIN;
+        	}
+
+		g_mutex_lock(seek_mutex);
+		if (jumpToTime != -1) {
+			seek_ctx(jumpToTime);
+			ipb->output->flush(jumpToTime);
+			jumpToTime = -1;
+			g_cond_signal(seek_cond);
+		}
+		g_mutex_unlock(seek_mutex);
 
 		xmp_get_buffer(ctx, &data, &size);
-        if ( !stop_flag && jumpToTime<0 ) {
-            ipb->output->write_audio(data,size);
-        }
-        if ( (xmp_player_frame(ctx) != 0) && jumpToTime<0 ) {
-            stop_flag = TRUE;
- DRAIN:
-            while ( !stop_flag && ipb->output->buffer_playing() ) {
-                g_usleep(20000);
-            }
-            break;
-        }
-	}
-    g_mutex_lock(seek_mutex);
-    stop_flag = TRUE;
-    g_cond_signal(seek_cond);  /* wake up any waiting request */
-    g_mutex_unlock(seek_mutex);
 
-    ipb->output->close_audio();
+        	if ( !stop_flag && jumpToTime<0 ) {
+			ipb->output->write_audio(data,size);
+        	}
+
+		if ( (xmp_player_frame(ctx) != 0) && jumpToTime<0 ) {
+			stop_flag = TRUE;
+ DRAIN:
+			while (!stop_flag && ipb->output->buffer_playing()) {
+				g_usleep(20000);
+			}
+			break;
+		}
+	}
+
+	g_mutex_lock(seek_mutex);
+	stop_flag = TRUE;
+	g_cond_signal(seek_cond);  /* wake up any waiting request */
+	g_mutex_unlock(seek_mutex);
+
+	ipb->output->close_audio();
 	xmp_player_end(ctx);
 	xmp_release_module(ctx);
 	xmp_close_audio(ctx);
-    return TRUE;
+	return TRUE;
 
  PLAY_ERROR_1:
-    g_free(filename);
-    return FALSE;
+	g_free(filename);
+	return FALSE;
 }
 
 
-static void configure_apply() {
+static void configure_apply()
+{
 	mcs_handle_t *cfg;
 	struct xmp_options *opt;
 
-    /* transfer Preferences UI config values back into XMPConfig */
-    if (guicfg.freq11) {
-        xmp_cfg.mixing_freq = FREQ_SAMPLE_11;
-    }
-    else if (guicfg.freq22) {
-        xmp_cfg.mixing_freq = FREQ_SAMPLE_22;
-    }
-    else {  /* if (guicfg.freq44) { */
-        xmp_cfg.mixing_freq = FREQ_SAMPLE_44;
-    }
-    xmp_cfg.convert8bit = guicfg.bits8;
-    xmp_cfg.force_mono = guicfg.mono;
-    xmp_cfg.fixloops = guicfg.fixloops;
-    xmp_cfg.modrange = guicfg.modrange;
-    xmp_cfg.interpolation = guicfg.interpolation;
-    xmp_cfg.filter = guicfg.filter;
-    xmp_cfg.pan_amplitude = (gint)guicfg.panamp;
+	/* transfer Preferences UI config values back into XMPConfig */
+	if (guicfg.freq11) {
+		xmp_cfg.mixing_freq = FREQ_SAMPLE_11;
+	} else if (guicfg.freq22) {
+		xmp_cfg.mixing_freq = FREQ_SAMPLE_22;
+	} else {  /* if (guicfg.freq44) { */
+		xmp_cfg.mixing_freq = FREQ_SAMPLE_44;
+	}
+
+	xmp_cfg.convert8bit = guicfg.bits8;
+	xmp_cfg.force_mono = guicfg.mono;
+	xmp_cfg.fixloops = guicfg.fixloops;
+	xmp_cfg.modrange = guicfg.modrange;
+	xmp_cfg.interpolation = guicfg.interpolation;
+	xmp_cfg.filter = guicfg.filter;
+	xmp_cfg.pan_amplitude = (gint)guicfg.panamp;
 
 	opt = xmp_get_options(ctx);
-    opt->mix = xmp_cfg.pan_amplitude;
+	opt->mix = xmp_cfg.pan_amplitude;
 
 	cfg = aud_cfg_db_open();
 
@@ -435,28 +450,30 @@ static void configure_apply() {
 	aud_cfg_db_close(cfg);
 }
 
-static void configure_init(void) {
-    /* transfer XMPConfig into Preferences UI config */
-    /* keeping compatibility with older releases */
-    guicfg.freq11 = (xmp_cfg.mixing_freq == FREQ_SAMPLE_11);
-    guicfg.freq22 = (xmp_cfg.mixing_freq == FREQ_SAMPLE_22);
-    guicfg.freq44 = (xmp_cfg.mixing_freq == FREQ_SAMPLE_44);
-    guicfg.mono = xmp_cfg.force_mono;
-    guicfg.stereo = !xmp_cfg.force_mono;
-    guicfg.bits8 = xmp_cfg.convert8bit;
-    guicfg.bits16 = !xmp_cfg.convert8bit;
-    guicfg.convert8bit = xmp_cfg.convert8bit;
-    guicfg.fixloops = xmp_cfg.fixloops;
-    guicfg.modrange = xmp_cfg.modrange;
-    guicfg.interpolation = xmp_cfg.interpolation;
-    guicfg.filter = xmp_cfg.filter;
-    guicfg.panamp = xmp_cfg.pan_amplitude;
+static void configure_init(void)
+{
+	/* transfer XMPConfig into Preferences UI config */
+	/* keeping compatibility with older releases */
+	guicfg.freq11 = (xmp_cfg.mixing_freq == FREQ_SAMPLE_11);
+	guicfg.freq22 = (xmp_cfg.mixing_freq == FREQ_SAMPLE_22);
+	guicfg.freq44 = (xmp_cfg.mixing_freq == FREQ_SAMPLE_44);
+	guicfg.mono = xmp_cfg.force_mono;
+	guicfg.stereo = !xmp_cfg.force_mono;
+	guicfg.bits8 = xmp_cfg.convert8bit;
+	guicfg.bits16 = !xmp_cfg.convert8bit;
+	guicfg.convert8bit = xmp_cfg.convert8bit;
+	guicfg.fixloops = xmp_cfg.fixloops;
+	guicfg.modrange = xmp_cfg.modrange;
+	guicfg.interpolation = xmp_cfg.interpolation;
+	guicfg.filter = xmp_cfg.filter;
+	guicfg.panamp = xmp_cfg.pan_amplitude;
 }
 
-void xmp_aud_about() {
-    static GtkWidget *about_window = NULL;
+void xmp_aud_about()
+{
+	static GtkWidget *about_window = NULL;
 
-    audgui_simple_message(&about_window, GTK_MESSAGE_INFO,
+	audgui_simple_message(&about_window, GTK_MESSAGE_INFO,
                           g_strdup_printf(
                 "Extended Module Player %s",VERSION),
                 "Written by Claudio Matsuoka and Hipolito Carraro Jr.\n"
@@ -466,96 +483,123 @@ void xmp_aud_about() {
                 "(C) 1997 Bert Jahn, (C) 1999 Tatsuyuki Satoh, (C)\n"
                 "2001-2006 Russell Marks, (C) 2005-2006 Michael Kohn\n"
                 "\n"
-                          /* TODO: list */
-                          /* "Supported module formats:" */
-                          );
+		/* TODO: list */
+		/* "Supported module formats:" */
+	);
 }
 
 
 static PreferencesWidget prefs_precision[] = {
-    {WIDGET_RADIO_BTN, "16 bit", &guicfg.bits16, NULL, NULL, FALSE, .cfg_type = VALUE_BOOLEAN},
-    {WIDGET_RADIO_BTN, "8 bit", &guicfg.bits8, NULL, NULL, FALSE, .cfg_type = VALUE_BOOLEAN},
+	{ WIDGET_RADIO_BTN, "16 bit", &guicfg.bits16,
+		NULL, NULL, FALSE, .cfg_type = VALUE_BOOLEAN },
+	{ WIDGET_RADIO_BTN, "8 bit", &guicfg.bits8,
+		NULL, NULL, FALSE, .cfg_type = VALUE_BOOLEAN },
 };
 
 static PreferencesWidget prefs_channels[] = {
-    {WIDGET_RADIO_BTN, "Stereo", &guicfg.stereo, NULL, NULL, FALSE, .cfg_type = VALUE_BOOLEAN},
-    {WIDGET_RADIO_BTN, "Mono", &guicfg.mono, NULL, NULL, FALSE, .cfg_type = VALUE_BOOLEAN},
+	{ WIDGET_RADIO_BTN, "Stereo", &guicfg.stereo,
+		NULL, NULL, FALSE, .cfg_type = VALUE_BOOLEAN },
+	{ WIDGET_RADIO_BTN, "Mono", &guicfg.mono,
+		NULL, NULL, FALSE, .cfg_type = VALUE_BOOLEAN },
 };
 
 static PreferencesWidget prefs_frequency[] = {
-    {WIDGET_RADIO_BTN, "44 kHz", &guicfg.freq44, NULL, NULL, FALSE, .cfg_type = VALUE_BOOLEAN},
-    {WIDGET_RADIO_BTN, "22 kHz", &guicfg.freq22, NULL, NULL, FALSE, .cfg_type = VALUE_BOOLEAN},
-    {WIDGET_RADIO_BTN, "11 kHz", &guicfg.freq11, NULL, NULL, FALSE, .cfg_type = VALUE_BOOLEAN},
+	{ WIDGET_RADIO_BTN, "44 kHz", &guicfg.freq44,
+		NULL, NULL, FALSE, .cfg_type = VALUE_BOOLEAN },
+	{ WIDGET_RADIO_BTN, "22 kHz", &guicfg.freq22,
+		NULL, NULL, FALSE, .cfg_type = VALUE_BOOLEAN },
+	{ WIDGET_RADIO_BTN, "11 kHz", &guicfg.freq11,
+		NULL, NULL, FALSE, .cfg_type = VALUE_BOOLEAN },
 };
 
 static PreferencesWidget prefs_opts[] = {
-    {WIDGET_CHK_BTN, "Convert 16 bit samples to 8 bit", &guicfg.convert8bit, NULL, NULL, FALSE,
-     .cfg_type = VALUE_BOOLEAN},
-    {WIDGET_CHK_BTN, "Fix sample loops", &guicfg.fixloops, NULL, NULL, FALSE,
-     .cfg_type = VALUE_BOOLEAN},
-    {WIDGET_CHK_BTN, "Force 3 octave range in standard MOD files", &guicfg.modrange, NULL, NULL, FALSE,
-     .cfg_type = VALUE_BOOLEAN},
-    {WIDGET_CHK_BTN, "Enable 32-bit linear interpolation", &guicfg.interpolation, NULL, NULL, FALSE,
-     .cfg_type = VALUE_BOOLEAN},
-    {WIDGET_CHK_BTN, "Enable IT filters", &guicfg.filter, NULL, NULL, FALSE,
-     .cfg_type = VALUE_BOOLEAN},
-    {WIDGET_LABEL, "Pan amplitude (%)", NULL, NULL, NULL, FALSE},
-    {WIDGET_SPIN_BTN, "", &guicfg.panamp, NULL, NULL, FALSE,
-     {.spin_btn = {0.0,100.0,1.0,""}},
-     .cfg_type = VALUE_FLOAT},
+	{ WIDGET_CHK_BTN, "Convert 16 bit samples to 8 bit",
+		&guicfg.convert8bit, NULL, NULL, FALSE,
+		.cfg_type = VALUE_BOOLEAN },
+	{ WIDGET_CHK_BTN, "Fix sample loops", &guicfg.fixloops,
+		NULL, NULL, FALSE, .cfg_type = VALUE_BOOLEAN },
+	{ WIDGET_CHK_BTN, "Force 3 octave range in standard MOD files",
+		&guicfg.modrange, NULL, NULL, FALSE,
+		.cfg_type = VALUE_BOOLEAN },
+	{ WIDGET_CHK_BTN, "Enable 32-bit linear interpolation",
+		&guicfg.interpolation, NULL, NULL, FALSE,
+		.cfg_type = VALUE_BOOLEAN },
+	{ WIDGET_CHK_BTN, "Enable IT filters", &guicfg.filter,
+		NULL, NULL, FALSE, .cfg_type = VALUE_BOOLEAN},
+	{ WIDGET_LABEL, "Pan amplitude (%)", NULL, NULL, NULL, FALSE },
+	{ WIDGET_SPIN_BTN, "", &guicfg.panamp, NULL, NULL, FALSE,
+		{ .spin_btn = { 0.0, 100.0, 1.0, "" } },
+		.cfg_type = VALUE_FLOAT},
 };
 
 static PreferencesWidget prefs_opts_tab[] = {
-    {WIDGET_BOX, NULL, NULL, NULL, NULL, FALSE,
-     {.box = {prefs_opts, G_N_ELEMENTS(prefs_opts), FALSE, FALSE}}},
+	{ WIDGET_BOX, NULL, NULL, NULL, NULL, FALSE,
+		{.box = { prefs_opts, G_N_ELEMENTS(prefs_opts), FALSE, FALSE}}},
 };
 
 static PreferencesWidget prefs_qual_row1[] = {
-    {WIDGET_BOX, "Resolution", NULL, NULL, NULL, FALSE,
-     {.box = {prefs_precision, G_N_ELEMENTS(prefs_precision), FALSE, TRUE}}},
-    {WIDGET_BOX, "Channels", NULL, NULL, NULL, FALSE,
-     {.box = {prefs_channels, G_N_ELEMENTS(prefs_channels), FALSE, TRUE}}},
+	{ WIDGET_BOX, "Resolution", NULL, NULL, NULL, FALSE,
+		{ .box = { prefs_precision, G_N_ELEMENTS(prefs_precision),
+			FALSE, TRUE }
+		}
+	},
+	{ WIDGET_BOX, "Channels", NULL, NULL, NULL, FALSE,
+		{ .box = { prefs_channels, G_N_ELEMENTS(prefs_channels),
+			FALSE, TRUE }
+		}
+	},
 };
 
 static PreferencesWidget prefs_qual_row2[] = {
-    {WIDGET_BOX, "Sampling rate", NULL, NULL, NULL, FALSE,
-     {.box = {prefs_frequency, G_N_ELEMENTS(prefs_frequency), FALSE, TRUE}}},
+	{ WIDGET_BOX, "Sampling rate", NULL, NULL, NULL, FALSE,
+		{ .box = { prefs_frequency, G_N_ELEMENTS(prefs_frequency),
+			FALSE, TRUE }
+		}
+	},
 };
 
 static PreferencesWidget prefs_qual_box1[] = {
-    {WIDGET_BOX, NULL, NULL, NULL, NULL, FALSE,
-     {.box = {prefs_qual_row1, G_N_ELEMENTS(prefs_qual_row1), TRUE, TRUE}}},
-    {WIDGET_BOX, NULL, NULL, NULL, NULL, FALSE,
-     {.box = {prefs_qual_row2, G_N_ELEMENTS(prefs_qual_row2), FALSE, TRUE}}},
+	{ WIDGET_BOX, NULL, NULL, NULL, NULL, FALSE,
+		{ .box = { prefs_qual_row1, G_N_ELEMENTS(prefs_qual_row1),
+			TRUE, TRUE }
+		}
+	},
+	{ WIDGET_BOX, NULL, NULL, NULL, NULL, FALSE,
+		{ .box = { prefs_qual_row2, G_N_ELEMENTS(prefs_qual_row2),
+			FALSE, TRUE }
+		}
+	},
 };
 
 static PreferencesWidget prefs_qual_tab[] = {
-    {WIDGET_BOX, NULL, NULL, NULL, NULL, FALSE,
-     {.box = {prefs_qual_box1, G_N_ELEMENTS(prefs_qual_box1), FALSE, TRUE}}},
+	{ WIDGET_BOX, NULL, NULL, NULL, NULL, FALSE,
+		{ .box = { prefs_qual_box1, G_N_ELEMENTS(prefs_qual_box1),
+			FALSE, TRUE }
+		}
+	},
 };
 
 static NotebookTab prefs_tabs[] = {
-    {"Quality", prefs_qual_tab, G_N_ELEMENTS(prefs_qual_tab)},
-    {"Options", prefs_opts_tab, G_N_ELEMENTS(prefs_opts_tab)},
+	{ "Quality", prefs_qual_tab, G_N_ELEMENTS(prefs_qual_tab) },
+	{ "Options", prefs_opts_tab, G_N_ELEMENTS(prefs_opts_tab) },
 };
 
 static PreferencesWidget prefs[] = {
-    {WIDGET_NOTEBOOK, NULL, NULL, NULL, NULL, FALSE,
-     {.notebook = {prefs_tabs, G_N_ELEMENTS(prefs_tabs)}},
-    },
+	{WIDGET_NOTEBOOK, NULL, NULL, NULL, NULL, FALSE,
+		{ .notebook = { prefs_tabs, G_N_ELEMENTS(prefs_tabs) } },
+	},
 };
 
 PluginPreferences xmp_aud_preferences = {
-    .domain = "xmpaudplugin",
-    .title = "Extended Module Player Configuration",
-    .prefs = prefs,
-    .n_prefs = G_N_ELEMENTS(prefs),
-    .type = PREFERENCES_WINDOW,
-    .init = configure_init,
-    .apply = configure_apply,
+	.domain = "xmpaudplugin",
+	.title = "Extended Module Player Configuration",
+	.prefs = prefs,
+	.n_prefs = G_N_ELEMENTS(prefs),
+	.type = PREFERENCES_WINDOW,
+	.init = configure_init,
+	.apply = configure_apply,
 };
 
-/* Filtering files by suffix is really stupid. */
 const gchar* const fmts[] = {
 	"xm", "mod", "m15", "it", "s2m", "s3m", "stm", "stx", "med", "dmf",
 	"mtm", "ice", "imf", "ptm", "mdl", "ult", "liq", "psm", "amf",
@@ -565,18 +609,17 @@ const gchar* const fmts[] = {
 	"zen", "crb", "tdd", "gmc", "gdm", "mdz", "xmz", "s3z", "j2b", NULL
 };
 
-AUD_INPUT_PLUGIN
-(
+AUD_INPUT_PLUGIN (
 	.name		= "XMP Plugin " VERSION,
 	.init		= init,
 	.about		= xmp_aud_about,
-    .settings	= &xmp_aud_preferences,
+	.settings	= &xmp_aud_preferences,
 	.play		= play,
 	.stop		= stop,
 	.pause		= mod_pause,
-    .probe_for_tuple = probe_for_tuple,
+	.probe_for_tuple = probe_for_tuple,
 	.is_our_file_from_vfs = is_our_file_from_vfs,
 	.cleanup	= cleanup,
 	.mseek		= mseek,
-	.extensions = fmts,
+	.extensions	= fmts,
 )
