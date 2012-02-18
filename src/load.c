f77a1660108c47d61952734e207759a945f326b4
Claudio Matsuoka <cmatsuoka@gmail.com>
2012-02-18 15:18:06 -0200

    Fix placement of string terminator
    Signed-off-by: Claudio Matsuoka <cmatsuoka@gmail.com>

--- src/load.c
+++ src/load.c
@@ -432,14 +432,15 @@ int xmp_test_module(xmp_context ctx, char *path, struct xmp_test_info *info)
 /* FIXME: ugly code, check allocations */
 static void split_name(char *s, char **d, char **b)
 {
-	char tmp, *div;
+	char *div;
+	int tmp;
 
 	_D("alloc dirname/basename");
 	if ((div = strrchr(s, '/'))) {
 		tmp = div - s + 1;
 		*d = malloc(tmp + 1);
 		memcpy(*d, s, tmp);
-		(*d)[tmp + 1] = 0;
+		(*d)[tmp] = 0;
 		*b = strdup(div + 1);
 	} else {
 		*d = strdup("");
