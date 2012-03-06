eb47f083a598c9ba4259b6fbd0e72337ca1366fb
Claudio Matsuoka <cmatsuoka@gmail.com>
2012-03-06 18:13:06 -0300

    Fix cutoff effect setting
    Signed-off-by: Claudio Matsuoka <cmatsuoka@gmail.com>

--- src/effects.c
+++ src/effects.c
@@ -222,7 +222,6 @@ void process_fx(struct context_data *ctx, int chn, uint8 note, uint8 fxt,
 		break;
 
 	case FX_SETPAN:		/* Set pan */
-		SET(NEW_PAN);
 		xc->pan = fxp;
 		break;
 	case FX_OFFSET:		/* Set sample offset */
