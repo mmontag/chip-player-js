79115bcc13c370432f49b4e3af3c5739b4839ae0
Claudio Matsuoka <cmatsuoka@gmail.com>
2012-03-24 10:44:36 -0300

    [test] Add arc decompression test
    Signed-off-by: Claudio Matsuoka <cmatsuoka@gmail.com>

---
+++ test/test_depack_arc.c
@@ -0,0 +1,33 @@
+#include "test.h"
+
+int decrunch_arc(FILE *f, FILE *fo);
+
+
+TEST(test_depack_arc)
+{
+	FILE *f, *fo;
+	int ret;
+	struct stat st;
+
+	f = fopen("data/040810", "rb");
+	fail_unless(f != NULL, "can't open data file");
+
+	fo = fopen(TMP_FILE, "wb");
+	fail_unless(fo != NULL, "can't open output file");
+
+	ret = decrunch_arc(f, fo);
+	fail_unless(ret == 0, "decompression fail");
+
+	fclose(fo);
+	fclose(f);
+
+	f = fopen(TMP_FILE, "rb");
+	fstat(fileno(f), &st);
+	fail_unless(st.st_size == 7732, "decompression size error");
+
+	ret = check_md5(TMP_FILE, "b868aef5b8843c0754284561cda5aba9");
+	fail_unless(ret == 0, "MD5 error");
+
+	fclose(f);
+}
+END_TEST
