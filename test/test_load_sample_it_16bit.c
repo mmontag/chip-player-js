5b47b46aab7cd9037d14c130d8691f74e8223b47
Claudio Matsuoka <cmatsuoka@gmail.com>
2012-03-27 20:22:24 -0300

    [test] Add IT sample decompression tests
    Signed-off-by: Claudio Matsuoka <cmatsuoka@gmail.com>

---
+++ test/test_load_sample_it_16bit.c
@@ -0,0 +1,31 @@
+#include "test.h"
+
+int itsex_decompress16(FILE *module, void *dst, int len, char it215);
+
+
+TEST(test_load_sample_it_16bit)
+{
+	FILE *f, *fo;
+	int ret;
+	char dest[10000];
+
+	f = fopen("data/it-sample-16bit.raw", "rb");
+	fail_unless(f != NULL, "can't open data file");
+
+	fo = fopen(TMP_FILE, "wb");
+	fail_unless(fo != NULL, "can't open output file");
+
+	ret = itsex_decompress16(f, dest, 4646, 0);
+	fail_unless(ret == 1, "decompression fail");
+	fwrite(dest, 1, 9292, fo);
+
+	fclose(fo);
+	fclose(f);
+
+	f = fopen(TMP_FILE, "rb");
+	ret = check_md5(TMP_FILE, "1e2395653f9bd7838006572d8fcdb646");
+	fail_unless(ret == 0, "MD5 error");
+
+	fclose(f);
+}
+END_TEST
---
+++ test/test_load_sample_it_8bit.c
@@ -0,0 +1,31 @@
+#include "test.h"
+
+int itsex_decompress8(FILE *module, void *dst, int len, char it215);
+
+
+TEST(test_load_sample_it_8bit)
+{
+	FILE *f, *fo;
+	int ret;
+	char dest[10000];
+
+	f = fopen("data/it-sample-8bit.raw", "rb");
+	fail_unless(f != NULL, "can't open data file");
+
+	fo = fopen(TMP_FILE, "wb");
+	fail_unless(fo != NULL, "can't open output file");
+
+	ret = itsex_decompress8(f, dest, 4879, 0);
+	fail_unless(ret == 1, "decompression fail");
+	fwrite(dest, 1, 4879, fo);
+
+	fclose(fo);
+	fclose(f);
+
+	f = fopen(TMP_FILE, "rb");
+	ret = check_md5(TMP_FILE, "299c9144ae2349b90b430aafde8d799a");
+	fail_unless(ret == 0, "MD5 error");
+
+	fclose(f);
+}
+END_TEST
