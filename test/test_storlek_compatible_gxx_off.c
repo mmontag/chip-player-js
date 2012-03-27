8d30a4f37a2c8bc6ffb1794e085dfaeaf45062ad
Claudio Matsuoka <cmatsuoka@gmail.com>
2012-03-27 13:11:15 -0300

    [test] Add storlek tests 03 and 04
    Signed-off-by: Claudio Matsuoka <cmatsuoka@gmail.com>

---
+++ test/test_storlek_compatible_gxx_off.c
@@ -0,0 +1,50 @@
+#include "test.h"
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
+/*
+Compatible Gxx off
+
+Impulse Tracker links the effect memories for Exx, Fxx, and Gxx together if
+"Compatible Gxx" in NOT enabled in the file header. In other formats,
+portamento to note is entirely separate from pitch slide up/down. Several
+players that claim to be IT-compatible do not check this flag, and always store
+the last Gxx value separately.
+
+When this test is played correctly, the first note will bend up, down, and back
+up again, and the final set of notes should only slide partway down. Players
+which do not correctly handle the Compatible Gxx flag will not perform the
+final pitch slide in the first part, or will "snap" the final notes.
+*/
+
+TEST(test_storlek_compatible_gxx_off)
+{
+	xmp_context opaque;
+	struct xmp_module_info info;
+	struct xmp_channel_info *ci = &info.channel_info[0];
+	int time, row, frame, period, volume;
+	FILE *f;
+
+	f = fopen("data/storlek_03.data", "r");
+
+	opaque = xmp_create_context();
+	xmp_load_module(opaque, "data/storlek_03.it");
+	xmp_player_start(opaque, 44100, 0);
+
+	while (1) {
+
+		xmp_player_frame(opaque);
+		xmp_player_get_info(opaque, &info);
+		if (info.loop_count > 0)
+			break;
+
+		fscanf(f, "%d %d %d %d %d", &time, &row, &frame, &period, &volume);
+		fail_unless(info.time  == time,   "time mismatch");
+		fail_unless(info.row   == row,    "row mismatch");
+		fail_unless(info.frame == frame,  "frame mismatch");
+		fail_unless(ci->period == period, "period mismatch");
+		fail_unless(ci->volume == volume, "volume mismatch");
+	}
+}
+END_TEST
---
+++ test/test_storlek_compatible_gxx_on.c
@@ -0,0 +1,45 @@
+#include "test.h"
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
+/*
+Compatible Gxx on
+
+If this test is played correctly, the first note will slide up and back down
+once, and the final series should play four distinct notes. If the first note
+slides up again, either (a) the player is testing the flag incorrectly (Gxx
+memory is only linked if the flag is not set), or (b) the effect memory values
+are not set to zero at start of playback.
+*/
+
+TEST(test_storlek_compatible_gxx_on)
+{
+	xmp_context opaque;
+	struct xmp_module_info info;
+	struct xmp_channel_info *ci = &info.channel_info[0];
+	int time, row, frame, period, volume;
+	FILE *f;
+
+	f = fopen("data/storlek_04.data", "r");
+
+	opaque = xmp_create_context();
+	xmp_load_module(opaque, "data/storlek_04.it");
+	xmp_player_start(opaque, 44100, 0);
+
+	while (1) {
+
+		xmp_player_frame(opaque);
+		xmp_player_get_info(opaque, &info);
+		if (info.loop_count > 0)
+			break;
+
+		fscanf(f, "%d %d %d %d %d", &time, &row, &frame, &period, &volume);
+		fail_unless(info.time  == time,   "time mismatch");
+		fail_unless(info.row   == row,    "row mismatch");
+		fail_unless(info.frame == frame,  "frame mismatch");
+		fail_unless(ci->period == period, "period mismatch");
+		fail_unless(ci->volume == volume, "volume mismatch");
+	}
+}
+END_TEST
