6ca2a9bb77e75e76ddea665a99a7c2d79c1b5e9b
Claudio Matsuoka <cmatsuoka@gmail.com>
2012-03-28 08:25:49 -0300

    [test] Add storlek test 10
    Signed-off-by: Claudio Matsuoka <cmatsuoka@gmail.com>

--- test/gen_replay_data.py
+++ test/gen_replay_data.py
@@ -2,27 +2,39 @@
 
 import sys, os, xmp
 
-if len(sys.argv) < 2:
-	print "Usage: %s <module>" % (os.path.basename(sys.argv[0]))
+argc = len(sys.argv)
+argv = sys.argv
+
+if argc < 2:
+	print "Usage: %s <module> [channels]" % (os.path.basename(argv[0]))
 	sys.exit(1)
 
 info = xmp.struct_xmp_module_info()
 
 x = xmp.Xmp()
 try:
-	x.loadModule(sys.argv[1])
+	x.loadModule(argv[1])
 except IOError as (errno, strerror):
-	sys.stderr.write("%s: %s\n" % (sys.argv[1], strerror))
+	sys.stderr.write("%s: %s\n" % (argv[1], strerror))
 	sys.exit(1)
 
 x.playerStart(44100, 0)
+x.getInfo(info)
+
+channels = []
+
+if argc > 2:
+	for i in range(argc - 2):
+		channels.append(int(argv[i + 2]))
+else:
+	channels = range(info.mod[0].chn)
 
 while x.playerFrame():
 	x.getInfo(info)
 	if info.loop_count > 0:
 		break
 
-	for i in range(info.mod[0].chn):
+	for i in channels:
 		ci = info.channel_info[i]
 		print "%d %d %d %d %d %d %d" % (info.time, info.row,
 			info.frame, i, ci.period, ci.volume, ci.instrument)
