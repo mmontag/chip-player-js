ea6531510387e683d49b2f9a317cebea831b3840
Claudio Matsuoka <cmatsuoka@gmail.com>
2012-08-17 10:33:57 -0300

    Update YM2149 license to LGPL
    The YM2149 author, Arnaud Carre, authorized relicensing his code under
    the LGPL for inclusion in libxmp.
    
    Signed-off-by: Claudio Matsuoka <cmatsuoka@gmail.com>

--- src/ym2149.h
+++ src/ym2149.h
@@ -11,21 +11,22 @@
 
 /*-----------------------------------------------------------------------------
 
-	This file is part of ST-Sound
+	Copyright (C) 1995-2012 Arnaud Carre
 
-	ST-Sound is free software; you can redistribute it and/or modify
-	it under the terms of the GNU General Public License as published by
-	the Free Software Foundation; either version 2 of the License, or
-	(at your option) any later version.
+	The YM2149 emulator is free software; you can redistribute it and/or
+	modify it under the terms of the GNU Lesser General Public
+	License as published by the Free Software Foundation; either
+	version 2.1 of the License, or (at your option) any later version.
 
-	ST-Sound is distributed in the hope that it will be useful,
+	The YM2149 emulator is distributed in the hope that it will be useful,
 	but WITHOUT ANY WARRANTY; without even the implied warranty of
-	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-	GNU General Public License for more details.
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+	Lesser General Public License for more details.
 
-	You should have received a copy of the GNU General Public License
-	along with ST-Sound; if not, write to the Free Software
-	Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+	You should have received a copy of the GNU Lesser General Public
+	License along with this library; if not, write to the Free Software
+	Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+	02110-1301  USA
 
 -----------------------------------------------------------------------------*/
 
