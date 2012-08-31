17a4edd677f7b2f15ef0bcc427a3de35aceca417
Claudio Matsuoka <cmatsuoka@gmail.com>
2012-08-30 21:44:20 -0300

    Fix mingw build
    Also fix compiler flags to build code as reentrant. All 130 tests pass
    in windows.
    
    Signed-off-by: Claudio Matsuoka <cmatsuoka@gmail.com>

--- src/oxm.c
+++ src/oxm.c
@@ -9,10 +9,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <sys/wait.h>
-#include <unistd.h>
 #include "vorbis.h"
 #include "common.h"
 
