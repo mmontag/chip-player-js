181d579bad0cb91c5880c1353d58a8a2749efb7a
Claudio Matsuoka <cmatsuoka@gmail.com>
2012-09-01 08:59:12 -0300

    [test] Add missing data file
    Signed-off-by: Claudio Matsuoka <cmatsuoka@gmail.com>

--- test/test_player_scan.c
+++ test/test_player_scan.c
@@ -4,9 +4,14 @@ TEST(test_player_scan)
 {
 	xmp_context opaque;
 	struct xmp_module_info info;
+	int ret;
 
 	opaque = xmp_create_context();
-	xmp_load_module(opaque, "data/ode2ptk.mod");
+	fail_unless(opaque != NULL, "can't create context");
+
+	ret = xmp_load_module(opaque, "data/ode2ptk.mod");
+	fail_unless(ret == 0, "can't load module");
+
 	xmp_player_start(opaque, 44100, 0);
 	xmp_player_get_info(opaque, &info);
 	fail_unless(info.total_time == 85472, "incorrect total time");
