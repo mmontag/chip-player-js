9e843553e3639ee41f60e2d8e2e53d820802b8ac
Claudio Matsuoka <cmatsuoka@gmail.com>
2012-09-07 20:00:43 -0300

    [test] Test all possible note periods
    Signed-off-by: Claudio Matsuoka <cmatsuoka@gmail.com>

--- test/test_player_period_amiga.c
+++ test/test_player_period_amiga.c
@@ -5,7 +5,11 @@ TEST(test_player_period_amiga)
 	xmp_context opaque;
 	struct context_data *ctx;
 	struct xmp_module_info info;
-	int i;
+	int i, p0, p1;
+	FILE *f;
+
+	f = fopen("data/periods.data", "r");
+	fail_unless(f != NULL, "can't open data file");
 
 	opaque = xmp_create_context();
 	ctx = (struct context_data *)opaque;
@@ -15,45 +19,21 @@ TEST(test_player_period_amiga)
 	xmp_player_start(opaque, 44100, 0);
 
 	/* test note periods */
-	new_event(ctx, 0, 0, 0, 20, 1, 0, 0x0f, 1, 0, 0);
-	new_event(ctx, 0, 1, 0, 30, 1, 0, 0x0f, 1, 0, 0);
-	new_event(ctx, 0, 2, 0, 50, 1, 0, 0x0f, 1, 0, 0);
-	new_event(ctx, 0, 3, 0, 60, 1, 0, 0x0f, 1, 0, 0);
-	new_event(ctx, 0, 4, 0, 70, 1, 0, 0x0f, 1, 0, 0);
-	new_event(ctx, 0, 5, 0, 80, 1, 0, 0x0f, 1, 0, 0);
-	new_event(ctx, 0, 6, 0, 90, 1, 0, 0x0f, 1, 0, 0);
-	new_event(ctx, 0, 7, 0, 100, 1, 0, 0x0f, 1, 0, 0);
+	for (i = 0; i < 60; i++) {
+		new_event(ctx, 0, i, 0, i, 1, 0, 0x0f, 1, 0, 0);
+		new_event(ctx, 0, i, 1, 60 + i, 1, 0, 0, 0, 0, 0);
+	}
 
-	xmp_player_frame(opaque);
-	xmp_player_get_info(opaque, &info);
-	fail_unless(info.channel_info[0].period == 18718000, "Bad period");
-	
-	xmp_player_frame(opaque);
-	xmp_player_get_info(opaque, &info);
-	fail_unless(info.channel_info[0].period == 10505122, "Bad period");
-	
-	xmp_player_frame(opaque);
-	xmp_player_get_info(opaque, &info);
-	fail_unless(info.channel_info[0].period == 3308906, "Bad period");
-	
-	xmp_player_frame(opaque);
-	xmp_player_get_info(opaque, &info);
-	fail_unless(info.channel_info[0].period == 1857060, "Bad period");
-	
-	xmp_player_frame(opaque);
-	xmp_player_get_info(opaque, &info);
-	fail_unless(info.channel_info[0].period == 1042240, "Bad period");
-	
-	xmp_player_frame(opaque);
-	xmp_player_get_info(opaque, &info);
-	fail_unless(info.channel_info[0].period == 584937, "Bad period");
-	
-	xmp_player_frame(opaque);
-	xmp_player_get_info(opaque, &info);
-	fail_unless(info.channel_info[0].period == 328285, "Bad period");
-	
-	xmp_player_frame(opaque);
-	xmp_player_get_info(opaque, &info);
-	fail_unless(info.channel_info[0].period == 184243, "Bad period");
+	for (i = 0; i < 60; i++) {
+		xmp_player_frame(opaque);
+		xmp_player_get_info(opaque, &info);
+		fscanf(f, "%d %d", &p0, &p1);
+		fail_unless(info.channel_info[0].period == p0, "Bad period");
+		fail_unless(info.channel_info[1].period == p1, "Bad period");
+	}
+
+	fscanf(f, "%d %d", &p0, &p1);
+	fail_unless(feof(f), "not end of data file");
+
 }
 END_TEST
