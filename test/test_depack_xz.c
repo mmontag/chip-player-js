548557d0e5706d100c5696ee5bbacc46e3b81750
Claudio Matsuoka <cmatsuoka@gmail.com>
2012-09-07 20:04:28 -0300

    [test] Test xz depacking through load API call
    Signed-off-by: Claudio Matsuoka <cmatsuoka@gmail.com>

--- test/test_depack_xz.c
+++ test/test_depack_xz.c
@@ -1,36 +1,21 @@
 #include "test.h"
-#include "unxz/xz.h"
-
-int decrunch_xz(FILE *f, FILE *fo);
 
 
 TEST(test_depack_xz)
 {
-	FILE *f, *fo;
+	xmp_context c;
+	struct xmp_module_info info;
 	int ret;
-	struct stat st;
 
-	xz_crc32_init();
+	c = xmp_create_context();
+	fail_unless(c != NULL, "can't create context");
+	ret = xmp_load_module(c, "data/xzdata");
+	fail_unless(ret == 0, "can't load module");
 
-	f = fopen("data/xzdata", "rb");
-	fail_unless(f != NULL, "can't open data file");
+	xmp_player_start(c, 44100, 0);
+	xmp_player_get_info(c, &info);
 
-	fo = fopen(TMP_FILE, "wb");
-	fail_unless(fo != NULL, "can't open output file");
-
-	ret = decrunch_xz(f, fo);
-	fail_unless(ret == 0, "decompression fail");
-
-	fclose(fo);
-	fclose(f);
-
-	f = fopen(TMP_FILE, "rb");
-	fstat(fileno(f), &st);
-	fail_unless(st.st_size == 14176, "decompression size error");
-
-	ret = check_md5(TMP_FILE, "37b8afe62ec42a47b1237b794193e785");
+	ret = compare_md5(info.mod->digest, "37b8afe62ec42a47b1237b794193e785");
 	fail_unless(ret == 0, "MD5 error");
-
-	fclose(f);
 }
 END_TEST
