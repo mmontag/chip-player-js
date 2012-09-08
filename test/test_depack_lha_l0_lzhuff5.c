1c0c0f33e08555417871b76f9a172b39e6574c4c
Claudio Matsuoka <cmatsuoka@gmail.com>
2012-09-08 08:56:47 -0300

    [test] Test lha depacking using load API call
    Signed-off-by: Claudio Matsuoka <cmatsuoka@gmail.com>

--- test/test_depack_lha_l0_lzhuff5.c
+++ test/test_depack_lha_l0_lzhuff5.c
@@ -1,35 +1,21 @@
 #include "test.h"
 
-int decrunch_lha(FILE *f, FILE *fo);
-
 
 TEST(test_depack_lha_l0_lzhuff5)
 {
-	FILE *f, *fo;
+	xmp_context c;
+	struct xmp_module_info info;
 	int ret;
-	struct stat st;
 
-	f = fopen("data/l0_lzhuff5", "rb");
-	fail_unless(f != NULL, "can't open data file");
+	c = xmp_create_context();
+	fail_unless(c != NULL, "can't create context");
+	ret = xmp_load_module(c, "data/l0_lzhuff5");
+	fail_unless(ret == 0, "can't load module");
 
-	fo = fopen(TMP_FILE, "wb");
-	fail_unless(fo != NULL, "can't open output file");
+	xmp_player_start(c, 44100, 0);
+	xmp_player_get_info(c, &info);
 
-	ret = decrunch_lha(f, fo);
-	fail_unless(ret == 0, "decompression fail");
-
-	fclose(fo);
-	fclose(f);
-
-	f = fopen(TMP_FILE, "rb");
-	fstat(fileno(f), &st);
-	fail_unless(st.st_size == 46056, "decompression size error");
-
-	ret = check_md5(TMP_FILE, "d62117b9d24b152b225bdb7be24d5c5c");
+	ret = compare_md5(info.mod->md5, "d62117b9d24b152b225bdb7be24d5c5c");
 	fail_unless(ret == 0, "MD5 error");
-
-	fclose(f);
 }
 END_TEST
-
-
--- test/test_depack_lha_l1_lzhuff7.c
+++ test/test_depack_lha_l1_lzhuff7.c
@@ -1,33 +1,21 @@
 #include "test.h"
 
-int decrunch_lha(FILE *f, FILE *fo);
-
 
 TEST(test_depack_lha_l1_lzhuff7)
 {
-	FILE *f, *fo;
+	xmp_context c;
+	struct xmp_module_info info;
 	int ret;
-	struct stat st;
 
-	f = fopen("data/l1_lzhuff7", "rb");
-	fail_unless(f != NULL, "can't open data file");
+	c = xmp_create_context();
+	fail_unless(c != NULL, "can't create context");
+	ret = xmp_load_module(c, "data/l1_lzhuff7");
+	fail_unless(ret == 0, "can't load module");
 
-	fo = fopen(TMP_FILE, "wb");
-	fail_unless(fo != NULL, "can't open output file");
+	xmp_player_start(c, 44100, 0);
+	xmp_player_get_info(c, &info);
 
-	ret = decrunch_lha(f, fo);
-	fail_unless(ret == 0, "decompression fail");
-
-	fclose(fo);
-	fclose(f);
-
-	f = fopen(TMP_FILE, "rb");
-	fstat(fileno(f), &st);
-	fail_unless(st.st_size == 8378, "decompression size error");
-
-	ret = check_md5(TMP_FILE, "c993a848f57227660f8b10db1d4d874f");
+	ret = compare_md5(info.mod->md5, "c993a848f57227660f8b10db1d4d874f");
 	fail_unless(ret == 0, "MD5 error");
-
-	fclose(f);
 }
 END_TEST
