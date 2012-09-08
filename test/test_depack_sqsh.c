6c493c070d4f40fad99bd2a13313dffe49571c05
Claudio Matsuoka <cmatsuoka@gmail.com>
2012-09-08 08:33:57 -0300

    [test] Test sqsh depacking using load API call
    Signed-off-by: Claudio Matsuoka <cmatsuoka@gmail.com>

--- test/test_depack_sqsh.c
+++ test/test_depack_sqsh.c
@@ -1,33 +1,21 @@
 #include "test.h"
 
-int decrunch_sqsh(FILE *f, FILE *fo);
-
 
 TEST(test_depack_sqsh)
 {
-	FILE *f, *fo;
+	xmp_context c;
+	struct xmp_module_info info;
 	int ret;
-	struct stat st;
 
-	f = fopen("data/sqsh_random1", "rb");
-	fail_unless(f != NULL, "can't open data file");
+	c = xmp_create_context();
+	fail_unless(c != NULL, "can't create context");
+	ret = xmp_load_module(c, "data/PRU2.PDX-Perihelion");
+	fail_unless(ret == 0, "can't load module");
 
-	fo = fopen(TMP_FILE, "wb");
-	fail_unless(fo != NULL, "can't open output file");
+	xmp_player_start(c, 44100, 0);
+	xmp_player_get_info(c, &info);
 
-	ret = decrunch_sqsh(f, fo);
-	fail_unless(ret == 0, "decompression fail");
-
-	fclose(fo);
-	fclose(f);
-
-	f = fopen(TMP_FILE, "rb");
-	fstat(fileno(f), &st);
-	fail_unless(st.st_size == 16384, "decompression size error");
-
-	ret = check_md5(TMP_FILE, "5f7c2705f9257eb65e38edbc08028375");
+	ret = compare_md5(info.mod->digest, "70e931a4c9835cd60493ae3d22b0cea621");
 	fail_unless(ret == 0, "MD5 error");
-
-	fclose(f);
 }
 END_TEST
