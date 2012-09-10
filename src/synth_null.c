a8d18951e15a2ae90c4f718569414ba0373d353f
Claudio Matsuoka <cmatsuoka@gmail.com>
2012-09-10 18:25:37 -0300

    Comment out usused synth_null code
    Signed-off-by: Claudio Matsuoka <cmatsuoka@gmail.com>

--- src/synth_null.c
+++ src/synth_null.c
@@ -10,21 +10,6 @@
 #include "common.h"
 #include "synth.h"
 
-static void synth_setpatch(struct context_data *ctx, int c, uint8 *data)
-{
-}
-
-static void synth_setnote(struct context_data *ctx, int c, int note, int bend)
-{
-}
-
-static void synth_setvol(struct context_data *ctx, int c, int vol)
-{
-}
-
-static void synth_seteffect(struct context_data *ctx, int c, int type, int val)
-{
-}
 
 static int synth_init(struct context_data *ctx, int freq)
 {
@@ -41,17 +26,8 @@ static int synth_reset(struct context_data *ctx)
 	return 0;
 }
 
-static void synth_mixer(struct context_data *ctx, int *tmp_bk, int count, int vl, int vr, int stereo)
-{
-}
-
 const struct synth_info synth_null = {
 	synth_init,
 	synth_deinit,
 	synth_reset,
-	synth_setpatch,
-	synth_setnote,
-	synth_setvol,
-	synth_seteffect,
-	synth_mixer
 };
