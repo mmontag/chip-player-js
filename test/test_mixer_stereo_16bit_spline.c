aea297a5f2d6edd08c2c8780c1b0f6d8daf07b3a
Claudio Matsuoka <cmatsuoka@gmail.com>
2012-09-13 15:33:05 -0300

    [test] Add cubic spline interpolation tests
    Signed-off-by: Claudio Matsuoka <cmatsuoka@gmail.com>

---
+++ test/test_mixer_stereo_16bit_spline.c
@@ -0,0 +1,41 @@
+#include "test.h"
+
+TEST(test_mixer_stereo_16bit_spline)
+{
+	xmp_context opaque;
+	struct context_data *ctx;
+	struct mixer_data *s;
+	struct xmp_module_info info;
+	FILE *f;
+	int i, j, k, val;
+
+	f = fopen("data/mixer_16bit_spline.data", "r");
+
+	opaque = xmp_create_context();
+	ctx = (struct context_data *)opaque;
+	s = &ctx->s;
+
+	xmp_load_module(opaque, "data/test.xm");
+
+	for (i = 0; i < 5; i++) {
+		new_event(ctx, 0, i, 0, 20 + i * 20, 2, 0, 0x0f, 2, 0, 0);
+	}
+
+	xmp_player_start(opaque, 8000, 0);
+	xmp_mixer_set(opaque, XMP_MIXER_INTERP, XMP_INTERP_SPLINE);
+
+	for (i = 0; i < 10; i++) {
+		xmp_player_frame(opaque);
+		xmp_player_get_info(opaque, &info);
+		for (k = j = 0; j < info.buffer_size / 4; j++) {
+			fscanf(f, "%d", &val);
+			fail_unless(s->buf32[k++] == val, "mixing error L");
+			fail_unless(s->buf32[k++] == val, "mixing error R");
+		}
+	}
+
+	xmp_player_end(opaque);
+	xmp_release_module(opaque);
+	xmp_free_context(opaque);
+}
+END_TEST
