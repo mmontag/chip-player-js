d781dddf92e605136037c50bb843f10d95da4206
Claudio Matsuoka <cmatsuoka@gmail.com>
2013-04-08 15:16:32 -0300

    Protoize md5 functions
    Add proper prototypes to MD5 calls. (reported by Douglas Carmichael)
    
    Signed-off-by: Claudio Matsuoka <cmatsuoka@gmail.com>

--- src/md5.h
+++ src/md5.h
@@ -48,9 +48,9 @@ typedef struct {
   unsigned char digest[16];     /* actual digest after MD5Final call */
 } MD5_CTX;
 
-void MD5Init ();
-void MD5Update ();
-void MD5Final ();
+void MD5Init (MD5_CTX *);
+void MD5Update (MD5_CTX *, unsigned char *, int);
+void MD5Final (MD5_CTX *);
 
 /*
  **********************************************************************
