aa60a45a675ae99a4a74cb2522dad6b65ddf936d
Claudio Matsuoka <cmatsuoka@gmail.com>
2013-04-13 13:56:07 -0300

    Don't use Microsoft runtime when building with Cygwin
    If building using Cygwin, use the Cygwin runtime to build libraries
    instead of MSCVRT. This prevents linker errors such as __imp__ symbols
    not found.
    
    Signed-off-by: Claudio Matsuoka <cmatsuoka@gmail.com>

--- include/xmp.h
+++ include/xmp.h
@@ -11,7 +11,7 @@ extern "C" {
 #define XMP_VER_MINOR 0
 #define XMP_VER_RELEASE 4
 
-#if defined(_WIN32) || defined(__CYGWIN__)
+#if defined(_WIN32) && !defined(__CYGWIN__)
 # ifdef BUILDING_DLL
 #  define EXPORT __declspec(dllexport)
 # else
