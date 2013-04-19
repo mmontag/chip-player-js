5c1ca92a768c9cd894a3f074d6d461f117ec4722
Claudio Matsuoka <cmatsuoka@gmail.com>
2013-04-19 20:34:17 -0300

    [test] Change S404 test to use a valid module file
    Signed-off-by: Claudio Matsuoka <cmatsuoka@gmail.com>

--- test/test_depack_s404.c
+++ test/test_depack_s404.c
@@ -1,33 +1,21 @@
 #include "test.h"
 
-int decrunch_s404(FILE *f, FILE *fo);
-
 
 TEST(test_depack_s404)
 {
-	FILE *f, *fo;
+	xmp_context c;
+	struct xmp_module_info info;
 	int ret;
-	struct stat st;
 
-	f = fopen("data/aon.wingsofdeath1.stc", "rb");
-	fail_unless(f != NULL, "can't open data file");
+	c = xmp_create_context();
+	fail_unless(c != NULL, "can't create context");
+	ret = xmp_load_module(c, "data/synth-a-.med.stc");
+	fail_unless(ret == 0, "can't load module");
 
-	fo = fopen(TMP_FILE, "wb");
-	fail_unless(fo != NULL, "can't open output file");
+	xmp_start_player(c, 44100, 0);
+	xmp_get_module_info(c, &info);
 
-	ret = decrunch_s404(f, fo);
-	fail_unless(ret == 0, "decompression fail");
-
-	fclose(fo);
-	fclose(f);
-
-	f = fopen(TMP_FILE, "rb");
-	fstat(fileno(f), &st);
-	fail_unless(st.st_size == 31360, "decompression size error");
-
-	ret = check_md5(TMP_FILE, "ab45d5c5427617d6ee6e4260710edaf1");
+	ret = compare_md5(info.md5, "a9405964cc276b316bba6976f7539f38");
 	fail_unless(ret == 0, "MD5 error");
-
-	fclose(f);
 }
 END_TEST
