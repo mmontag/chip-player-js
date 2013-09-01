f74c7cda32b4302a5889db33fd5fc95d4611b089
Claudio Matsuoka <cmatsuoka@gmail.com>
2013-09-01 08:55:03 -0300

    [test] Update python script to latest API
    Signed-off-by: Claudio Matsuoka <cmatsuoka@gmail.com>

--- test-dev/gen_replay_data.py
+++ test-dev/gen_replay_data.py
@@ -6,7 +6,7 @@ import os
 ROOT_PATH = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 sys.path.append(ROOT_PATH + "/../libxmp-python")
 
-import pyxmp
+from pyxmp import Xmp
 
 argc = len(sys.argv)
 argv = sys.argv
@@ -15,10 +15,7 @@ if argc < 2:
 	print "Usage: %s <module> [channels]" % (os.path.basename(argv[0]))
 	sys.exit(1)
 
-mi = pyxmp.struct_xmp_module_info()
-fi = pyxmp.struct_xmp_frame_info()
-
-xmp = pyxmp.Xmp()
+xmp = Xmp()
 try:
 	xmp.load_module(argv[1])
 except IOError as (errno, strerror):
@@ -26,7 +23,8 @@ except IOError as (errno, strerror):
 	sys.exit(1)
 
 xmp.start_player(8000, 0)
-xmp.get_module_info(mi)
+mi = xmp.get_module_info()
+fi = Xmp.frame_info()
 
 channels = []
 
