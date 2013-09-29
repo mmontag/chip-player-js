94ef43cb797027d4f1fbdcc0035828c7f77f0f2d
Claudio Matsuoka <cmatsuoka@gmail.com>
2013-09-29 08:46:34 -0300

    Add Android NDK build files
    Signed-off-by: Claudio Matsuoka <cmatsuoka@gmail.com>

---
+++ jni/Android.mk
@@ -0,0 +1,25 @@
+LOCAL_PATH	:= $(call my-dir)/..
+#LOCAL_ARM_MODE	:= arm
+
+include $(CLEAR_VARS)
+
+include $(LOCAL_PATH)/src/Makefile
+include $(LOCAL_PATH)/src/loaders/Makefile
+include $(LOCAL_PATH)/src/loaders/prowizard/Makefile
+include $(LOCAL_PATH)/src/depackers/Makefile
+
+SRC_SOURCES	:= $(addprefix src/,$(SRC_OBJS))
+LOADERS_SOURCES := $(addprefix src/loaders/,$(LOADERS_OBJS))
+PROWIZ_SOURCES	:= $(addprefix src/loaders/prowizard/,$(PROWIZ_OBJS))
+DEPACKERS_SOURCES := $(addprefix src/depackers/,$(DEPACKERS_OBJS))
+
+LOCAL_MODULE    := xmp
+LOCAL_CFLAGS	:= -O3 -DHAVE_MKSTEMP -DHAVE_FNMATCH -I$(LOCAL_PATH)/include \
+		   -I$(LOCAL_PATH)/src
+LOCAL_LDLIBS	:= -Lbuild/platforms/android-3/arch-arm/usr/lib -llog
+LOCAL_SRC_FILES := $(SRC_SOURCES:.o=.c.arm) \
+		   $(LOADERS_SOURCES:.o=.c) \
+		   $(PROWIZ_SOURCES:.o=.c) \
+		   $(DEPACKERS_SOURCES:.o=.c)
+
+include $(BUILD_SHARED_LIBRARY)
