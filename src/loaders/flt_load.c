464c68017ca38615557528ec9a6a57b5f4973da2
Claudio Matsuoka <cmatsuoka@gmail.com>
2013-09-30 15:16:30 -0300

    Handle memory allocation errors in Startrekker loader
    Signed-off-by: Claudio Matsuoka <cmatsuoka@gmail.com>

--- src/loaders/flt_load.c
+++ src/loaders/flt_load.c
@@ -351,10 +351,15 @@ static int flt_load(struct module_data *m, HIO_HANDLE *f, const int start)
     set_type(m, "%s (%4.4s)", tracker, mh.magic);
     MODULE_INFO();
 
-    INSTRUMENT_INIT();
+    if (instrument_init(mod) < 0)
+	return -1;
 
     for (i = 0; i < mod->ins; i++) {
-	mod->xxi[i].sub = calloc(sizeof (struct xmp_subinstrument), 1);
+	mod->xxi[i].nsm = 1;
+
+	if (subinstrument_alloc(mod, i) < 0)
+	    return -1;
+
 	mod->xxs[i].len = 2 * mh.ins[i].size;
 	mod->xxs[i].lps = 2 * mh.ins[i].loop_start;
 	mod->xxs[i].lpe = mod->xxs[i].lps + 2 * mh.ins[i].loop_size;
@@ -363,7 +368,6 @@ static int flt_load(struct module_data *m, HIO_HANDLE *f, const int start)
 	mod->xxi[i].sub[0].vol = mh.ins[i].volume;
 	mod->xxi[i].sub[0].pan = 0x80;
 	mod->xxi[i].sub[0].sid = i;
-	mod->xxi[i].nsm = !!(mod->xxs[i].len);
 	mod->xxi[i].rls = 0xfff;
 
 	copy_adjust(mod->xxi[i].name, mh.ins[i].name, 22);
@@ -379,7 +383,8 @@ static int flt_load(struct module_data *m, HIO_HANDLE *f, const int start)
 	}
     }
 
-    PATTERN_INIT();
+    if (pattern_init(mod) < 0)
+	return -1;
 
     /* Load and convert patterns */
     D_(D_INFO "Stored patterns: %d", mod->pat);
@@ -396,9 +401,15 @@ static int flt_load(struct module_data *m, HIO_HANDLE *f, const int start)
      * portamento command, that would be hard to patch).
      */
     for (i = 0; i < mod->pat; i++) {
-	PATTERN_ALLOC(i);
+
+	if (pattern_alloc(mod, i) < 0)
+	    return -1;
+
 	mod->xxp[i]->rows = 64;
-	TRACK_ALLOC(i);
+
+	if (pattern_tracks_alloc(mod, i) < 0)
+	    return -1;
+
 	for (j = 0; j < (64 * 4); j++) {
 	    event = &EVENT(i, j % 4, j / 4);
 	    hio_read(mod_event, 1, 4, f);
