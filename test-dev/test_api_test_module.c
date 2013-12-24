1cbd13cc382dad999dcfca50d05fce09f5c370e7
Claudio Matsuoka <cmatsuoka@gmail.com>
2013-12-24 12:05:20 -0200

    [test] Update tests for vitamin-caig's changes
    Signed-off-by: Claudio Matsuoka <cmatsuoka@gmail.com>

--- test-dev/test_api_test_module.c
+++ test-dev/test_api_test_module.c
@@ -50,25 +50,25 @@ TEST(test_api_test_module)
 	ret = xmp_test_module("data/test.mmcmp", &tinfo);
 	fail_unless(ret == 0, "XM test module fail");
 	fail_unless(strcmp(tinfo.name, "playboy") == 0, "XM module name fail");
-	fail_unless(strcmp(tinfo.type, "Fast Tracker II (XM)") == 0, "XM module type fail");
+	fail_unless(strcmp(tinfo.type, "Fast Tracker II") == 0, "XM module type fail");
 
 	/* MOD */
 	ret = xmp_test_module("data/ode2ptk.mod", &tinfo);
 	fail_unless(ret == 0, "MOD test module fail");
 	fail_unless(strcmp(tinfo.name, "Ode to Protracker") == 0, "MOD module name fail");
-	fail_unless(strcmp(tinfo.type, "Protracker (MOD)") == 0, "MOD module type fail");
+	fail_unless(strcmp(tinfo.type, "Amiga Protracker/Compatible") == 0, "MOD module type fail");
 
 	/* IT */
 	ret = xmp_test_module("data/storlek_01.it", &tinfo);
 	fail_unless(ret == 0, "IT test module fail");
 	fail_unless(strcmp(tinfo.name, "arpeggio + pitch slide") == 0, "IT module name fail");
-	fail_unless(strcmp(tinfo.type, "Impulse Tracker (IT)") == 0, "IT module type fail");
+	fail_unless(strcmp(tinfo.type, "Impulse Tracker") == 0, "IT module type fail");
 
 	/* S3M */
 	ret = xmp_test_module("data/xzdata", &tinfo);
 	fail_unless(ret == 0, "S3M test module fail");
 	fail_unless(strcmp(tinfo.name, "Inspiration") == 0, "S3M module name fail");
-	fail_unless(strcmp(tinfo.type, "Scream Tracker 3 (S3M)") == 0, "S3M module type fail");
+	fail_unless(strcmp(tinfo.type, "Scream Tracker 3") == 0, "S3M module type fail");
 
 	/* Prowizard */
 	ret = xmp_test_module("data/PRU1.intro-electro", &tinfo);
