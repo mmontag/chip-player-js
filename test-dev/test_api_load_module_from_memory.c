498de9f8fd4add02abae8c3450f6da6c53c065e9
Claudio Matsuoka <cmatsuoka@gmail.com>
2013-12-29 08:08:39 -0200

    [test] Add reborning.mod to load_from_memory tests
    from:	 Andreas Argirakis <andreas@blazer.nu>
    to:	 xmp-devel@lists.sourceforge.net
    date:	 Sat, Dec 28, 2013 at 11:14 PM
    subject: [xmp] certain .mod crash on loading using xmp_load_module_from_memory
    
    Using libxmp 4.2.0.
    
    Using xmp_load_module_from_memory() loading (modland)Protracker/6-Mate/reborning.mod gives a crash. I tracked it down to void read_title(HIO_HANDLE *f, char *t, int s) in common.c.
    Using xmp_load_module() works however fine with that module.
    
    Signed-off-by: Claudio Matsuoka <cmatsuoka@gmail.com>

--- test-dev/test_api_load_module_from_memory.c
+++ test-dev/test_api_load_module_from_memory.c
@@ -35,5 +35,20 @@ TEST(test_api_load_module_from_memory)
 
 	xmp_get_frame_info(ctx, &fi);
 	fail_unless(fi.total_time == 7680, "module duration");
+
+
+	/* reported crashing in 4.2.0 by Andreas Argirakis */
+	xmp_release_module(ctx);
+	f = fopen("data/m/reborning.mod", "rb");
+	fail_unless(f != NULL, "can't open module");
+	size = fread(buffer, 1, 8192, f);
+	fclose(f);
+
+	ret = xmp_load_module_from_memory(ctx, buffer, size);
+	fail_unless(ret == 0, "load file");
+
+	xmp_get_frame_info(ctx, &fi);
+	fail_unless(fi.total_time == 107520, "module duration");
+
 }
 END_TEST
