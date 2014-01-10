8cd4c0f5752e6abb73ed9fd2563db39d628928b5
Claudio Matsuoka <cmatsuoka@gmail.com>
2014-01-10 11:15:34 -0200

    [test] Update tests to use new module types
    Signed-off-by: Claudio Matsuoka <cmatsuoka@gmail.com>

--- test-dev/test_loader_sfx.c
+++ test-dev/test_loader_sfx.c
@@ -10,7 +10,7 @@ TEST(test_loader_sfx)
 	f = fopen("data/format_sfx.data", "r");
 
 	opaque = xmp_create_context();
-	ret = xmp_load_module(opaque, "data/m/PSA.sfx");
+	ret = xmp_load_module(opaque, "data/m/SFX.Crockett's_theme");
 	fail_unless(ret == 0, "module load");
 
 	xmp_get_module_info(opaque, &info);
