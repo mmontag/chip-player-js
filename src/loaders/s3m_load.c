mmontag/chip-player-js

commit 93fd7105072b503852dac35959eb46c927d6c675

Fix number of patterns in S3M loader
Signed-off-by: Claudio Matsuoka <cmatsuoka@gmail.com>

src/loaders/s3m_load.c
@@ -279,13 +279,15 @@ static int s3m_load(struct module_data *m, HIO_HANDLE *f, const int start)
 	hio_read(mod->xxo, 1, mod->len, f);
 	hio_seek(f, sfh.ordnum - XMP_MAX_MOD_LENGTH, SEEK_CUR);
     }
-    mod->pat = 0;
-    for (i = 0; i < mod->len && mod->xxo[i] != 0xff; ++i) {
-      if (mod->xxo[i] > mod->pat)
-        mod->pat = mod->xxo[i];
+
+    mod->pat = -1;
+    for (i = 0; i < mod->len && mod->xxo[i] < 0xfe; ++i) {
+	if (mod->xxo[i] > mod->pat)
+            mod->pat = mod->xxo[i];
     }
+    mod->pat++;
     if (mod->pat > sfh.patnum)
-      mod->pat = sfh.patnum;
+	mod->pat = sfh.patnum;
 
     mod->trk = mod->pat * mod->chn;
     /* Load and convert header */

commit 3368bb9a4583d75e1844fad5caa964045d007179

Fix S3M replay length
In S3M order list pattern 0xfe is a skip marker and 0xff is an end of
module marker. A bug introduced in commit 93fd710 cut the module length
at the first occurrence of the 0xfe marker, fix it to have full length
replay.

Signed-off-by: Claudio Matsuoka <cmatsuoka@gmail.com>

src/loaders/s3m_load.c
@@ -281,7 +281,7 @@ static int s3m_load(struct module_data *m, HIO_HANDLE *f, const int start)
     }
 
     mod->pat = -1;
-    for (i = 0; i < mod->len && mod->xxo[i] < 0xfe; ++i) {
+    for (i = 0; i < mod->len && mod->xxo[i] < 0xff; ++i) {
 	if (mod->xxo[i] > mod->pat)
             mod->pat = mod->xxo[i];
     }