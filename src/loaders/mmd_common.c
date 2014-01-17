07e35a9d48296dc9ff92dd3f2be58a098e1b8de5
Claudio Matsuoka <cmatsuoka@gmail.com>
2014-01-17 10:02:09 -0200

    Change MED support files license to MIT
    Start relicensing core portions of the library to MIT. The library as
    a whole stays under LGPL because certain third-party components require
    that. We expect to remove or replace these parts so the next major
    version could be entirely MIT-licensed.
    
    Signed-off-by: Claudio Matsuoka <cmatsuoka@gmail.com>

--- src/loaders/mmd_common.c
+++ src/loaders/mmd_common.c
@@ -1,9 +1,23 @@
-/* Extended Module Player
- * Copyright (C) 1996-2014 Claudio Matsuoka and Hipolito Carraro Jr
+/* MED/OctaMED module support for xmp
+ * Copyright (C) 2000-2014 Claudio Matsuoka
  *
- * This file is part of the Extended Module Player and is distributed
- * under the terms of the GNU Lesser General Public License. See COPYING.LIB
- * for more information.
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
  */
 
 /*
