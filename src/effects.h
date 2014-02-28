63b998224c66535187e8153e2b86102d05de443d
Claudio Matsuoka <cmatsuoka@gmail.com>
2014-02-28 19:40:19 -0300

    Revert "Renumber effect macros"
    This reverts commit 9f896e1c198f43712192690c74552c4f0305d366.

--- src/effects.h
+++ src/effects.h
@@ -18,6 +18,20 @@
 #define FX_EXTENDED	0x0e
 #define FX_SPEED	0x0f
 
+/* Fast tracker effects */
+#define FX_SETPAN	0x08
+
+/* Fast Tracker II effects */
+#define FX_GLOBALVOL	0x10
+#define FX_GVOL_SLIDE	0x11
+#define FX_KEYOFF	0x14
+#define FX_ENVPOS	0x15
+#define FX_MASTER_PAN	0x16
+#define FX_PANSLIDE	0x19
+#define FX_MULTI_RETRIG	0x1b
+#define FX_TREMOR	0x1d
+#define FX_XF_PORTA	0x21
+
 /* Protracker extended effects */
 #define EX_F_PORTA_UP	0x01
 #define EX_F_PORTA_DN	0x02
@@ -34,88 +48,73 @@
 #define EX_PATT_DELAY	0x0e
 #define EX_INVLOOP	0x0f
 
-/* Fast tracker effects */
-#define FX_SETPAN	0x08
-
-/* Fast Tracker II effects */
-#define FX_GLOBALVOL	0x20
-#define FX_GVOL_SLIDE	0x21
-#define FX_KEYOFF	0x22
-#define FX_ENVPOS	0x23
-#define FX_MASTER_PAN	0x24
-#define FX_PANSLIDE	0x25
-#define FX_MULTI_RETRIG	0x26
-#define FX_TREMOR	0x27
-#define FX_XF_PORTA	0x28
-#define FX_VOLSLIDE_2	0x29
-
-/* S3M effects */
-#define FX_S3M_SPEED	0x30	/* S3M */
-#define FX_S3M_BPM	0x31	/* S3M */
-#define FX_S3M_ARPEGGIO	0x32
-#define FX_FINE_VIBRATO	0x33	/* S3M/PTM/IMF/LIQ */
-
-/* IT effects */
-#define FX_TRK_VOL      0x40
-#define FX_TRK_VSLIDE   0x41
-#define FX_TRK_FVSLIDE  0x42
-#define FX_IT_INSTFUNC	0x43
-#define FX_FLT_CUTOFF	0x44
-#define FX_FLT_RESN	0x45
-#define FX_FINE2_VIBRA	0x46
-#define FX_IT_BPM	0x47
-#define FX_IT_ROWDELAY	0x48
-#define FX_IT_PANSLIDE	0x49
-#define FX_PANBRELLO	0x4a
-#define FX_PANBRELLO_WF	0x4b
-
 #ifndef XMP_CORE_PLAYER
-/* PTM effects */
-#define FX_NSLIDE_DN	0x71	/* IMF/PTM note slide down */
-#define FX_NSLIDE_UP	0x72	/* IMF/PTM note slide up */
-#define FX_NSLIDE_R_UP	0x73	/* PTM note slide down with retrigger */
-#define FX_NSLIDE_R_DN	0x74	/* PTM note slide up with retrigger */
-
-/* MED effects */
-#define FX_HOLD_DECAY	0x75
-#define FX_SETPITCH	0x76
-#define FX_MED_DECAY	0x77	/* MMD hold/decay */
-
 /* Oktalyzer effects */
-#define FX_OKT_ARP3	0x80
-#define FX_OKT_ARP4	0x81
-#define FX_OKT_ARP5	0x82
-#define FX_NSLIDE2_DN	0x83
-#define FX_NSLIDE2_UP	0x84
-#define FX_F_NSLIDE_DN	0x85
-#define FX_F_NSLIDE_UP	0x86
-
-/* His Master's Noise effects */
-#define FX_MEGAARP	0x88	/* Smaksak effect 7: MegaArp */
+#define FX_OKT_ARP3	0x70
+#define FX_OKT_ARP4	0x71
+#define FX_OKT_ARP5	0x72
+#define FX_NSLIDE2_DN	0x73
+#define FX_NSLIDE2_UP	0x74
+#define FX_F_NSLIDE_DN	0x75
+#define FX_F_NSLIDE_UP	0x76
 
 /* Persistent effects -- for 669, FNK and FAR */
-#define FX_PER_PORTA_DN	0x90
-#define FX_PER_PORTA_UP	0x91
-#define FX_PER_TPORTA	0x92
-#define FX_PER_VIBRATO	0x93
-#define FX_PER_VSLD_UP	0x94
-#define FX_PER_VSLD_DN	0x95
-#define FX_SPEED_CP	0x96
-#define FX_PER_CANCEL	0x97
+#define FX_PER_PORTA_DN	0x78
+#define FX_PER_PORTA_UP	0x79
+#define FX_PER_TPORTA	0x7a
+#define FX_PER_VIBRATO	0x7b
+#define FX_PER_VSLD_UP	0x7c
+#define FX_PER_VSLD_DN	0x7d
+#define FX_SPEED_CP	0x7e
+#define FX_PER_CANCEL	0x7f
+#endif
+
+/* IT effects */
+#define FX_TRK_VOL      0x80
+#define FX_TRK_VSLIDE   0x81
+#define FX_TRK_FVSLIDE  0x82
+#define FX_IT_INSTFUNC	0x83
+#define FX_FLT_CUTOFF	0x84
+#define FX_FLT_RESN	0x85
+#define FX_FINE2_VIBRA	0x86
+#define FX_IT_BPM	0x87
+#define FX_IT_ROWDELAY	0x88
+#define FX_IT_PANSLIDE	0x89
+#define FX_PANBRELLO	0x8a
+#define FX_PANBRELLO_WF	0x8b
+
+#define FX_S3M_SPEED	0xa3	/* S3M */
+#define FX_S3M_BPM	0xab	/* S3M */
+#define FX_S3M_ARPEGGIO	0xb4
+#define FX_VOLSLIDE_2	0xa4
+#define FX_FINE_VIBRATO	0xac	/* S3M/PTM/IMF/LIQ */
+
+#ifndef XMP_CORE_PLAYER
+/* MED effects */
+#define FX_HOLD_DECAY	0x90
+#define FX_SETPITCH	0x91
+
+/* PTM effects */
+#define FX_NSLIDE_DN	0x9c	/* IMF/PTM note slide down */
+#define FX_NSLIDE_UP	0x9d	/* IMF/PTM note slide up */
+#define FX_NSLIDE_R_UP	0x9e	/* PTM note slide down with retrigger */
+#define FX_NSLIDE_R_DN	0x9f	/* PTM note slide up with retrigger */
 
 /* Extra effects */
 #define FX_VOLSLIDE_UP	0xa0	/* SFX, MDL */
 #define FX_VOLSLIDE_DN	0xa1
-#define FX_F_VSLIDE	0xa2	/* IMF/MDL */
-#define FX_CHORUS	0xa3	/* IMF */
-#define FX_REVERB	0xa4	/* IMF */
+#define FX_F_VSLIDE	0xa5	/* IMF/MDL */
+#define FX_CHORUS	0xa9	/* IMF */
+#define FX_REVERB	0xaa	/* IMF */
+#define FX_MED_DECAY	0xb1	/* MMD hold/decay */
+#define FX_MEGAARP	0xb2	/* Smaksak effect 7: MegaArp */
 #endif
 
-#define FX_F_VSLIDE_UP	0xb0	/* MMD */
-#define FX_F_VSLIDE_DN	0xb1	/* MMD */
-#define FX_F_PORTA_UP	0xb2	/* MMD */
-#define FX_F_PORTA_DN	0xb3	/* MMD */
-#define FX_FINETUNE	0xb4
-#define FX_PATT_DELAY	0xb5	/* MMD */
+#define FX_FINETUNE	0xa6
+#define FX_PATT_DELAY	0xb3	/* MMD */
+#define FX_F_VSLIDE_UP	0xad	/* MMD */
+#define FX_F_VSLIDE_DN	0xae	/* MMD */
+#define FX_F_PORTA_UP	0xaf	/* MMD */
+#define FX_F_PORTA_DN	0xb0	/* MMD */
 
 #endif /* LIBXMP_EFFECTS_H */
