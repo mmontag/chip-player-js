91819cecd42f77909640a6d6b1e8144becf9e034
Claudio Matsuoka <cmatsuoka@gmail.com>
2014-03-27 09:51:38 -0300

    [test] Update retrig effect test
    Signed-off-by: Claudio Matsuoka <cmatsuoka@gmail.com>

--- test-dev/test_effect_e9_retrig.c
+++ test-dev/test_effect_e9_retrig.c
@@ -15,7 +15,9 @@ TEST(test_effect_e9_retrig)
 	p = &ctx->p;
 
  	create_simple_module(ctx, 2, 2);
-	new_event(ctx, 0, 0, 0, 60, 2, 40, 0x0e, 0x92, 0, 0);
+	new_event(ctx, 0, 0, 0, 60, 2, 40, 0x0e, 0x92, 0x00, 0x00);
+	new_event(ctx, 0, 1, 0, 61, 2, 40, 0x0e, 0x90, 0x0f, 0x03);
+	new_event(ctx, 0, 2, 0, 62, 2, 40, 0x0e, 0x90, 0x00, 0x00);
 
 	xmp_start_player(opaque, 44100, 0);
 
@@ -56,5 +58,40 @@ TEST(test_effect_e9_retrig)
 	fail_unless(vi->note == 59, "row 0 frame 5");
 	fail_unless(vi->pos0 !=  0, "sample position frame 5");
 
+	/* Row 1 - without S3M quirk */
+
+	/* frame 0 */
+	xmp_play_frame(opaque);
+	fail_unless(vi->note == 60, "row 0 frame 0");
+	fail_unless(vi->pos0 ==  0, "sample position frame 0");
+
+	/* frame 1 */
+	xmp_play_frame(opaque);
+	fail_unless(vi->note == 60, "row 0 frame 3");
+	fail_unless(vi->pos0 !=  0, "sample position frame 1");
+
+	/* frame 2 */
+	xmp_play_frame(opaque);
+	fail_unless(vi->note == 60, "row 0 frame 3");
+	fail_unless(vi->pos0 !=  0, "sample position frame 2");
+
+	/* Row 2 - with S3M quirk */
+
+	set_quirk(ctx, QUIRK_S3MRTG, READ_EVENT_MOD);
+
+	/* frame 0 */
+	xmp_play_frame(opaque);
+	fail_unless(vi->note == 61, "row 0 frame 0");
+	fail_unless(vi->pos0 ==  0, "sample position frame 0");
+
+	/* frame 1 */
+	xmp_play_frame(opaque);
+	fail_unless(vi->note == 61, "row 0 frame 3");
+	fail_unless(vi->pos0 ==  0, "sample position frame 1");
+
+	/* frame 2 */
+	xmp_play_frame(opaque);
+	fail_unless(vi->note == 61, "row 0 frame 3");
+	fail_unless(vi->pos0 ==  0, "sample position frame 2");
 }
 END_TEST
