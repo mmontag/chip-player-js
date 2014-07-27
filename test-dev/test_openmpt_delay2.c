22386f4ce5f972934e9308dd2c3cc236822dfd65
Claudio Matsuoka <cmatsuoka@gmail.com>
2014-07-27 14:13:57 -0300

    [test] Add OpenMPT delay2 test
    Signed-off-by: Claudio Matsuoka <cmatsuoka@gmail.com>

---
+++ test-dev/test_openmpt_delay2.c
@@ -0,0 +1,81 @@
+#include "test.h"
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include "../src/mixer.h"
+#include "../src/virtual.h"
+
+/*
+ Rogue note delay test. It seems that internally, Fasttracker 2 always acts
+ like the last played note is next to a note delay (EDx with x > 0) if there
+ is no note. Doing exactly this is probably the easiest way to pass this test.
+ This also explains Fasttracker 2’s behaviour if there is an instrument number
+ next to such a rogue note delay, which is shown in this test. Both channels
+ should play exactly the same combination of snare and bass sounds.
+*/
+
+TEST(test_openmpt_delay2)
+{
+	xmp_context opaque;
+	struct context_data *ctx;
+	struct module_data *m;
+        struct player_data *p;
+        struct mixer_voice *vi;
+	struct xmp_frame_info fi;
+	int time, row, frame, chan, period, note, ins, vol, pan, pos0;
+	char line[200];
+	FILE *f;
+	int i, voc;
+
+	f = fopen("data/openmpt/delay2.data", "r");
+
+	opaque = xmp_create_context();
+	xmp_load_module(opaque, "data/openmpt/delay2.xm");
+
+	ctx = (struct context_data *)opaque;
+	m = &ctx->m;
+	p = &ctx->p;
+
+	xmp_start_player(opaque, 44100, 0);
+
+	while (1) {
+		xmp_play_frame(opaque);
+		xmp_get_frame_info(opaque, &fi);
+		if (fi.loop_count > 0)
+			break;
+
+		for (i = 0; i < m->mod.chn; i++) {
+			struct xmp_channel_info *ci = &fi.channel_info[i];
+
+			voc = map_channel(p, i);
+			if (voc < 0)
+				continue;
+
+			vi = &p->virt.voice_array[voc];
+
+			fgets(line, 200, f);
+			sscanf(line, "%d %d %d %d %d %d %d %d %d %d",
+				&time, &row, &frame, &chan, &period,
+				&note, &ins, &vol, &pan, &pos0);
+
+			fail_unless(fi.time    == time,   "time mismatch");
+			fail_unless(fi.row     == row,    "row mismatch");
+			fail_unless(fi.frame   == frame,  "frame mismatch");
+			fail_unless(ci->period == period, "period mismatch");
+			fail_unless(vi->note   == note,   "note mismatch");
+			fail_unless(vi->ins    == ins,    "instrument");
+			fail_unless(vi->vol    == vol,    "volume mismatch");
+			fail_unless(vi->pan    == pan,    "pan mismatch");
+			fail_unless(vi->pos0   == pos0,   "position mismatch");
+		}
+		
+	}
+
+	fgets(line, 200, f);
+	fail_unless(feof(f), "not end of data file");
+
+	xmp_end_player(opaque);
+	xmp_release_module(opaque);
+	xmp_free_context(opaque);
+}
+END_TEST
