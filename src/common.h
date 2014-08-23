91ea20f1e51b5ec1651f439600c360c62e6739a2
Jürgen Wothke <juergen@wothke.ch>
2014-08-23 18:51:04 -0300

    Don't use versioned symbols in emscripten build
    Signed-off-by: Claudio Matsuoka <cmatsuoka@gmail.com>

--- src/common.h
+++ src/common.h
@@ -10,7 +10,7 @@
 #include "xmp.h"
 
 #if defined(__GNUC__) || defined(__clang__)
-#if !defined(WIN32) && !defined(ANDROID) && !defined(__APPLE__) && !defined(__AMIGA__) && !defined(B_BEOS_VERSION) && !defined(__ATHEOS__)
+#if !defined(WIN32) && !defined(ANDROID) && !defined(__APPLE__) && !defined(__AMIGA__) && !defined(B_BEOS_VERSION) && !defined(__ATHEOS__) && !defined(EMSCRIPTEN)
 #define USE_VERSIONED_SYMBOLS
 #endif
 #endif
