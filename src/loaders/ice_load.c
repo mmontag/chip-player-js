f874f1cf2f394de0c066b86b1887764c57079371
Claudio Matsuoka <cmatsuoka@gmail.com>
2014-10-11 18:37:55 -0300

    Use standard finetune setting in ST2.6 loader
    Signed-off-by: Claudio Matsuoka <cmatsuoka@gmail.com>

--- src/loaders/ice_load.c
+++ src/loaders/ice_load.c
@@ -130,7 +130,7 @@ static int ice_load(struct module_data *m, HIO_HANDLE *f, const int start)
 	xxs->lpe = xxs->lps + 2 * ih.ins[i].loop_size;
 	xxs->flg = ih.ins[i].loop_size > 1 ? XMP_SAMPLE_LOOP : 0;
 	xxi->sub[0].vol = ih.ins[i].volume;
-	xxi->sub[0].fin = ((int16)ih.ins[i].finetune / 0x48) << 4;
+	xxi->sub[0].fin = (int8)(ih.ins[i].finetune << 4);
 	xxi->sub[0].pan = 0x80;
 	xxi->sub[0].sid = i;
 
