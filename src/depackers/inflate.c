8b9abe1446cb81454173f52e54a5934aef1e8d2f
Claudio Matsuoka <cmatsuoka@gmail.com>
2015-01-28 09:39:55 -0200

    Add sanity check to inflate decompressor
    Signed-off-by: Claudio Matsuoka <cmatsuoka@gmail.com>

--- src/depackers/inflate.c
+++ src/depackers/inflate.c
@@ -53,6 +53,8 @@ struct huffman_tree_t
   short int right;
 };
 
+#define DIST_CODES_SIZE 30
+
 static const int length_codes[29] = { 3,4,5,6,7,8,9,10,11,13,15,17,19,
                          23,27,31,35,43,51,59,67,83,99,115,
                          131,163,195,227,258 };
@@ -61,7 +63,7 @@ static const int length_extra_bits[29] = { 0,0,0,0,0,0,0,0,1,1,1,1,
                               2,2,2,2,3,3,3,3,4,4,4,4,
                               5,5,5,5,0 };
 
-static const int dist_codes[30] = { 1,2,3,4,5,7,9,13,17,25,
+static const int dist_codes[DIST_CODES_SIZE] = { 1,2,3,4,5,7,9,13,17,25,
                              33,49,65,97,129,193,257,385,513,769,
                              1025,1537,2049,3073,4097,6145,8193,
                              12289,16385,24577 };
@@ -382,7 +384,7 @@ static int load_codes(FILE *in, struct bitstream_t *bitstream, int *lengths, int
     }
       else
     {
-      fprintf(stderr, "unzip: error in bitstream reading in literal code length %d\n",t);
+      fprintf(stderr, "inflate: error in bitstream reading in literal code length %d\n",t);
       return -1;
     }
   }
@@ -829,6 +831,11 @@ int decompress(FILE *in, struct huffman_t *huffman, struct bitstream_t *bitstrea
           bitstream->bitptr-=1;
           bitstream->holding>>=1;
         }
+      }
+
+      if (code >= DIST_CODES_SIZE) {
+	fprintf(stderr, "inflate: corrupt input\n");
+        return -1;
       }
 
       dist=dist_codes[code];
