a983dc5c1a6a25d3044de0c70bf76fe2bc7214ed
Claudio Matsuoka <cmatsuoka@gmail.com>
2015-03-31 14:09:43 -0300

    Fix loading of 16-bit samples in MMD3 modules
    Signed-off-by: Claudio Matsuoka <cmatsuoka@gmail.com>

--- src/loaders/mmd3_load.c
+++ src/loaders/mmd3_load.c
@@ -484,7 +484,7 @@ static int mmd3_load(struct module_data *m, HIO_HANDLE *f, const int start)
 			D_(D_WARN "stereo sample unsupported");
 			mod->xxi[i].nsm = 0;
 			continue;
-		} */ else if (instr.type == 0) {		/* Sample */
+		} */ else if ((instr.type & ~S_16) == 0) {	/* Sample */
 			int ret;
 
 			hio_seek(f, start + smpl_offset + 6, SEEK_SET);
