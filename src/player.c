eed97486a26d4a14b5c824838e2360d372fc0bce
Claudio Matsuoka <cmatsuoka@gmail.com>
2015-04-01 17:33:47 -0300

    Fix misleading indent
    Signed-off-by: Claudio Matsuoka <cmatsuoka@gmail.com>

--- src/player.c
+++ src/player.c
@@ -361,7 +361,7 @@ static void process_volume(struct context_data *ctx, int chn, int act)
 	if (check_envelope_end(&instrument->aei, xc->v_idx)) {
 		if (vol_envelope == 0)
 			SET_NOTE(NOTE_END);
-			SET_NOTE(NOTE_ENV_END);
+		SET_NOTE(NOTE_ENV_END);
 	}
 
 	if (TEST_NOTE(NOTE_PRERELEASE)) {
