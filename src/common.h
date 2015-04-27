4c9d2b61183292903877fa16cbd09b3fdbfe95d2
Claudio Matsuoka <cmatsuoka@gmail.com>
2015-04-27 09:20:01 -0300

    Don't prettify function names in debug messages
    Signed-off-by: Claudio Matsuoka <cmatsuoka@gmail.com>

--- src/common.h
+++ src/common.h
@@ -109,7 +109,7 @@ void __inline CLIB_DECL D_(const char *text, ...) { do {} while (0); }
 #define D_CRIT "\x1b[31m"
 #define D_WARN "\x1b[36m"
 #define D_(args...) do { \
-	printf("\x1b[33m%s \x1b[37m[%s:%d] " D_INFO, __PRETTY_FUNCTION__, \
+	printf("\x1b[33m%s \x1b[37m[%s:%d] " D_INFO, __FUNCTION__, \
 		__FILE__, __LINE__); printf (args); printf ("\x1b[0m\n"); \
 	} while (0)
 #else
