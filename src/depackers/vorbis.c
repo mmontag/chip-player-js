bdb1c8e6c8d1fdecc0b53eff4a5b5912e6d71fa2
Claudio Matsuoka <cmatsuoka@gmail.com>
2015-06-13 08:18:29 -0300

    Use common type definitions in vorbis decoder
    Signed-off-by: Claudio Matsuoka <cmatsuoka@gmail.com>

--- src/depackers/vorbis.c
+++ src/depackers/vorbis.c
@@ -236,12 +236,14 @@
 #define MAX_BLOCKSIZE      (1 << MAX_BLOCKSIZE_LOG)
 
 
+#if 0
 typedef unsigned char  uint8;
 typedef   signed char   int8;
 typedef unsigned short uint16;
 typedef   signed short  int16;
 typedef unsigned int   uint32;
 typedef   signed int    int32;
+#endif
 
 #ifndef TRUE
 #define TRUE 1
