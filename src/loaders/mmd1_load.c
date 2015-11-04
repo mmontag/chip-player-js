d1299aae9adce518fd613738273750212f72b133
Claudio Matsuoka <cmatsuoka@gmail.com>
2015-11-04 09:44:21 -0200

    Handle signal extension error in MMD1 loader
    Signed-off-by: Claudio Matsuoka <cmatsuoka@gmail.com>

--- src/loaders/mmd1_load.c
+++ src/loaders/mmd1_load.c
@@ -449,8 +449,11 @@ static int mmd1_load(struct module_data *m, HIO_HANDLE *f, const int start)
 
 		if (expdata_offset && i < expdata.i_ext_entries) {
 			struct xmp_instrument *xxi = &mod->xxi[i];
-			if (hio_seek(f, iinfo_offset + i * expdata.i_ext_entrsz, SEEK_SET) != 0)
+			int offset = iinfo_offset + i * expdata.i_ext_entrsz;
+			
+			if (offset < 0 || hio_seek(f, offset, SEEK_SET) < 0) {
 				return -1;
+			}
 			hio_read(&xxi->name, 40, 1, f);
 		}
 
@@ -458,8 +461,11 @@ static int mmd1_load(struct module_data *m, HIO_HANDLE *f, const int start)
 
 		exp_smp.finetune = 0;
 		if (expdata_offset && i < expdata.s_ext_entries) {
-			if (hio_seek(f, expsmp_offset + i * expdata.s_ext_entrsz, SEEK_SET) != 0)
-			  return -1;
+			int offset = expsmp_offset + i * expdata.s_ext_entrsz;
+
+			if (offset < 0 || hio_seek(f, offset, SEEK_SET) < 0) {
+				return -1;
+			}
 			exp_smp.hold = hio_read8(f);
 			exp_smp.decay = hio_read8(f);
 			exp_smp.suppress_midi_off = hio_read8(f);
