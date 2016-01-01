e6f3da389ce24e8b3877db68402202484c0ad8bd
Claudio Matsuoka <cmatsuoka@gmail.com>
2016-01-01 09:11:00 -0200

    Update copyright notice
    Signed-off-by: Claudio Matsuoka <cmatsuoka@gmail.com>

--- src/loaders/stc_load.c
+++ src/loaders/stc_load.c
@@ -1,5 +1,5 @@
 /* Extended Module Player
- * Copyright (C) 1996-2015 Claudio Matsuoka and Hipolito Carraro Jr
+ * Copyright (C) 1996-2016 Claudio Matsuoka and Hipolito Carraro Jr
  *
  * This file is part of the Extended Module Player and is distributed
  * under the terms of the GNU Lesser General Public License. See COPYING.LIB
