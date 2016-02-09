d488c4824fd670a58d4aa7a80211d1cbde6aa165
Claudio Matsuoka <cmatsuoka@gmail.com>
2016-02-09 06:57:29 -0200

    Adjust copyright notice in format headers
    Signed-off-by: Claudio Matsuoka <cmatsuoka@gmail.com>

--- src/loaders/it.h
+++ src/loaders/it.h
@@ -1,4 +1,4 @@
-/* Extended Module Player format loaders
+/* Extended Module Player
  * Copyright (C) 1996-2016 Claudio Matsuoka and Hipolito Carraro Jr
  *
  * Permission is hereby granted, free of charge, to any person obtaining a
