54053815767cdda93bafca16f195e4782494a368
Claudio Matsuoka <cmatsuoka@gmail.com>
2016-02-27 20:23:02 -0300

    Fix S3M subsong detection
    We can't trust the number of patterns reported in the file header, it may
    be overestimated. Check the highest pattern number in the orders instead of
    doing it only until 0xff ("When the heavens fall" has multiple 0xff markers
    in the order list).
    
    Signed-off-by: Claudio Matsuoka <cmatsuoka@gmail.com>

--- src/loaders/s3m_load.c
+++ src/loaders/s3m_load.c
@@ -321,10 +321,12 @@ static int s3m_load(struct module_data *m, HIO_HANDLE *f, const int start)
         goto err3;
     }
 
+    /* Don't trust sfh.patnum */
     mod->pat = -1;
-    for (i = 0; i < mod->len && mod->xxo[i] < 0xff; ++i) {
-	if (mod->xxo[i] > mod->pat)
+    for (i = 0; i < mod->len; ++i) {
+	if (mod->xxo[i] < 0xfe && mod->xxo[i] > mod->pat) {
             mod->pat = mod->xxo[i];
+	}
     }
     mod->pat++;
     if (mod->pat > sfh.patnum)
