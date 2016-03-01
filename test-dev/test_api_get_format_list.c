767aef63160ee11a57c8953282b11658152cb6fa
Claudio Matsuoka <cmatsuoka@gmail.com>
2016-03-01 11:26:16 -0300

    [test] Update number of formats
    Signed-off-by: Claudio Matsuoka <cmatsuoka@gmail.com>

--- test-dev/test_api_get_format_list.c
+++ test-dev/test_api_get_format_list.c
@@ -12,6 +12,6 @@ TEST(test_api_get_format_list)
 		fail_unless(*list[i] != 0, "empty format name");
 	}
 
-	fail_unless(i == 92, "wrong number of formats");
+	fail_unless(i == 96, "wrong number of formats");
 }
 END_TEST
