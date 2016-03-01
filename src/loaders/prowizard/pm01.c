5889b277a017721281d06fc5489a323adf4b85a4
Claudio Matsuoka <cmatsuoka@gmail.com>
2016-03-01 20:04:12 -0300

    Disable Promizer 0.1 format converter
    Signed-off-by: Claudio Matsuoka <cmatsuoka@gmail.com>

--- src/loaders/prowizard/pm01.c
+++ src/loaders/prowizard/pm01.c
@@ -8,6 +8,7 @@
 
 #include <string.h>
 #include <stdlib.h>
+#include <math.h>
 #include "prowiz.h"
 
 static int depack_pm01(HIO_HANDLE *in, FILE *out)
@@ -18,7 +19,7 @@ static int depack_pm01(HIO_HANDLE *in, FILE *out)
 	uint8 tmp[1024];
 	uint8 pdata[1024];
 	uint8 fin[31];
-	uint8 Old_ins_Nbr[4];
+	uint8 oldins[4];
 	int i, j;
 	int psize, size, ssize = 0;
 	int pat_ofs[128];
@@ -26,7 +27,7 @@ static int depack_pm01(HIO_HANDLE *in, FILE *out)
 	memset(ptable, 0, 128);
 	memset(pat_ofs, 0, 128 * 4);
 	memset(fin, 0, 31);
-	memset(Old_ins_Nbr, 0, 4);
+	memset(oldins, 0, 4);
 
 	pw_write_zero(out, 20);			/* title */
 
@@ -96,44 +97,37 @@ static int depack_pm01(HIO_HANDLE *in, FILE *out)
 			pdata[j] = pdata[j] ^ 0xff;
 		}
 
+		/* now take care of these 'finetuned' values ... pfff */
+		oldins[0] = oldins[1] = oldins[2] = oldins[3] = 0x1f;
+		for (j = 0; j < 64 * 4; j++) {
+			uint8 *p = pdata + j * 4;
+			int note = readmem16b(p) & 0x0fff;
+			int ins = (p[0] & 0xf0) | ((p[2] >> 4) & 0x0f);
+
+			if (note == 0) {
+				continue;
+			}
+
+			if (ins == 0) {
+				ins = oldins[i % 4];
+			} else {
+				oldins[i % 4] = ins;
+			}
+
+			note = (int)((double)note *
+					pow(2, -1.0 * fin[j % 4] / 12 / 8));
+			if (note > 0) {
+				note = period_to_note(note) - 48;
+			}
+
+			p[0] = ptk_table[note][0];
+			p[1] = ptk_table[note][1];
+		}
+
 		if (fwrite(pdata, 1, 1024, out) != 1024) {
 			return -1;
 		}
 	}
-
-#if 0
-	/* all right, now, let's take care of these 'finetuned' value ... pfff */
-	Old_ins_Nbr[0] = Old_ins_Nbr[1] = Old_ins_Nbr[2] = Old_ins_Nbr[3] =
-		0x1f;
-	memset(tmp, 0, j);
-	for (i = 0; i < j / 4; i++) {
-		c1 = pdata[i * 4] & 0x0f;
-		c2 = pdata[i * 4 + 1];
-		k = (c1 << 8) + c2;
-		c3 =
-			(pdata[i * 4] & 0xf0) | ((pdata[i * 4 +
-					   2] >> 4) & 0x0f);
-		if (c3 == 0)
-			c3 = Old_ins_Nbr[i % 4];
-		else
-			Old_ins_Nbr[i % 4] = c3;
-		if ((k != 0) && (fin[c3 - 1] != 0x00)) {
-/*fprintf ( info , "! (at %ld)(smp:%x)(pitch:%ld)\n" , (i*4)+382 , c3 , k );*/
-			for (l = 0; l < 36; l++) {
-				if (k == tun_table[fin[c3 - 1]][l]) {
-					tmp[i * 4] = ptk_table[l + 1][0];
-					tmp[i * 4 + 1] = ptk_table[l + 1][1];
-				}
-			}
-		} else {
-			tmp[i * 4] = pdata[i * 4] & 0x0f;
-			tmp[i * 4 + 1] = pdata[i * 4 + 1];
-		}
-		tmp[i * 4] |= (pdata[i * 4] & 0xf0);
-		tmp[i * 4 + 2] = pdata[i * 4 + 2];
-		tmp[i * 4 + 3] = pdata[i * 4 + 3];
-	}
-#endif
 
 	/* sample data */
 	pw_move_data(out, in, ssize);
@@ -145,7 +139,7 @@ static int depack_pm01(HIO_HANDLE *in, FILE *out)
 static int test_pm01(uint8 *data, char *t, int s)
 {
 	int i;
-	int len, psize, ssize;
+	int len, psize, ssize = 0;
 
 	PW_REQUEST_DATA(s, 1024);
 
