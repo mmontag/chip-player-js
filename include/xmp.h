9c999e7f06e4230d6e189037d9263ee5675603d6
Claudio Matsuoka <cmatsuoka@gmail.com>
2016-04-22 22:52:34 -0300

    Update version number
    Signed-off-by: Claudio Matsuoka <cmatsuoka@gmail.com>

--- include/xmp.h
+++ include/xmp.h
@@ -5,11 +5,11 @@
 extern "C" {
 #endif
 
-#define XMP_VERSION "4.3.13"
-#define XMP_VERCODE 0x04030d
+#define XMP_VERSION "4.3.14"
+#define XMP_VERCODE 0x04030e
 #define XMP_VER_MAJOR 4
 #define XMP_VER_MINOR 3
-#define XMP_VER_RELEASE 13
+#define XMP_VER_RELEASE 14
 
 #if defined(_WIN32) && !defined(__CYGWIN__)
 # if defined(BUILDING_STATIC)
