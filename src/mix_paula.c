38180b5a738653fcef4597c649376a10bf8a3c9e
Claudio Matsuoka <cmatsuoka@gmail.com>
2016-06-06 20:34:44 -0300

    Add Antti Lankila's Paula bandlimited steps reference code
    Signed-off-by: Claudio Matsuoka <cmatsuoka@gmail.com>

---
+++ src/mix_paula.c
@@ -0,0 +1,63 @@
+/*
+ * Based on Antti S. Lankila's reference code, modified for libxmp
+ * by Claudio Matsuoka.
+ */
+#include "common.h"
+#include "paula.h"
+#include "precomp_blep.h"
+
+/* return output simulated as series of bleps */
+int16 output_sample(struct paula_data *paula, int tabnum)
+{
+	int i;
+	int32 output;
+
+	output = paula->global_output_level << BLEP_SCALE;
+	for (i = 0; i < paula->active_bleps; i++) {
+		int age = paula->blepstate[i].age;
+		int level = paula->blepstate[i].level;
+		output -= winsinc_integral[tabnum][age] * level;
+	}
+	output >>= BLEP_SCALE;
+
+	if (output < -32768)
+		output = -32768;
+	else if (output > 32767)
+		output = 32767;
+
+	return output;
+}
+
+void input_sample(struct paula_data *paula, int16 sample)
+{
+	if (sample != paula->global_output_level) {
+		/* Start a new blep: level is the difference, age (or phase) is 0 clocks. */
+		if (paula->active_bleps > MAX_BLEPS - 1) {
+			fprintf(stderr, "warning: active blep list truncated!\n");
+			paula->active_bleps = MAX_BLEPS - 1;
+		}
+
+		/* Make room for new blep */
+		memmove(&paula->blepstate[1], &paula->blepstate[0],
+			sizeof(struct blep_state) * paula->active_bleps);
+
+		/* Update state to account for the new blep */
+		paula->active_bleps++;
+		paula->blepstate[0].age = 0;
+		paula->blepstate[0].level = sample - paula->global_output_level;
+		paula->global_output_level = sample;
+	}
+}
+
+void clock(struct paula_data *paula, unsigned int cycles)
+{
+	int i;
+
+	for (i = 0; i < paula->active_bleps; i++) {
+		paula->blepstate[i].age += cycles;
+		if (paula->blepstate[i].age >= BLEP_SIZE) {
+			paula->active_bleps = i;
+			break;
+		}
+	}
+}
---
+++ src/paula.h
@@ -0,0 +1,30 @@
+#ifndef LIBXMP_PAULA_H
+#define LIBXMP_PAULA_H
+
+/* 131072 to 0, 2048 entries */
+#define MINIMUM_INTERVAL 16
+#define BLEP_SCALE 17
+#define BLEP_SIZE 2048
+#define MAX_BLEPS (BLEP_SIZE / MINIMUM_INTERVAL)
+
+/* the structure that holds data of bleps */
+struct blep_state {
+	int level;
+	int age;
+};
+
+struct paula_data {
+	/* the instantenous value of Paula output */
+	int16 global_output_level;
+
+	/* count of simultaneous bleps to keep track of */
+	unsigned int active_bleps;
+
+	/* place to keep our bleps in. MAX_BLEPS should be
+	 * defined as a BLEP_SIZE / MINIMUM_EVENT_INTERVAL.
+	 * For Paula, minimum event interval could be even 1, but it makes
+	 * sense to limit it to some higher value such as 16. */
+	struct blep_state blepstate[MAX_BLEPS];
+};
+
+#endif /* LIBXMP_PAULA_H */
