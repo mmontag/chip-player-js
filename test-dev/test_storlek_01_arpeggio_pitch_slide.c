5d241543d7e8da8e57c6e71c754ce6c2a8690d16
Claudio Matsuoka <cmatsuoka@gmail.com>
2016-07-13 21:48:49 -0300

    [test] Update Storlek tests reference data
    Signed-off-by: Claudio Matsuoka <cmatsuoka@gmail.com>

--- test-dev/test_storlek_01_arpeggio_pitch_slide.c
+++ test-dev/test_storlek_01_arpeggio_pitch_slide.c
@@ -1,7 +1,4 @@
 #include "test.h"
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <fcntl.h>
 
 /*
 01 - Arpeggio and pitch slide
@@ -28,41 +25,8 @@ note is playing.
 
 TEST(test_storlek_01_arpeggio_pitch_slide)
 {
-	xmp_context opaque;
-	struct xmp_frame_info info;
-	struct xmp_channel_info *ci = &info.channel_info[0];
-	int time, row, frame, chan, period, volume;
-	char line[200];
-	FILE *f;
-
-	f = fopen("data/storlek_01.data", "r");
-
-	opaque = xmp_create_context();
-	xmp_load_module(opaque, "data/storlek_01.it");
-	xmp_start_player(opaque, 44100, 0);
-
-	while (1) {
-		xmp_play_frame(opaque);
-		xmp_get_frame_info(opaque, &info);
-		if (info.loop_count > 0)
-			break;
-
-		fgets(line, 200, f);
-		sscanf(line, "%d %d %d %d %d %d",
-			&time, &row, &frame, &chan, &period, &volume);
-
-		fail_unless(info.time  == time,   "time mismatch");
-		fail_unless(info.row   == row,    "row mismatch");
-		fail_unless(info.frame == frame,  "frame mismatch");
-		fail_unless(ci->period == period, "period mismatch");
-		fail_unless(ci->volume == volume, "volume mismatch");
-	}
-
-	fgets(line, 200, f);
-	fail_unless(feof(f), "not end of data file");
-
-	xmp_end_player(opaque);
-	xmp_release_module(opaque);
-	xmp_free_context(opaque);
+	compare_mixer_data(
+		"data/storlek_01.it",
+		"data/storlek_01.data");
 }
 END_TEST
