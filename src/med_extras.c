9f1662f90907da54b872548121045f0949f47cec
Claudio Matsuoka <cmatsuoka@gmail.com>
2016-07-22 20:34:55 -0300

    Don't change phase when switching MED synth samples
    Signed-off-by: Claudio Matsuoka <cmatsuoka@gmail.com>

--- src/med_extras.c
+++ src/med_extras.c
@@ -322,8 +322,11 @@ void med_play_extras(struct context_data *ctx, struct channel_data *xc, int chn)
 		default:
 			xxi = &m->mod.xxi[xc->ins];
 			if (b < xxi->nsm && xxi->sub[b].sid != xc->smp) {
+				int pos;
 				xc->smp = xxi->sub[b].sid;
+				pos = virt_getvoicepos(ctx, chn);
 				virt_setsmp(ctx, chn, xc->smp);
+				virt_voicepos(ctx, chn, pos);
 			}
 		}
 
