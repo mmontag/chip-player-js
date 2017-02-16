57911fca4a7179677174cea7449af9687232e473
loki666 <simons.philippe@gmail.com>
2017-02-16 18:13:10 +0100

    add android build files

---
+++ android/Android.mk
@@ -0,0 +1,9 @@
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := adlmidi_shared
+LOCAL_SRC_FILES := libs/$(TARGET_ARCH_ABI)/libADLMIDI.so
+LOCAL_EXPORT_C_INCLUDES := $(LOCAL_PATH)/../src
+TARGET_PLATFORM = android-10
+
+include $(PREBUILT_SHARED_LIBRARY)
---
+++ android/jni/Android.mk
@@ -0,0 +1,17 @@
+LOCAL_PATH := $(call my-dir)/../..
+include $(CLEAR_VARS)
+
+LOCAL_MODULE     := ADLMIDI
+
+LOCAL_C_INCLUDES := \
+	$(LOCAL_PATH)/src
+
+LOCAL_ARM_MODE   := arm
+LOCAL_CPP_FEATURES := exceptions
+LOCAL_LDLIBS     := -llog
+LOCAL_SRC_FILES := src/adldata.cpp src/adlmidi.cpp src/dbopl.cpp src/nukedopl3.c \
+                   src/adlmidi_load.cpp src/adlmidi_midiplay.cpp \
+                   src/adlmidi_opl3.cpp src/adlmidi_private.cpp \
+                   src/adlmidi_xmi2mid.c src/adlmidi_mus2mid.c
+
+include $(BUILD_SHARED_LIBRARY)
