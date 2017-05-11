01f847ba04f5c77693d78222c721d3de2fc33e8c
Peter Chapman <peter@conglomo.co.nz>
2017-05-11 14:58:20 +1200

    Updated to 3.9.9

--- version.hpp
+++ version.hpp
@@ -1,6 +1,6 @@
 #define RARVER_MAJOR     3
 #define RARVER_MINOR    92
-#define RARVER_BETA      1
-#define RARVER_DAY      21
-#define RARVER_MONTH     1
+#define RARVER_BETA      0
+#define RARVER_DAY      10
+#define RARVER_MONTH     2
 #define RARVER_YEAR   2010
