70d98fc6ac39b216bd76d83e7d4bb6def566c639
AliceLR <petrifiedrowan@gmail.com>
2017-07-14 03:02:13 -0600

    Fixed .it bug where Cxx on same row as SBx would not be ignored

--- src/effects.c
+++ src/effects.c
@@ -586,8 +586,11 @@ void libxmp_process_fx(struct context_data *ctx, struct channel_data *xc, int ch
 		xc->vol.fslide2 = -fxp;
 		break;
 	case FX_IT_BREAK:	/* Pattern break with hex parameter */
-		p->flow.pbreak = 1;
-		p->flow.jumpline = fxp;
+		if (!f->loop_chn)
+		{
+			p->flow.pbreak = 1;
+			p->flow.jumpline = fxp;
+		}
 		break;
 
 #endif
