e2e45e21d5fe00818788130b6c4a625cb98d43aa
JP Cimalando <jpcima@users.noreply.github.com>
2018-04-12 03:50:19 +0200

    fix SPtr implementation mistake

--- src/adlmidi_private.hpp
+++ src/adlmidi_private.hpp
@@ -216,6 +216,8 @@ public:
 
     AdlMIDI_SPtr &operator=(const AdlMIDI_SPtr &other)
     {
+        if(this == &other)
+            return *this;
         reset();
         m_p = other.m_p;
         m_counter = other.m_counter;
