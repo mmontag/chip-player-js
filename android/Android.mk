c7f87c27e88535403c0d97c7e3bdfea9efb90351
loki666 <simons.philippe@gmail.com>
2018-04-21 12:55:20 +0200

    android: add build rules for rt

--- android/Android.mk
+++ android/Android.mk
@@ -3,7 +3,12 @@ LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 LOCAL_MODULE := adlmidi_shared
 LOCAL_SRC_FILES := libs/$(TARGET_ARCH_ABI)/libADLMIDI.so
-LOCAL_CFLAGS := -DENABLE_BEGIN_SILENCE_SKIPPING
+LOCAL_EXPORT_C_INCLUDES := $(LOCAL_PATH)/../src $(LOCAL_PATH)/../include
+TARGET_PLATFORM = android-14
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := adlmidi_RT_shared
+LOCAL_SRC_FILES := libs/$(TARGET_ARCH_ABI)/libADLMIDIrt.so
 LOCAL_EXPORT_C_INCLUDES := $(LOCAL_PATH)/../src $(LOCAL_PATH)/../include
 TARGET_PLATFORM = android-14
 
--- android/jni/Application.mk
+++ android/jni/Application.mk
@@ -1,5 +1,5 @@
 APP_ABI               := armeabi-v7a arm64-v8a x86 x86_64
-APP_MODULES           := ADLMIDI
+APP_MODULES           := ADLMIDI ADLMIDIrt
 APP_PLATFORM          := android-14
 APP_OPTIM             := release
 APP_CPPFLAGS          += -std=c++11
