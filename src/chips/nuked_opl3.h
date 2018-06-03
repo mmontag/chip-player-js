e3cad2f46925ac0b9ae303bea68a5fe508e78bc9
JP Cimalando <jpcima@users.noreply.github.com>
2018-06-03 14:50:53 +0200

    optional high-quality resampling

--- src/chips/nuked_opl3.h
+++ src/chips/nuked_opl3.h
@@ -3,12 +3,18 @@
 
 #include "opl_chip_base.h"
 
+#if defined(ADLMIDI_ENABLE_HQ_RESAMPLER)
+class VResampler;
+#endif
+
 class NukedOPL3 final : public OPLChipBase
 {
     void *m_chip;
+#if defined(ADLMIDI_ENABLE_HQ_RESAMPLER)
+    VResampler *m_resampler;
+#endif
 public:
     NukedOPL3();
-    NukedOPL3(const NukedOPL3 &c);
     virtual ~NukedOPL3() override;
 
     virtual void setRate(uint32_t rate) override;
@@ -20,6 +26,11 @@ public:
     virtual int generate32(int32_t *output, size_t frames) override;
     virtual int generateAndMix32(int32_t *output, size_t frames) override;
     virtual const char *emulatorName() override;
+private:
+#if defined(ADLMIDI_ENABLE_HQ_RESAMPLER)
+    void generateResampledHq(int16_t *out);
+    void generateResampledHq32(int32_t *out);
+#endif
 };
 
 #endif // NUKED_OPL3_H
--- src/chips/nuked_opl3_v174.h
+++ src/chips/nuked_opl3_v174.h
@@ -3,12 +3,18 @@
 
 #include "opl_chip_base.h"
 
+#if defined(ADLMIDI_ENABLE_HQ_RESAMPLER)
+class VResampler;
+#endif
+
 class NukedOPL3v174 final : public OPLChipBase
 {
     void *m_chip;
+#if defined(ADLMIDI_ENABLE_HQ_RESAMPLER)
+    VResampler *m_resampler;
+#endif
 public:
     NukedOPL3v174();
-    NukedOPL3v174(const NukedOPL3v174 &c);
     virtual ~NukedOPL3v174() override;
 
     virtual void setRate(uint32_t rate) override;
@@ -20,6 +26,11 @@ public:
     virtual int generate32(int32_t *output, size_t frames) override;
     virtual int generateAndMix32(int32_t *output, size_t frames) override;
     virtual const char *emulatorName() override;
+private:
+#if defined(ADLMIDI_ENABLE_HQ_RESAMPLER)
+    void generateResampledHq(int16_t *out);
+    void generateResampledHq32(int32_t *out);
+#endif
 };
 
 #endif // NUKED_OPL3174_H
