d38e8982f3741a2e67c0f1c01a6ebad1f8a1428c
Matt Montag <montag@spotify.com>
2018-08-15 16:49:11 -0700

    Move libgme.js to src to be imported as module

--- build-gme.js
+++ build-gme.js
@@ -1,5 +1,10 @@
 var glob = require('glob');
 var spawn = require('child_process').spawn;
+var tmp = require('tmp');
+var fs = require('fs');
+
+var tmpObj = tmp.fileSync();
+fs.writeSync(tmpObj.fd, "/*eslint-disable*/\n");
 
 /**
  * Compile the C libraries with emscripten.
@@ -9,44 +14,47 @@ var gme_dir = './game_music_emu/gme';
 var source_files = glob.sync(gme_dir + '/*.cpp');
 var outfile = 'public/libgme.js';
 var exported_functions = [
-    '_gme_open_data',
-    '_gme_play',
-    '_gme_delete',
-    '_gme_mute_voices',
-    '_gme_track_count',
-    '_gme_track_ended',
-    '_gme_voice_count',
-    '_gme_track_info',
-    '_gme_start_track',
-    '_gme_open_data',
-    '_gme_ignore_silence',
-    '_gme_set_tempo'
+  '_gme_open_data',
+  '_gme_play',
+  '_gme_delete',
+  '_gme_mute_voices',
+  '_gme_track_count',
+  '_gme_track_ended',
+  '_gme_voice_count',
+  '_gme_track_info',
+  '_gme_start_track',
+  '_gme_open_data',
+  '_gme_ignore_silence',
+  '_gme_set_tempo'
 ];
 
 var flags = [
-    '-s', 'EXPORTED_FUNCTIONS=[' + exported_functions.join(',') + ']',
-    '-s', 'EXTRA_EXPORTED_RUNTIME_METHODS=[allocate,ALLOC_NORMAL,ccall,getValue,Pointer_stringify]',
-    '-s', 'ALLOW_MEMORY_GROWTH=1',
-    '-O3',
-    '-I' + gme_dir,
-    '-o', outfile,
+  '-s', 'EXPORTED_FUNCTIONS=[' + exported_functions.join(',') + ']',
+  '-s', 'EXTRA_EXPORTED_RUNTIME_METHODS=[allocate,ALLOC_NORMAL,ccall,getValue,Pointer_stringify]',
+  '-s', 'ALLOW_MEMORY_GROWTH=1',
+  '-s', 'WASM=0',
+  '-O1',
+  '-I' + gme_dir,
+  '-o', outfile,
 
-    // GCC/Clang arguments to shut up about warnings in code I didn't
-    // write. :D
-    '-Wno-deprecated',
-    '-Qunused-arguments',
-    '-DVGM_YM2612_NUKED=1',
-    '-Wno-logical-op-parentheses',
-    '-Wno-c++11-extensions',
-    '-Wno-inconsistent-missing-override',
-    '-std=c++11'
+  // Flags to eliminate linter warnings
+  '-s', 'MODULARIZE_INSTANCE=1',
+  '-s', 'EXPORT_NAME=GME',
+  '-s', 'ENVIRONMENT=web',
+  '--pre-js', tmpObj.name,
+
+  // GCC/Clang arguments to shut up about warnings in code I didn't
+  // write. :D
+  '-Wno-deprecated',
+  '-Qunused-arguments',
+  '-DVGM_YM2612_NUKED=1',
+  '-Wno-logical-op-parentheses',
+  '-Wno-c++11-extensions',
+  '-Wno-inconsistent-missing-override',
+  '-std=c++11'
 ];
 var args = [].concat(flags, source_files);
 
 console.log('Compiling to %s...', outfile);
 var begin = Date.now();
 var build_proc = spawn(empp, args, {stdio: 'inherit'});
-build_proc.on('exit', function () {
-    var end = Date.now();
-    console.log('Done in %s sec.', ((end - begin)/1000).toFixed(2));
-});
--- src/App.js
+++ src/App.js
@@ -1,15 +1,117 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import './App.css';
+import Module from './libgme';
+
+let emu = null;
+let node = null;
+const audioContext = new AudioContext();
+
+function message(str) {
+  console.log(str);
+}
+
+function playMusicData(payload, subtune) {
+  message("subtune:" + subtune);
+
+  const ref = Module.allocate(1, "i32", Module.ALLOC_NORMAL);
+  Module.allocate(1, "i32", Module.ALLOC_NORMAL);
+
+  const samplerate = audioContext.sampleRate;
+
+  if (Module.ccall("gme_open_data", "number", ["array", "number", "number", "number"], [payload, payload.length, ref, samplerate]) !== 0) {
+    console.error("gme_open_data failed.");
+    return;
+  }
+  emu = Module.getValue(ref, "i32");
+
+  var subtune_count = Module.ccall("gme_track_count", "number", ["number"], [emu]);
+
+  Module.ccall("gme_ignore_silence", "number", ["number"], [emu, 1]);
+
+  var voice_count = Module.ccall("gme_voice_count", "number", ["number"], [emu]);
+  message("Channel count: ", voice_count);
+  message("Track count: ", subtune_count);
+
+  // $("#tempo").val(1.0);
+  // $("#tempolabel").html(1.0);
+
+  if (Module.ccall("gme_start_track", "number", ["number", "number"], [emu, subtune]) !== 0)
+    message("could not load track");
+  // setChannels();
+
+  var bufferSize = 4096;
+  var inputs = 2;
+  var outputs = 2;
+
+  if (!node && audioContext.createJavaScriptNode) node = audioContext.createJavaScriptNode(bufferSize, inputs, outputs);
+  if (!node && audioContext.createScriptProcessor) node = audioContext.createScriptProcessor(bufferSize, inputs, outputs);
+
+  var buffer = Module.allocate(bufferSize * 16, "i16", Module.ALLOC_NORMAL);
+
+  var INT16_MAX = Math.pow(2, 16) - 1;
+
+  node.onaudioprocess = function (e) {
+    if (Module.ccall("gme_track_ended", "number", ["number"], [emu]) === 1) {
+      node.disconnect();
+      message("end of stream");
+      return;
+    }
+
+    var channels = [e.outputBuffer.getChannelData(0), e.outputBuffer.getChannelData(1)];
+
+    var err = Module.ccall("gme_play", "number", ["number", "number", "number"], [emu, bufferSize * 2, buffer]);
+    for (var i = 0; i < bufferSize; i++)
+      for (var channel = 0; channel < e.outputBuffer.numberOfChannels; channel++)
+        channels[channel][i] = Module.getValue(buffer +
+          // Interleaved channel format
+          // frame number * bytes per frame +
+          i * 4 +
+          // channel number * bytes per sample
+          channel * 2,
+          // 16-bit integer
+          "i16") / INT16_MAX;
+  };
+  // node.connect(filterNode);
+  node.connect(audioContext.destination);
+
+  window.savedReferences = [audioContext, node];
+}
 
 class App extends Component {
+  playSong(filename, subtune) {
+    if (node) {
+      node.disconnect();
+      node = null;
+    }
+    var xhr = new XMLHttpRequest();
+    xhr.open("GET", filename, true);
+    xhr.responseType = "arraybuffer";
+    xhr.onerror = function (e) {
+      message(e);
+    };
+    xhr.onload = function (e) {
+      if (this.status === 404) {
+        message("not found");
+        return;
+      }
+      var payload = new Uint8Array(this.response);
+      playMusicData(payload, subtune);
+      //updateSongInfo(filename, subtune);
+    };
+    xhr.send();
+  }
+
+
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <h1 className="App-title">JavaScript Game Music Player</h1>
+          <small>powered by <a href="https://bitbucket.org/mpyne/game-music-emu/wiki/Home">Game Music Emu</a></small>
         </header>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          <button onClick={this.playSong('songs/wt-01.spc')}>Play Music</button>
         </p>
       </div>
     );
