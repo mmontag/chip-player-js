91eb4559f83728765d5473300b04b051281c5207
Matt Montag <montag@spotify.com>
2018-08-14 23:48:08 -0700

    Enable gh-pages deployment; side-by-side w/ old page

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
@@ -7,8 +6,7 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">JavaScript Game Music Player</h1>
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
