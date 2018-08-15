9415b76450cb3ff92ee10c49fbdcc9a7e229f218
Matt Montag <montag@spotify.com>
2018-08-14 23:27:06 -0700

    Transitioning to React with react-create-app

---
+++ build-gme.js
@@ -0,0 +1,52 @@
+var glob = require('glob');
+var spawn = require('child_process').spawn;
+
+/**
+ * Compile the C libraries with emscripten.
+ */
+var empp = process.env.EMPP_BIN || 'em++';
+var gme_dir = './game_music_emu/gme';
+var source_files = glob.sync(gme_dir + '/*.cpp');
+var outfile = 'public/libgme.js';
+var exported_functions = [
+    '_gme_open_data',
+    '_gme_play',
+    '_gme_delete',
+    '_gme_mute_voices',
+    '_gme_track_count',
+    '_gme_track_ended',
+    '_gme_voice_count',
+    '_gme_track_info',
+    '_gme_start_track',
+    '_gme_open_data',
+    '_gme_ignore_silence',
+    '_gme_set_tempo'
+];
+
+var flags = [
+    '-s', 'EXPORTED_FUNCTIONS=[' + exported_functions.join(',') + ']',
+    '-s', 'EXTRA_EXPORTED_RUNTIME_METHODS=[allocate,ALLOC_NORMAL,ccall,getValue,Pointer_stringify]',
+    '-s', 'ALLOW_MEMORY_GROWTH=1',
+    '-O3',
+    '-I' + gme_dir,
+    '-o', outfile,
+
+    // GCC/Clang arguments to shut up about warnings in code I didn't
+    // write. :D
+    '-Wno-deprecated',
+    '-Qunused-arguments',
+    '-DVGM_YM2612_NUKED=1',
+    '-Wno-logical-op-parentheses',
+    '-Wno-c++11-extensions',
+    '-Wno-inconsistent-missing-override',
+    '-std=c++11'
+];
+var args = [].concat(flags, source_files);
+
+console.log('Compiling to %s...', outfile);
+var begin = Date.now();
+var build_proc = spawn(empp, args, {stdio: 'inherit'});
+build_proc.on('exit', function () {
+    var end = Date.now();
+    console.log('Done in %s sec.', ((end - begin)/1000).toFixed(2));
+});
