77c0101084347c5fc76689c36a9440f726b8dac8
Matt Montag <montag@spotify.com>
2018-10-05 18:14:09 -0700

    Fixup build-catalog script

--- scripts/build-catalog.js
+++ scripts/build-catalog.js
@@ -1,14 +1,13 @@
 const fs = require('fs');
 const glob = require('glob');
-
 const {listToTrie} = require('../src/ResigTrie');
 
-const root = '/Users/montag/Music/Video Game Music/chip-archive/';
-const files = glob.sync(
-  // '{Nintendo,Sega Master System,Sega Genesis,Sega Game Gear,Nintendo SNES,MIDI}/' +
-  root + '*/*/*.{spc,vgm,vgz,nsf,nsfe,mid,s3m,it,mod,xm}',
-  {nocase: true},
-).map(file => file.replace(root, ''));
+// Point this to the place where you keep all the music...
+const root = '/Users/montag/Music/Chip Archive/';
+const formats = 'spc,vgm,vgz,nsf,nsfe,mid,s3m,it,mod,xm';
+
+const files = glob.sync(`${root}**/*.{${formats}}`, {nocase: true},)
+  .map(file => file.replace(root, ''));
 
 const trie = listToTrie(files);
 fs.writeSync(fs.openSync('src/catalog.json', 'w+'), JSON.stringify(trie));
