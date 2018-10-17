c53b7a5b9946bb0aaab4bb5dffa7ca4fd41f818b
Matt Montag <montag@spotify.com>
2018-10-17 03:39:59 -0700

    Log catalog stats in build-catalog.js

--- scripts/build-catalog.js
+++ scripts/build-catalog.js
@@ -2,12 +2,15 @@ const fs = require('fs');
 const glob = require('glob');
 const {listToTrie} = require('../src/ResigTrie');
 
+const outputPath = 'public/catalog.json';
 // Point this to the place where you keep all the music...
 const root = '/Users/montag/Music/Chip Archive/';
-const formats = 'spc,vgm,vgz,nsf,nsfe,mid,s3m,it,mod,xm';
+const formats = 'spc,vgm,vgz,nsf,nsfe,mid,s3m,it,mod,xm,ay';
 
 const files = glob.sync(`${root}**/*.{${formats}}`, {nocase: true},)
   .map(file => file.replace(root, ''));
 
 const trie = listToTrie(files);
-fs.writeSync(fs.openSync('public/catalog.json', 'w+'), JSON.stringify(trie));
+const data = JSON.stringify(trie);
+fs.writeSync(fs.openSync(outputPath, 'w+'), data);
+console.log('Wrote %d entries in %s (%d bytes).', files.length, outputPath, data.length);
