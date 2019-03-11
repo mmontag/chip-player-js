15f3d3eae070ea3e685bd16cb21524729493e1e9
Matt Montag <matt.montag@gmail.com>
2019-03-10 20:09:43 -0700

    Disable dev error overlay: too clunky for wasm-related errors

--- config/webpack.config.dev.js
+++ config/webpack.config.dev.js
@@ -44,9 +44,13 @@ module.exports = {
     // Note: instead of the default WebpackDevServer client, we use a custom one
     // to bring better experience for Create React App users. You can replace
     // the line below with these two lines if you prefer the stock client:
-    // require.resolve('webpack-dev-server/client') + '?/',
-    // require.resolve('webpack/hot/dev-server'),
-    require.resolve('react-dev-utils/webpackHotDevClient'),
+
+    // dev client:
+    require.resolve('webpack-dev-server/client') + '?/',
+    require.resolve('webpack/hot/dev-server'),
+    // dev client with Error Overlay:
+    // require.resolve('react-dev-utils/webpackHotDevClient'),
+
     // Finally, this is your app's code:
     paths.appIndexJs,
     // We include the app code last so that if there is a runtime error during
