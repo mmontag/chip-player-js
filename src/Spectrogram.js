83dc022879b694a2bb26ee931b064c077efe9b06
Matt Montag <matt.montag@gmail.com>
2019-03-13 09:29:50 -0700

    Tweak A-Weighting

--- src/Spectrogram.js
+++ src/Spectrogram.js
@@ -25,7 +25,7 @@ let _lastTime = 0;
 
 function _getAWeighting(f) {
   var f2 = f*f;
-  return 1.2588966 * 148840000 * f2*f2 /
+  return 1.5 * 1.2588966 * 148840000 * f2*f2 /
     ((f2 + 424.36) * Math.sqrt((f2 + 11599.29) * (f2 + 544496.41)) * (f2 + 148840000));
 }
 
