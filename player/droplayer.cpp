ValleyBell/libvgm

switch to c-based fileloader

player/droplayer.cpp
@@ -61,24 +61,24 @@ const char* DROPlayer::GetPlayerName(void) const
 	return "DRO";
 }
 
-/*static*/ UINT8 DROPlayer::IsMyFile(FileLoader& fileLoader)
+/*static*/ UINT8 DROPlayer::IsMyFile(FileLoader *fileLoader)
 {
-	fileLoader.ReadUntil(0x10);
-	if (fileLoader.GetFileSize() < 0x10)
+	FileLoader_ReadUntil(fileLoader,0x10);
+	if (FileLoader_GetFileSize(fileLoader) < 0x10)
 		return 0xF1;	// file too small
-	if (memcmp(&fileLoader.GetFileData()[0x00], "DBRAWOPL", 8))
+	if (memcmp(FileLoader_GetFileData(fileLoader), "DBRAWOPL", 8))
 		return 0xF0;	// invalid signature
 	return 0x00;
 }
 
-UINT8 DROPlayer::LoadFile(FileLoader& fileLoader)
+UINT8 DROPlayer::LoadFile(FileLoader *fileLoader)
 {
 	UINT32 tempLng;
 	
 	_fLoad = NULL;
-	fileLoader.ReadUntil(0x10);
-	_fileData = &fileLoader.GetFileData()[0];
-	if (fileLoader.GetFileSize() < 0x10 || memcmp(&_fileData[0x00], "DBRAWOPL", 8))
+	FileLoader_ReadUntil(fileLoader,0x10);
+	_fileData = FileLoader_GetFileData(fileLoader);
+	if (FileLoader_GetFileSize(fileLoader) < 0x10 || memcmp(_fileData, "DBRAWOPL", 8))
 		return 0xF0;	// invalid file
 	
 	// --- try to detect the DRO version ---
@@ -104,9 +104,9 @@ UINT8 DROPlayer::LoadFile(FileLoader& fileLoader)
 	if (_fileHdr.verMajor > 2)
 		return 0xF1;	// unsupported version
 	
-	_fLoad = &fileLoader;
-	_fLoad->ReadFullFile();
-	_fileData = &fileLoader.GetFileData()[0];
+	_fLoad = fileLoader;
+	FileLoader_ReadFullFile(_fLoad);
+	_fileData = FileLoader_GetFileData(fileLoader);
 	
 	switch(_fileHdr.verMajor)
 	{
@@ -219,7 +219,7 @@ void DROPlayer::ScanInitBlock(void)
 		selPort = 0;
 		lastReg = 0x000;
 		// The file begins with a register dump with increasing register numbers.
-		while(filePos < _fLoad->GetFileSize())
+		while(filePos < FileLoader_GetFileSize(_fLoad))
 		{
 			curCmd = _fileData[filePos];
 			if (curCmd == 0x02 || curCmd == 0x03)
@@ -240,7 +240,7 @@ void DROPlayer::ScanInitBlock(void)
 			lastReg = curReg;
 			filePos += 0x02;
 		}
-		while(filePos < _fLoad->GetFileSize())
+		while(filePos < FileLoader_GetFileSize(_fLoad))
 		{
 			curCmd = _fileData[filePos];
 			
@@ -268,7 +268,7 @@ void DROPlayer::ScanInitBlock(void)
 	{
 		lastReg = 0x000;
 		// The file begins with a register dump with increasing register numbers.
-		while(filePos < _fLoad->GetFileSize())
+		while(filePos < FileLoader_GetFileSize(_fLoad))
 		{
 			curCmd = _fileData[filePos];
 			if (curCmd == _fileHdr.cmdDlyShort || curCmd == _fileHdr.cmdDlyLong)
@@ -292,6 +292,7 @@ UINT8 DROPlayer::UnloadFile(void)
 {
 	if (_playState & PLAYSTATE_PLAY)
 		return 0xFF;
+	FileLoader_FreeData(_fLoad);
 	
 	_playState = 0x00;
 	_fLoad = NULL;
@@ -583,7 +584,7 @@ void DROPlayer::ParseFile(UINT32 ticks)
 
 void DROPlayer::DoCommand_v1(void)
 {
-	if (_filePos >= _fLoad->GetFileSize())
+	if (_filePos >= FileLoader_GetFileSize(_fLoad))
 	{
 		DoFileEnd();
 		return;
@@ -642,7 +643,7 @@ void DROPlayer::DoCommand_v1(void)
 
 void DROPlayer::DoCommand_v2(void)
 {
-	if (_filePos >= _fLoad->GetFileSize())
+	if (_filePos >= FileLoader_GetFileSize(_fLoad))
 	{
 		DoFileEnd();
 		return;

player/s98player.cpp
@@ -88,33 +88,33 @@ const char* S98Player::GetPlayerName(void) const
 	return "S98";
 }
 
-/*static*/ UINT8 S98Player::IsMyFile(FileLoader& fileLoader)
+/*static*/ UINT8 S98Player::IsMyFile(FileLoader *fileLoader)
 {
-	fileLoader.ReadUntil(0x20);
-	if (fileLoader.GetFileSize() < 0x20)
+	FileLoader_ReadUntil(fileLoader,0x20);
+	if (FileLoader_GetFileSize(fileLoader) < 0x20)
 		return 0xF1;	// file too small
-	if (memcmp(&fileLoader.GetFileData()[0x00], "S98", 3))
+	if (memcmp(FileLoader_GetFileData(fileLoader), "S98", 3))
 		return 0xF0;	// invalid signature
 	return 0x00;
 }
 
-UINT8 S98Player::LoadFile(FileLoader& fileLoader)
+UINT8 S98Player::LoadFile(FileLoader *fileLoader)
 {
 	UINT32 devCount;
 	UINT32 curDev;
 	UINT32 curPos;
 	
 	_fLoad = NULL;
-	fileLoader.ReadUntil(0x20);
-	_fileData = &fileLoader.GetFileData()[0];
-	if (fileLoader.GetFileSize() < 0x20 || memcmp(&_fileData[0x00], "S98", 3))
+	FileLoader_ReadUntil(fileLoader,0x20);
+	_fileData = FileLoader_GetFileData(fileLoader);
+	if (FileLoader_GetFileSize(fileLoader) < 0x20 || memcmp(_fileData, "S98", 3))
 		return 0xF0;	// invalid file
 	if (! (_fileData[0x03] >= '0' && _fileData[0x03] <= '3'))
 		return 0xF1;	// unsupported version
 	
-	_fLoad = &fileLoader;
-	_fLoad->ReadFullFile();
-	_fileData = &fileLoader.GetFileData()[0];
+	_fLoad = fileLoader;
+	FileLoader_ReadFullFile(_fLoad);
+	_fileData = FileLoader_GetFileData(fileLoader);
 	
 	_fileHdr.fileVer = _fileData[0x03] - '0';
 	_fileHdr.tickMult = ReadLE32(&_fileData[0x04]);
@@ -197,7 +197,7 @@ void S98Player::CalcSongLength(void)
 	
 	fileEnd = false;
 	filePos = _fileHdr.dataOfs;
-	while(! fileEnd && filePos < _fLoad->GetFileSize())
+	while(! fileEnd && filePos < FileLoader_GetFileSize(_fLoad))
 	{
 		if (filePos == _fileHdr.loopOfs)
 			_loopTick = _totalTicks;
@@ -235,9 +235,9 @@ UINT8 S98Player::LoadTags(void)
 	
 	// find end of string (can be either '\0' or EOF)
 	startPtr = (const char*)&_fileData[_fileHdr.tagOfs];
-	endPtr = (const char*)memchr(startPtr, '\0', _fLoad->GetFileSize() - _fileHdr.tagOfs);
+	endPtr = (const char*)memchr(startPtr, '\0', FileLoader_GetFileSize(_fLoad) - _fileHdr.tagOfs);
 	if (endPtr == NULL)
-		endPtr = (const char*)_fileData + _fLoad->GetFileSize();
+		endPtr = (const char*)_fileData + FileLoader_GetFileSize(_fLoad);
 	
 	if (_fileHdr.fileVer < 3)
 	{
@@ -377,6 +377,7 @@ UINT8 S98Player::UnloadFile(void)
 {
 	if (_playState & PLAYSTATE_PLAY)
 		return 0xFF;
+	FileLoader_FreeData(_fLoad);
 	
 	_playState = 0x00;
 	_fLoad = NULL;
@@ -718,13 +719,13 @@ void S98Player::ParseFile(UINT32 ticks)
 
 void S98Player::DoCommand(void)
 {
-	if (_filePos >= _fLoad->GetFileSize())
+	if (_filePos >= FileLoader_GetFileSize(_fLoad))
 	{
 		_playState |= PLAYSTATE_END;
 		_psTrigger |= PLAYSTATE_END;
 		if (_eventCbFunc != NULL)
 			_eventCbFunc(this, _eventCbParam, PLREVT_END, NULL);
-		fprintf(stderr, "S98 file ends early! (filePos 0x%06X, fileSize 0x%06X)\n", _filePos, _fLoad->GetFileSize());
+		fprintf(stderr, "S98 file ends early! (filePos 0x%06X, fileSize 0x%06X)\n", _filePos, FileLoader_GetFileSize(_fLoad));
 		return;
 	}
 	

player/vgmplayer.cpp
@@ -137,27 +137,27 @@ const char* VGMPlayer::GetPlayerName(void) const
 	return "VGM";
 }
 
-/*static*/ UINT8 VGMPlayer::IsMyFile(FileLoader& fileLoader)
+/*static*/ UINT8 VGMPlayer::IsMyFile(FileLoader *fileLoader)
 {
-	fileLoader.ReadUntil(0x38);
-	if (fileLoader.GetFileSize() < 0x38)
+	FileLoader_ReadUntil(fileLoader,0x38);
+	if (FileLoader_GetFileSize(fileLoader) < 0x38)
 		return 0xF1;	// file too small
-	if (memcmp(&fileLoader.GetFileData()[0x00], "Vgm ", 4))
+	if (memcmp(FileLoader_GetFileData(fileLoader), "Vgm ", 4))
 		return 0xF0;	// invalid signature
 	return 0x00;
 }
 
-UINT8 VGMPlayer::LoadFile(FileLoader& fileLoader)
+UINT8 VGMPlayer::LoadFile(FileLoader *fileLoader)
 {
 	_fLoad = NULL;
-	fileLoader.ReadUntil(0x38);
-	_fileData = &fileLoader.GetFileData()[0];
-	if (fileLoader.GetFileSize() < 0x38 || memcmp(&_fileData[0x00], "Vgm ", 4))
+	FileLoader_ReadUntil(fileLoader,0x38);
+	_fileData = FileLoader_GetFileData(fileLoader);
+	if (FileLoader_GetFileSize(fileLoader) < 0x38 || memcmp(_fileData, "Vgm ", 4))
 		return 0xF0;	// invalid file
 	
-	_fLoad = &fileLoader;
-	_fLoad->ReadFullFile();
-	_fileData = &fileLoader.GetFileData()[0];
+	_fLoad = fileLoader;
+	FileLoader_ReadFullFile(_fLoad);
+	_fileData = FileLoader_GetFileData(fileLoader);
 	
 	// parse main header
 	ParseHeader();
@@ -177,20 +177,21 @@ UINT8 VGMPlayer::ParseHeader(void)
 	memset(&_fileHdr, 0x00, sizeof(VGM_HEADER));
 	
 	_fileHdr.fileVer = ReadLE32(&_fileData[0x08]);
+
 	
-	_fileHdr.dataOfs = (_fileHdr.fileVer >= 0x150) ? ReadRelOfs(&_fileData[0], 0x34) : 0x00;
+	_fileHdr.dataOfs = (_fileHdr.fileVer >= 0x150) ? ReadRelOfs(_fileData, 0x34) : 0x00;
 	if (! _fileHdr.dataOfs)
 		_fileHdr.dataOfs = 0x40;
 	_hdrLenFile = _fileHdr.dataOfs;
 	
-	_fileHdr.extraHdrOfs = (_hdrLenFile >= 0xC0) ? ReadRelOfs(&_fileData[0], 0xBC) : 0x00;
+	_fileHdr.extraHdrOfs = (_hdrLenFile >= 0xC0) ? ReadRelOfs(_fileData, 0xBC) : 0x00;
 	if (_fileHdr.extraHdrOfs && _hdrLenFile > _fileHdr.extraHdrOfs)
 		_hdrLenFile = _fileHdr.extraHdrOfs;	// the main header ends where the extra header begins
 	
 	if (_hdrLenFile > _HDR_BUF_SIZE)
 		_hdrLenFile = _HDR_BUF_SIZE;
 	memset(_hdrBuffer, 0x00, _HDR_BUF_SIZE);
-	memcpy(_hdrBuffer, &_fileData[0x00], _hdrLenFile);
+	memcpy(_hdrBuffer, _fileData, _hdrLenFile);
 	
 	_fileHdr.eofOfs = ReadRelOfs(_hdrBuffer, 0x04);
 	_fileHdr.gd3Ofs = ReadRelOfs(_hdrBuffer, 0x14);
@@ -214,13 +215,13 @@ UINT8 VGMPlayer::ParseHeader(void)
 		
 		xhLen = ReadLE32(&_fileData[_fileHdr.extraHdrOfs]);
 		if (xhLen >= 0x08)
-			_fileHdr.xhChpClkOfs = ReadRelOfs(&_fileData[0], _fileHdr.extraHdrOfs + 0x04);
+			_fileHdr.xhChpClkOfs = ReadRelOfs(_fileData, _fileHdr.extraHdrOfs + 0x04);
 		if (xhLen >= 0x0C)
-			_fileHdr.xhChpVolOfs = ReadRelOfs(&_fileData[0], _fileHdr.extraHdrOfs + 0x08);
+			_fileHdr.xhChpVolOfs = ReadRelOfs(_fileData, _fileHdr.extraHdrOfs + 0x08);
 	}
 	
-	if (! _fileHdr.eofOfs || _fileHdr.eofOfs > _fLoad->GetFileSize())
-		_fileHdr.eofOfs = _fLoad->GetFileSize();	// catch invalid EOF values
+	if (! _fileHdr.eofOfs || _fileHdr.eofOfs > FileLoader_GetFileSize(_fLoad))
+		_fileHdr.eofOfs = FileLoader_GetFileSize(_fLoad);	// catch invalid EOF values
 	_fileHdr.dataEnd = _fileHdr.eofOfs;
 	// command data ends at the GD3 offset if:
 	//	GD3 is used && GD3 offset < EOF (just to be sure) && GD3 offset > dataOfs (catch files with GD3 between header and data)
@@ -233,7 +234,7 @@ UINT8 VGMPlayer::ParseHeader(void)
 void VGMPlayer::ParseXHdr_Data32(UINT32 fileOfs, std::vector<XHDR_DATA32>& xData)
 {
 	xData.clear();
-	if (! fileOfs || fileOfs >= _fLoad->GetFileSize())
+	if (! fileOfs || fileOfs >= FileLoader_GetFileSize(_fLoad))
 		return;
 	
 	UINT32 curPos = fileOfs;
@@ -242,7 +243,7 @@ void VGMPlayer::ParseXHdr_Data32(UINT32 fileOfs, std::vector<XHDR_DATA32>& xData
 	xData.resize(_fileData[curPos]);	curPos ++;
 	for (curChip = 0; curChip < xData.size(); curChip ++, curPos += 0x05)
 	{
-		if (curPos + 0x05 > _fLoad->GetFileSize())
+		if (curPos + 0x05 > FileLoader_GetFileSize(_fLoad))
 		{
 			xData.resize(curChip);
 			break;
@@ -259,7 +260,7 @@ void VGMPlayer::ParseXHdr_Data32(UINT32 fileOfs, std::vector<XHDR_DATA32>& xData
 void VGMPlayer::ParseXHdr_Data16(UINT32 fileOfs, std::vector<XHDR_DATA16>& xData)
 {
 	xData.clear();
-	if (! fileOfs || fileOfs >= _fLoad->GetFileSize())
+	if (! fileOfs || fileOfs >= FileLoader_GetFileSize(_fLoad))
 		return;
 	
 	UINT32 curPos = fileOfs;
@@ -268,7 +269,7 @@ void VGMPlayer::ParseXHdr_Data16(UINT32 fileOfs, std::vector<XHDR_DATA16>& xData
 	xData.resize(_fileData[curPos]);	curPos ++;
 	for (curChip = 0; curChip < xData.size(); curChip ++, curPos += 0x04)
 	{
-		if (curPos + 0x04 > _fLoad->GetFileSize())
+		if (curPos + 0x04 > FileLoader_GetFileSize(_fLoad))
 		{
 			xData.resize(curChip);
 			break;
@@ -343,6 +344,7 @@ UINT8 VGMPlayer::UnloadFile(void)
 {
 	if (_playState & PLAYSTATE_PLAY)
 		return 0xFF;
+	FileLoader_FreeData(_fLoad);
 	
 	_playState = 0x00;
 	_fLoad = NULL;

player/vgmplayer.hpp
@@ -7,7 +7,7 @@
 #include "../utils/StrUtils.h"
 #include "helper.h"
 #include "playerbase.hpp"
-#include "../utils/FileLoader.hpp"
+#include "../utils/FileLoader.h"
 #include "dblk_compr.h"
 #include <vector>
 #include <string>
@@ -116,8 +116,8 @@ public:
 	
 	UINT32 GetPlayerType(void) const;
 	const char* GetPlayerName(void) const;
-	static UINT8 IsMyFile(FileLoader& fileLoader);
-	UINT8 LoadFile(FileLoader& fileLoader);
+	static UINT8 IsMyFile(FileLoader *fileLoader);
+	UINT8 LoadFile(FileLoader *fileLoader);
 	UINT8 UnloadFile(void);
 	const VGM_HEADER* GetFileHeader(void) const;
 	const char* GetSongTitle(void);
@@ -215,7 +215,7 @@ private:
 	void Cmd_AY_Stereo(void);				// command 30 - set AY8910 stereo mask
 	
 	CPCONV* _cpcUTF16;	// UTF-16 LE -> UTF-8 codepage conversion
-	FileLoader* _fLoad;
+	FileLoader *_fLoad;
 	const UINT8* _fileData;	// data pointer for quick access, equals _fLoad->GetFileData().data()
 	
 	enum

utils/FileLoader.h
@@ -1,0 +1,73 @@
+#ifndef __FILELOADER_H__
+#define __FILELOADER_H__
+
+#include <stdtype.h>
+#include <stdio.h>
+#include <zlib.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+union LoaderHandles
+{
+	const void *dataPtr;
+    FILE *hFileRaw;
+    gzFile hFileGZ;
+};
+
+typedef union LoaderHandles LoaderHandles;
+
+enum
+{
+	FLSTAT_EMPTY = 0,
+	FLSTAT_LOADING = 1,
+	FLSTAT_LOADED = 2
+};
+
+enum
+{
+	// mode: sources
+	FLMODE_SRC_MEMORY = 0x01,
+	FLMODE_SRC_FILE = 0x02,
+
+	// mode: compression
+	FLMODE_CMP_RAW = 0x00,
+	FLMODE_CMP_GZ = 0x10
+};
+
+struct FileLoader {
+	UINT8 _modeSrc;
+	UINT8 _modeCompr;
+	UINT8 _status;
+	UINT32 _bytesTotal;
+	UINT32 _bytesLoaded;
+    UINT32 _readStopOfs;
+    LoaderHandles _hLoad[1];
+    UINT8 *_data;
+};
+
+typedef struct FileLoader FileLoader;
+
+FileLoader *FileLoader_New(void);
+void FileLoader_Delete(FileLoader *loader);
+UINT8 FileLoader_Init(FileLoader *loader);
+UINT8 *FileLoader_GetFileData(FileLoader *loader);
+UINT32 FileLoader_GetTotalFileSize(FileLoader *loader);
+UINT32 FileLoader_GetFileSize(FileLoader *loader);
+UINT8 FileLoader_GetMode(FileLoader *loader);
+UINT8 FileLoader_GetStatus(FileLoader *loader);
+UINT8 FileLoader_ReadData(FileLoader *loader, UINT32 numBytes);
+UINT8 FileLoader_CancelLoading(FileLoader *loader);
+UINT8 FileLoader_LoadData(FileLoader *loader, UINT32 dataSize, const UINT8 *data);
+UINT8 FileLoader_LoadFile(FileLoader *loader, const char *fileName);
+UINT8 FileLoader_FreeData(FileLoader *loader);
+void FileLoader_SetPreloadBytes(FileLoader *loader, UINT32 byteCount);
+void FileLoader_ReadUntil(FileLoader *loader, UINT32 fileOffset);
+void FileLoader_ReadFullFile(FileLoader *loader);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* __FILELOADER_H__ */

mmontag/chip-player-js

player/droplayer.cpp
@@ -61,24 +61,24 @@ const char* DROPlayer::GetPlayerName(void) const
 	return "DRO";
 }
 
-/*static*/ UINT8 DROPlayer::IsMyFile(FileLoader& fileLoader)
+/*static*/ UINT8 DROPlayer::IsMyFile(FILE_LOADER *fileLoader)
 {
-	fileLoader.ReadUntil(0x10);
-	if (fileLoader.GetFileSize() < 0x10)
+	FileLoader_ReadUntil(fileLoader,0x10);
+	if (FileLoader_GetFileSize(fileLoader) < 0x10)
 		return 0xF1;	// file too small
-	if (memcmp(&fileLoader.GetFileData()[0x00], "DBRAWOPL", 8))
+	if (memcmp(FileLoader_GetFileData(fileLoader), "DBRAWOPL", 8))
 		return 0xF0;	// invalid signature
 	return 0x00;
 }
 
-UINT8 DROPlayer::LoadFile(FileLoader& fileLoader)
+UINT8 DROPlayer::LoadFile(FILE_LOADER *fileLoader)
 {
 	UINT32 tempLng;
 	
 	_fLoad = NULL;
-	fileLoader.ReadUntil(0x10);
-	_fileData = &fileLoader.GetFileData()[0];
-	if (fileLoader.GetFileSize() < 0x10 || memcmp(&_fileData[0x00], "DBRAWOPL", 8))
+	FileLoader_ReadUntil(fileLoader,0x10);
+	_fileData = FileLoader_GetFileData(fileLoader);
+	if (FileLoader_GetFileSize(fileLoader) < 0x10 || memcmp(_fileData, "DBRAWOPL", 8))
 		return 0xF0;	// invalid file
 	
 	// --- try to detect the DRO version ---
@@ -104,9 +104,9 @@ UINT8 DROPlayer::LoadFile(FileLoader& fileLoader)
 	if (_fileHdr.verMajor > 2)
 		return 0xF1;	// unsupported version
 	
-	_fLoad = &fileLoader;
-	_fLoad->ReadFullFile();
-	_fileData = &fileLoader.GetFileData()[0];
+	_fLoad = fileLoader;
+	FileLoader_ReadFullFile(_fLoad);
+	_fileData = FileLoader_GetFileData(fileLoader);
 	
 	switch(_fileHdr.verMajor)
 	{
@@ -219,7 +219,7 @@ void DROPlayer::ScanInitBlock(void)
 		selPort = 0;
 		lastReg = 0x000;
 		// The file begins with a register dump with increasing register numbers.
-		while(filePos < _fLoad->GetFileSize())
+		while(filePos < FileLoader_GetFileSize(_fLoad))
 		{
 			curCmd = _fileData[filePos];
 			if (curCmd == 0x02 || curCmd == 0x03)
@@ -240,7 +240,7 @@ void DROPlayer::ScanInitBlock(void)
 			lastReg = curReg;
 			filePos += 0x02;
 		}
-		while(filePos < _fLoad->GetFileSize())
+		while(filePos < FileLoader_GetFileSize(_fLoad))
 		{
 			curCmd = _fileData[filePos];
 			
@@ -268,7 +268,7 @@ void DROPlayer::ScanInitBlock(void)
 	{
 		lastReg = 0x000;
 		// The file begins with a register dump with increasing register numbers.
-		while(filePos < _fLoad->GetFileSize())
+		while(filePos < FileLoader_GetFileSize(_fLoad))
 		{
 			curCmd = _fileData[filePos];
 			if (curCmd == _fileHdr.cmdDlyShort || curCmd == _fileHdr.cmdDlyLong)
@@ -583,7 +583,7 @@ void DROPlayer::ParseFile(UINT32 ticks)
 
 void DROPlayer::DoCommand_v1(void)
 {
-	if (_filePos >= _fLoad->GetFileSize())
+	if (_filePos >= FileLoader_GetFileSize(_fLoad))
 	{
 		DoFileEnd();
 		return;
@@ -642,7 +642,7 @@ void DROPlayer::DoCommand_v1(void)
 
 void DROPlayer::DoCommand_v2(void)
 {
-	if (_filePos >= _fLoad->GetFileSize())
+	if (_filePos >= FileLoader_GetFileSize(_fLoad))
 	{
 		DoFileEnd();
 		return;

player/s98player.cpp
@@ -88,33 +88,33 @@ const char* S98Player::GetPlayerName(void) const
 	return "S98";
 }
 
-/*static*/ UINT8 S98Player::IsMyFile(FileLoader& fileLoader)
+/*static*/ UINT8 S98Player::IsMyFile(FILE_LOADER *fileLoader)
 {
-	fileLoader.ReadUntil(0x20);
-	if (fileLoader.GetFileSize() < 0x20)
+	FileLoader_ReadUntil(fileLoader,0x20);
+	if (FileLoader_GetFileSize(fileLoader) < 0x20)
 		return 0xF1;	// file too small
-	if (memcmp(&fileLoader.GetFileData()[0x00], "S98", 3))
+	if (memcmp(FileLoader_GetFileData(fileLoader), "S98", 3))
 		return 0xF0;	// invalid signature
 	return 0x00;
 }
 
-UINT8 S98Player::LoadFile(FileLoader& fileLoader)
+UINT8 S98Player::LoadFile(FILE_LOADER *fileLoader)
 {
 	UINT32 devCount;
 	UINT32 curDev;
 	UINT32 curPos;
 	
 	_fLoad = NULL;
-	fileLoader.ReadUntil(0x20);
-	_fileData = &fileLoader.GetFileData()[0];
-	if (fileLoader.GetFileSize() < 0x20 || memcmp(&_fileData[0x00], "S98", 3))
+	FileLoader_ReadUntil(fileLoader,0x20);
+	_fileData = FileLoader_GetFileData(fileLoader);
+	if (FileLoader_GetFileSize(fileLoader) < 0x20 || memcmp(_fileData, "S98", 3))
 		return 0xF0;	// invalid file
 	if (! (_fileData[0x03] >= '0' && _fileData[0x03] <= '3'))
 		return 0xF1;	// unsupported version
 	
-	_fLoad = &fileLoader;
-	_fLoad->ReadFullFile();
-	_fileData = &fileLoader.GetFileData()[0];
+	_fLoad = fileLoader;
+	FileLoader_ReadFullFile(_fLoad);
+	_fileData = FileLoader_GetFileData(fileLoader);
 	
 	_fileHdr.fileVer = _fileData[0x03] - '0';
 	_fileHdr.tickMult = ReadLE32(&_fileData[0x04]);
@@ -197,7 +197,7 @@ void S98Player::CalcSongLength(void)
 	
 	fileEnd = false;
 	filePos = _fileHdr.dataOfs;
-	while(! fileEnd && filePos < _fLoad->GetFileSize())
+	while(! fileEnd && filePos < FileLoader_GetFileSize(_fLoad))
 	{
 		if (filePos == _fileHdr.loopOfs)
 			_loopTick = _totalTicks;
@@ -235,9 +235,9 @@ UINT8 S98Player::LoadTags(void)
 	
 	// find end of string (can be either '\0' or EOF)
 	startPtr = (const char*)&_fileData[_fileHdr.tagOfs];
-	endPtr = (const char*)memchr(startPtr, '\0', _fLoad->GetFileSize() - _fileHdr.tagOfs);
+	endPtr = (const char*)memchr(startPtr, '\0', FileLoader_GetFileSize(_fLoad) - _fileHdr.tagOfs);
 	if (endPtr == NULL)
-		endPtr = (const char*)_fileData + _fLoad->GetFileSize();
+		endPtr = (const char*)_fileData + FileLoader_GetFileSize(_fLoad);
 	
 	if (_fileHdr.fileVer < 3)
 	{
@@ -718,13 +718,13 @@ void S98Player::ParseFile(UINT32 ticks)
 
 void S98Player::DoCommand(void)
 {
-	if (_filePos >= _fLoad->GetFileSize())
+	if (_filePos >= FileLoader_GetFileSize(_fLoad))
 	{
 		_playState |= PLAYSTATE_END;
 		_psTrigger |= PLAYSTATE_END;
 		if (_eventCbFunc != NULL)
 			_eventCbFunc(this, _eventCbParam, PLREVT_END, NULL);
-		fprintf(stderr, "S98 file ends early! (filePos 0x%06X, fileSize 0x%06X)\n", _filePos, _fLoad->GetFileSize());
+		fprintf(stderr, "S98 file ends early! (filePos 0x%06X, fileSize 0x%06X)\n", _filePos, FileLoader_GetFileSize(_fLoad));
 		return;
 	}
 	

player/vgmplayer.cpp
@@ -137,27 +137,27 @@ const char* VGMPlayer::GetPlayerName(void) const
 	return "VGM";
 }
 
-/*static*/ UINT8 VGMPlayer::IsMyFile(FileLoader& fileLoader)
+/*static*/ UINT8 VGMPlayer::IsMyFile(FILE_LOADER *fileLoader)
 {
-	fileLoader.ReadUntil(0x38);
-	if (fileLoader.GetFileSize() < 0x38)
+	FileLoader_ReadUntil(fileLoader,0x38);
+	if (FileLoader_GetFileSize(fileLoader) < 0x38)
 		return 0xF1;	// file too small
-	if (memcmp(&fileLoader.GetFileData()[0x00], "Vgm ", 4))
+	if (memcmp(FileLoader_GetFileData(fileLoader), "Vgm ", 4))
 		return 0xF0;	// invalid signature
 	return 0x00;
 }
 
-UINT8 VGMPlayer::LoadFile(FileLoader& fileLoader)
+UINT8 VGMPlayer::LoadFile(FILE_LOADER *fileLoader)
 {
 	_fLoad = NULL;
-	fileLoader.ReadUntil(0x38);
-	_fileData = &fileLoader.GetFileData()[0];
-	if (fileLoader.GetFileSize() < 0x38 || memcmp(&_fileData[0x00], "Vgm ", 4))
+	FileLoader_ReadUntil(fileLoader,0x38);
+	_fileData = FileLoader_GetFileData(fileLoader);
+	if (FileLoader_GetFileSize(fileLoader) < 0x38 || memcmp(_fileData, "Vgm ", 4))
 		return 0xF0;	// invalid file
 	
-	_fLoad = &fileLoader;
-	_fLoad->ReadFullFile();
-	_fileData = &fileLoader.GetFileData()[0];
+	_fLoad = fileLoader;
+	FileLoader_ReadFullFile(_fLoad);
+	_fileData = FileLoader_GetFileData(fileLoader);
 	
 	// parse main header
 	ParseHeader();
@@ -177,20 +177,21 @@ UINT8 VGMPlayer::ParseHeader(void)
 	memset(&_fileHdr, 0x00, sizeof(VGM_HEADER));
 	
 	_fileHdr.fileVer = ReadLE32(&_fileData[0x08]);
+
 	
-	_fileHdr.dataOfs = (_fileHdr.fileVer >= 0x150) ? ReadRelOfs(&_fileData[0], 0x34) : 0x00;
+	_fileHdr.dataOfs = (_fileHdr.fileVer >= 0x150) ? ReadRelOfs(_fileData, 0x34) : 0x00;
 	if (! _fileHdr.dataOfs)
 		_fileHdr.dataOfs = 0x40;
 	_hdrLenFile = _fileHdr.dataOfs;
 	
-	_fileHdr.extraHdrOfs = (_hdrLenFile >= 0xC0) ? ReadRelOfs(&_fileData[0], 0xBC) : 0x00;
+	_fileHdr.extraHdrOfs = (_hdrLenFile >= 0xC0) ? ReadRelOfs(_fileData, 0xBC) : 0x00;
 	if (_fileHdr.extraHdrOfs && _hdrLenFile > _fileHdr.extraHdrOfs)
 		_hdrLenFile = _fileHdr.extraHdrOfs;	// the main header ends where the extra header begins
 	
 	if (_hdrLenFile > _HDR_BUF_SIZE)
 		_hdrLenFile = _HDR_BUF_SIZE;
 	memset(_hdrBuffer, 0x00, _HDR_BUF_SIZE);
-	memcpy(_hdrBuffer, &_fileData[0x00], _hdrLenFile);
+	memcpy(_hdrBuffer, _fileData, _hdrLenFile);
 	
 	_fileHdr.eofOfs = ReadRelOfs(_hdrBuffer, 0x04);
 	_fileHdr.gd3Ofs = ReadRelOfs(_hdrBuffer, 0x14);
@@ -214,13 +215,13 @@ UINT8 VGMPlayer::ParseHeader(void)
 		
 		xhLen = ReadLE32(&_fileData[_fileHdr.extraHdrOfs]);
 		if (xhLen >= 0x08)
-			_fileHdr.xhChpClkOfs = ReadRelOfs(&_fileData[0], _fileHdr.extraHdrOfs + 0x04);
+			_fileHdr.xhChpClkOfs = ReadRelOfs(_fileData, _fileHdr.extraHdrOfs + 0x04);
 		if (xhLen >= 0x0C)
-			_fileHdr.xhChpVolOfs = ReadRelOfs(&_fileData[0], _fileHdr.extraHdrOfs + 0x08);
+			_fileHdr.xhChpVolOfs = ReadRelOfs(_fileData, _fileHdr.extraHdrOfs + 0x08);
 	}
 	
-	if (! _fileHdr.eofOfs || _fileHdr.eofOfs > _fLoad->GetFileSize())
-		_fileHdr.eofOfs = _fLoad->GetFileSize();	// catch invalid EOF values
+	if (! _fileHdr.eofOfs || _fileHdr.eofOfs > FileLoader_GetFileSize(_fLoad))
+		_fileHdr.eofOfs = FileLoader_GetFileSize(_fLoad);	// catch invalid EOF values
 	_fileHdr.dataEnd = _fileHdr.eofOfs;
 	// command data ends at the GD3 offset if:
 	//	GD3 is used && GD3 offset < EOF (just to be sure) && GD3 offset > dataOfs (catch files with GD3 between header and data)
@@ -233,7 +234,7 @@ UINT8 VGMPlayer::ParseHeader(void)
 void VGMPlayer::ParseXHdr_Data32(UINT32 fileOfs, std::vector<XHDR_DATA32>& xData)
 {
 	xData.clear();
-	if (! fileOfs || fileOfs >= _fLoad->GetFileSize())
+	if (! fileOfs || fileOfs >= FileLoader_GetFileSize(_fLoad))
 		return;
 	
 	UINT32 curPos = fileOfs;
@@ -242,7 +243,7 @@ void VGMPlayer::ParseXHdr_Data32(UINT32 fileOfs, std::vector<XHDR_DATA32>& xData
 	xData.resize(_fileData[curPos]);	curPos ++;
 	for (curChip = 0; curChip < xData.size(); curChip ++, curPos += 0x05)
 	{
-		if (curPos + 0x05 > _fLoad->GetFileSize())
+		if (curPos + 0x05 > FileLoader_GetFileSize(_fLoad))
 		{
 			xData.resize(curChip);
 			break;
@@ -259,7 +260,7 @@ void VGMPlayer::ParseXHdr_Data32(UINT32 fileOfs, std::vector<XHDR_DATA32>& xData
 void VGMPlayer::ParseXHdr_Data16(UINT32 fileOfs, std::vector<XHDR_DATA16>& xData)
 {
 	xData.clear();
-	if (! fileOfs || fileOfs >= _fLoad->GetFileSize())
+	if (! fileOfs || fileOfs >= FileLoader_GetFileSize(_fLoad))
 		return;
 	
 	UINT32 curPos = fileOfs;
@@ -268,7 +269,7 @@ void VGMPlayer::ParseXHdr_Data16(UINT32 fileOfs, std::vector<XHDR_DATA16>& xData
 	xData.resize(_fileData[curPos]);	curPos ++;
 	for (curChip = 0; curChip < xData.size(); curChip ++, curPos += 0x04)
 	{
-		if (curPos + 0x04 > _fLoad->GetFileSize())
+		if (curPos + 0x04 > FileLoader_GetFileSize(_fLoad))
 		{
 			xData.resize(curChip);
 			break;

player/vgmplayer.hpp
@@ -7,7 +7,7 @@
 #include "../utils/StrUtils.h"
 #include "helper.h"
 #include "playerbase.hpp"
-#include "../utils/FileLoader.hpp"
+#include "../utils/FileLoader.h"
 #include "dblk_compr.h"
 #include <vector>
 #include <string>
@@ -116,8 +116,8 @@ public:
 	
 	UINT32 GetPlayerType(void) const;
 	const char* GetPlayerName(void) const;
-	static UINT8 IsMyFile(FileLoader& fileLoader);
-	UINT8 LoadFile(FileLoader& fileLoader);
+	static UINT8 IsMyFile(FILE_LOADER *fileLoader);
+	UINT8 LoadFile(FILE_LOADER *fileLoader);
 	UINT8 UnloadFile(void);
 	const VGM_HEADER* GetFileHeader(void) const;
 	const char* GetSongTitle(void);
@@ -215,7 +215,7 @@ private:
 	void Cmd_AY_Stereo(void);				// command 30 - set AY8910 stereo mask
 	
 	CPCONV* _cpcUTF16;	// UTF-16 LE -> UTF-8 codepage conversion
-	FileLoader* _fLoad;
+	FILE_LOADER *_fLoad;
 	const UINT8* _fileData;	// data pointer for quick access, equals _fLoad->GetFileData().data()
 	
 	enum

utils/FileLoader.h
@@ -1,0 +1,73 @@
+#ifndef __FILELOADER_H__
+#define __FILELOADER_H__
+
+#include <stdtype.h>
+#include <stdio.h>
+#include <zlib.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+union LoaderHandles
+{
+	const void *dataPtr;
+	FILE *hFileRaw;
+	gzFile hFileGZ;
+};
+
+typedef union LoaderHandles LoaderHandles;
+
+enum
+{
+	FLSTAT_EMPTY = 0,
+	FLSTAT_LOADING = 1,
+	FLSTAT_LOADED = 2
+};
+
+enum
+{
+	// mode: sources
+	FLMODE_SRC_MEMORY = 0x01,
+	FLMODE_SRC_FILE = 0x02,
+
+	// mode: compression
+	FLMODE_CMP_RAW = 0x00,
+	FLMODE_CMP_GZ = 0x10
+};
+
+struct FileLoader {
+	UINT8 _modeSrc;
+	UINT8 _modeCompr;
+	UINT8 _status;
+	UINT32 _bytesTotal;
+	UINT32 _bytesLoaded;
+	UINT32 _readStopOfs;
+	LoaderHandles _hLoad[1];
+	UINT8 *_data;
+};
+
+typedef struct FileLoader FILE_LOADER;
+
+FILE_LOADER *FileLoader_New(void);
+void FileLoader_Delete(FILE_LOADER *loader);
+UINT8 FileLoader_Init(FILE_LOADER *loader);
+UINT8 *FileLoader_GetFileData(FILE_LOADER *loader);
+UINT32 FileLoader_GetTotalFileSize(FILE_LOADER *loader);
+UINT32 FileLoader_GetFileSize(FILE_LOADER *loader);
+UINT8 FileLoader_GetMode(FILE_LOADER *loader);
+UINT8 FileLoader_GetStatus(FILE_LOADER *loader);
+UINT8 FileLoader_ReadData(FILE_LOADER *loader, UINT32 numBytes);
+UINT8 FileLoader_CancelLoading(FILE_LOADER *loader);
+UINT8 FileLoader_LoadData(FILE_LOADER *loader, UINT32 dataSize, const UINT8 *data);
+UINT8 FileLoader_LoadFile(FILE_LOADER *loader, const char *fileName);
+UINT8 FileLoader_FreeData(FILE_LOADER *loader);
+void FileLoader_SetPreloadBytes(FILE_LOADER *loader, UINT32 byteCount);
+void FileLoader_ReadUntil(FILE_LOADER *loader, UINT32 fileOffset);
+void FileLoader_ReadFullFile(FILE_LOADER *loader);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* __FILELOADER_H__ */