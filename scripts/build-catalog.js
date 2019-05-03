49533ebc29195a6cad2dceda5ca06df15530acfd
Matt Montag <matt.montag@gmail.com>
2019-05-02 20:26:50 -0700

    Create directories.json directory tree in build-catalog

--- scripts/build-catalog.js
+++ scripts/build-catalog.js
@@ -1,6 +1,7 @@
 const fs = require('fs');
 const glob = require('glob');
 const {listToTrie} = require('../src/ResigTrie');
+const directoryTree = require('directory-tree');
 
 // Point this to the place where you keep all the music.
 // this location is untracked, so put a symlink here.
@@ -9,7 +10,9 @@ const {listToTrie} = require('../src/ResigTrie');
 const catalogRoot = 'catalog/';
 const outputPath = 'public/catalog.json';
 const trieOutputPath = 'public/catalog-trie.json';
+const dirDictOutputPath = 'public/directories.json';
 const formats = 'spc,vgm,vgz,nsf,nsfe,mid,s3m,it,mod,xm,ay';
+const formatsRegex = new RegExp(`\\.(${formats.split(',').join('|')})$`);
 
 if(!fs.existsSync(catalogRoot)) {
     console.log('Couldn\'t find a music folder for indexing. Create a folder or symlink at \'%s\'.', catalogRoot);
@@ -19,7 +22,7 @@ if(!fs.existsSync(catalogRoot)) {
 const files = glob.sync(`${catalogRoot}**/*.{${formats}}`, {nocase: true},)
   .map(file => file.replace(catalogRoot, ''));
 
-const data = JSON.stringify(files);
+const data = JSON.stringify(files, null, 2);
 fs.writeSync(fs.openSync(outputPath, 'w+'), data);
 console.log('Wrote %d entries in %s (%d bytes).', files.length, outputPath, data.length);
 
@@ -28,3 +31,25 @@ const trie = listToTrie(files);
 const trieData = JSON.stringify(trie);
 fs.writeSync(fs.openSync(trieOutputPath, 'w+'), trieData);
 console.log('Wrote %d entries in %s (%d bytes).', files.length, trieOutputPath, trieData.length);
+
+const dirDict = {};
+directoryTree(catalogRoot, { extensions: formatsRegex }, null, item => {
+    if (item.children) {
+        item.children = item.children.map(child => {
+            child.path = child.path.replace(catalogRoot, '/');
+            if (child.children) {
+                child.numChildren = child.children.length;
+                delete child.children;
+            }
+            delete child.name;
+            delete child.extension;
+            return child;
+        });
+        item.path = item.path.replace(catalogRoot, '/');
+        dirDict[item.path] = item.children;
+    }
+});
+
+const dirDictData = JSON.stringify(dirDict, null, 2);
+fs.writeSync(fs.openSync(dirDictOutputPath, 'w+'), dirDictData);
+console.log('Wrote %d entries in %s (%d bytes).', Object.keys(dirDict).length, dirDictOutputPath, dirDictData.length);
