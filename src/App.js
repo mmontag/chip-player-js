7bc1c2cc4ebca96765f66ab0ac70c9faa0d1994f
Matt Montag <matt.montag@gmail.com>
2019-05-05 22:20:42 -0700

    MP3 path fix (single-page app routing)

--- src/App.js
+++ src/App.js
@@ -239,7 +239,7 @@ class App extends React.Component {
 
       // Limitations of MediaSession: there must always be an active audio element :(
       const audio = document.createElement('audio');
-      audio.src = './5-seconds-of-silence.mp3';
+      audio.src = process.env.PUBLIC_URL + '/5-seconds-of-silence.mp3';
       audio.loop = true;
       audio.volume = 0;
       audio.play();
