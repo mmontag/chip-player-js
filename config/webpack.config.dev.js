4ab2da2b37fea111a0130456d17302b802cdc220
Matt Montag <matt.montag@gmail.com>
2019-05-26 16:16:28 -0700

    Add .txt raw-loader (currently unused)

--- config/webpack.config.dev.js
+++ config/webpack.config.dev.js
@@ -193,6 +193,10 @@ module.exports = {
               },
             ],
           },
+          {
+            test: /\.txt$/i,
+            use: 'raw-loader',
+          },
           // "file" loader makes sure those assets get served by WebpackDevServer.
           // When you `import` an asset, you get its (virtual) filename.
           // In production, they would get copied to the `build` folder.
--- config/webpack.config.prod.js
+++ config/webpack.config.prod.js
@@ -213,6 +213,10 @@ module.exports = {
             ),
             // Note: this won't work without `new ExtractTextPlugin()` in `plugins`.
           },
+          {
+            test: /\.txt$/i,
+            use: 'raw-loader',
+          },
           // "file" loader makes sure assets end up in the `build` folder.
           // When you `import` an asset, you get its filename.
           // This loader doesn't use a "test" so it will catch all modules
