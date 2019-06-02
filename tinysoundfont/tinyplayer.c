c021816274ed42d78b048e571ad657d4cd887cdb
Matt Montag <matt.montag@gmail.com>
2019-06-02 14:52:36 -0700

    Create common interface to MIDI synth in tinyplayer
    Remove refs to TinySoundFont (never implemented)

--- tinysoundfont/tinyplayer.c
+++ tinysoundfont/tinyplayer.c
@@ -6,23 +6,24 @@
 #define TML_NO_STDIO
 #define TML_IMPLEMENTATION
 
-#include "tml.h"
-
-#define TSF_IMPLEMENTATION
-
-#include "tsf.h"
-
-#include "../fluidlite/include/fluidlite.h"
-
+#include <math.h>
 #include <emscripten.h>
+#include "tml.h"
+#include "../fluidlite/include/fluidlite.h"
+#include "../libADLMIDI/include/adlmidi.h"
 
 #ifdef __cplusplus
 extern "C" {
 #endif
 
 //TODO: Remove debug logging (EM_ASM_)
-tsf *g_TSF;                  // instance of TinySoundFont
 fluid_synth_t *g_FluidSynth; // instance of FluidSynth
+struct ADL_MIDIPlayer *g_adlSynth;
+struct ADLMIDI_AudioFormat adl_AudioFormat = {
+        ADLMIDI_SampleType_F32,
+        sizeof(float_t),
+        2 * sizeof(float_t),
+};
 tml_message *g_MidiEvt;      // next message to be played
 tml_message *g_FirstEvt;     // first event in current file
 double g_MidiTimeMs;         // current playback time
@@ -32,9 +33,106 @@ unsigned int g_DurationMs = 0;
 char g_ChannelsInUse[16];
 char g_ChannelsMuted[16];
 int g_ChannelProgramNums[16];
+short g_shortBuffer[4096];
+int tp_set_synth_engine(int);
+
+typedef struct Synth {
+  void (*noteOn)(int channel, int key, int velocity);
+  void (*noteOff)(int channel, int key);
+  void (*programChange)(int channel, int program);
+  void (*pitchBend)(int channel, int value);
+  void (*controlChange)(int channel, int control, int value);
+  void (*channelPressure)(int channel, int value);
+  void (*render)(float *buffer, int samples);
+  void (*panic)();
+  void (*panicChannel)(int channel);
+  void (*reset)();
+} Synth;
+
+int g_synthId = 0;
+static const int NUM_SYNTHS = 2;
+Synth g_Synths[NUM_SYNTHS];
+Synth g_synth;
+
+// Fluid Synth *********************************************
+
+void fluidNoteOn(int channel, int key, int velocity) {
+  fluid_synth_noteon(g_FluidSynth, channel, key, velocity);
+}
+void fluidNoteOff(int channel, int key) {
+  fluid_synth_noteoff(g_FluidSynth, channel, key);
+}
+void fluidProgramChange(int channel, int program) {
+  fluid_synth_program_change(g_FluidSynth, channel, program);
+}
+void fluidPitchBend(int channel, int pitch) {
+  fluid_synth_pitch_bend(g_FluidSynth, channel, pitch);
+}
+void fluidControlChange(int channel, int control, int value) {
+  fluid_synth_cc(g_FluidSynth, channel, control, value);
+}
+void fluidChannelPressure(int channel, int value) {
+  fluid_synth_channel_pressure(g_FluidSynth, channel, value);
+}
+void fluidRender(float *buffer, int samples) {
+  int frames = samples / 2;
+  fluid_synth_write_float(g_FluidSynth, frames, buffer, 0, 2, buffer, 1, 2); // interleaved stereo
+}
+void fluidPanic() {
+  for (int i = 0; i < 16; i++)
+    fluid_synth_all_notes_off(g_FluidSynth, i);
+}
+void fluidPanicChannel(int channel) {
+  fluid_synth_all_notes_off(g_FluidSynth, channel);
+}
+void fluidReset() {
+  fluid_synth_system_reset(g_FluidSynth);
+  fluid_synth_program_reset(g_FluidSynth);
+}
+Synth fluidSynth = {fluidNoteOn, fluidNoteOff, fluidProgramChange, fluidPitchBend, fluidControlChange,
+                    fluidChannelPressure, fluidRender, fluidPanic, fluidPanicChannel, fluidReset};
+
+// ADL OPL3 Synth *********************************************
+
+void adlNoteOn(int channel, int key, int velocity) {
+  adl_rt_noteOn(g_adlSynth, channel, key, velocity);
+}
+void adlNoteOff(int channel, int key) {
+  adl_rt_noteOff(g_adlSynth, channel, key);
+}
+void adlProgramChange(int channel, int program) {
+  adl_rt_patchChange(g_adlSynth, channel, program);
+}
+void adlPitchBend(int channel, int pitch) {
+  adl_rt_pitchBend(g_adlSynth, channel, pitch);
+}
+void adlControlChange(int channel, int control, int value) {
+  adl_rt_controllerChange(g_adlSynth, channel, control, value);
+}
+void adlChannelPressure(int channel, int value) {
+  adl_rt_channelPressure(g_adlSynth, channel, value);
+}
+void adlRender(float *buffer, int samples) {
+  adl_generateFormat(g_adlSynth, samples, (ADL_UInt8 *)buffer, (ADL_UInt8 *)(buffer + 1), &adl_AudioFormat);
+}
+void adlPanic() {
+  adl_panic(g_adlSynth);
+}
+void adlPanicChannel(int channel) {
+  // Hack: ADLMIDI doesn't have a channel notes-off method
+  for (int i = 0; i < 128; i++)
+    adl_rt_noteOff(g_adlSynth, channel, i);
+}
+void adlReset() {
+  adl_rt_resetState(g_adlSynth);
+};
+Synth adlSynth = {adlNoteOn, adlNoteOff, adlProgramChange, adlPitchBend, adlControlChange,
+                  adlChannelPressure, adlRender, adlPanic, adlPanicChannel, adlReset};
 
 // TODO: TinySoundFont not implemented
 extern void tp_init(int sampleRate) {
+  g_SampleRate = sampleRate;
+
   fluid_settings_t *settings = new_fluid_settings();
   fluid_settings_setstr(settings, "synth.reverb.active", "yes");
   fluid_settings_setstr(settings, "synth.chorus.active", "no");
@@ -42,9 +140,17 @@ extern void tp_init(int sampleRate) {
   fluid_settings_setnum(settings, "synth.gain", 0.5);
   fluid_settings_setnum(settings, "synth.sample-rate", sampleRate);
   g_FluidSynth = new_fluid_synth(settings);
-  fluid_synth_set_interp_method(g_FluidSynth, -1, FLUID_INTERP_LINEAR);
-  g_SampleRate = sampleRate;
+    fluid_synth_set_interp_method(g_FluidSynth, -1, FLUID_INTERP_LINEAR);
+
+  g_adlSynth = adl_init(sampleRate);
+    adl_setSoftPanEnabled(g_adlSynth, 1);
+    adl_setNumChips(g_adlSynth, 4);
+
+  g_Synths[0] = fluidSynth;
+  g_Synths[1] = adlSynth;
+  g_synth = g_Synths[0];
 }
+
 
 // Returns the number of bytes written. Value of 0 means the song has ended.
 extern int tp_write_audio(float *buffer, int bufferSize) {
@@ -63,52 +169,30 @@ extern int tp_write_audio(float *buffer, int bufferSize) {
       switch (g_MidiEvt->type) {
         case TML_NOTE_ON:
           if (g_ChannelsMuted[g_MidiEvt->channel]) break;
-          if (g_FluidSynth) {
-            fluid_synth_noteon(g_FluidSynth, g_MidiEvt->channel, g_MidiEvt->key, g_MidiEvt->velocity);
-          } else {
-            tsf_channel_note_on(g_TSF, g_MidiEvt->channel, g_MidiEvt->key, g_MidiEvt->velocity / 127.0f);
-          }
+          g_synth.noteOn(g_MidiEvt->channel, g_MidiEvt->key, g_MidiEvt->velocity);
           break;
         case TML_NOTE_OFF:
           if (g_ChannelsMuted[g_MidiEvt->channel]) break;
-          if (g_FluidSynth) {
-            fluid_synth_noteoff(g_FluidSynth, g_MidiEvt->channel, g_MidiEvt->key);
-          } else {
-            tsf_channel_note_off(g_TSF, g_MidiEvt->channel, g_MidiEvt->key);
-          }
+          g_synth.noteOff(g_MidiEvt->channel, g_MidiEvt->key);
           break;
         case TML_PROGRAM_CHANGE: // program change (special handling for 10th MIDI channel with drums)
-          if (g_FluidSynth) {
-            fluid_synth_program_change(g_FluidSynth, g_MidiEvt->channel, g_MidiEvt->program);
-          } else {
-            tsf_channel_set_presetnumber(g_TSF, g_MidiEvt->channel, g_MidiEvt->program, g_MidiEvt->channel == 9);
-          }
+          g_synth.programChange(g_MidiEvt->channel, g_MidiEvt->program);
           break;
         case TML_PITCH_BEND:
-          if (g_FluidSynth) {
-            fluid_synth_pitch_bend(g_FluidSynth, g_MidiEvt->channel, g_MidiEvt->pitch_bend);
-          } else {
-            tsf_channel_set_pitchwheel(g_TSF, g_MidiEvt->channel, g_MidiEvt->pitch_bend);
-          }
+          g_synth.pitchBend(g_MidiEvt->channel, g_MidiEvt->pitch_bend);
           break;
         case TML_CONTROL_CHANGE:
-          if (g_FluidSynth) {
-            fluid_synth_cc(g_FluidSynth, g_MidiEvt->channel, g_MidiEvt->control, g_MidiEvt->control_value);
-          } else {
-            tsf_channel_midi_control(g_TSF, g_MidiEvt->channel, g_MidiEvt->control, g_MidiEvt->control_value);
-          }
+          g_synth.controlChange(g_MidiEvt->channel, g_MidiEvt->control, g_MidiEvt->control_value);
           break;
+        case TML_CHANNEL_PRESSURE:
+          g_synth.channelPressure(g_MidiEvt->channel, g_MidiEvt->channel_pressure);
         default:
           break;
       }
 
     }
     // Render the block of audio samples in float format
-    if (g_FluidSynth) {
-      fluid_synth_write_float(g_FluidSynth, batchSize / 2, buffer, 0, 2, buffer, 1, 2); // interleaved stereo
-    } else {
-      tsf_render_float(g_TSF, buffer, batchSize / 2, 0);
-    }
+    g_synth.render(buffer, batchSize);
 
     buffer += batchSize;
     bytesWritten += batchSize;
@@ -147,37 +231,20 @@ extern void tp_seek(int ms) {
     g_MidiEvt = g_FirstEvt;
   }
 
-  if (g_FluidSynth) {
-    for (int i = 0; i < 16; i++)
-      fluid_synth_all_notes_off(g_FluidSynth, i);
-  } else {
-    tsf_note_off_all(g_TSF);
-  }
+  g_synth.panic();
 
   while (g_MidiEvt && g_MidiEvt->time < ms) {
     switch (g_MidiEvt->type) {
       // Ignore note on/note off events during seek
       case TML_PROGRAM_CHANGE: // program change (special handling for 10th MIDI channel with drums)
-        if (g_FluidSynth) {
-          fluid_synth_program_change(g_FluidSynth, g_MidiEvt->channel, g_MidiEvt->program);
-        } else {
-          tsf_channel_set_presetnumber(g_TSF, g_MidiEvt->channel, g_MidiEvt->program, g_MidiEvt->channel == 9);
-        }
+        g_synth.programChange(g_MidiEvt->channel, g_MidiEvt->program);
         break;
       case TML_PITCH_BEND:
-        if (g_FluidSynth) {
-          fluid_synth_pitch_bend(g_FluidSynth, g_MidiEvt->channel, g_MidiEvt->pitch_bend);
-        } else {
-          tsf_channel_set_pitchwheel(g_TSF, g_MidiEvt->channel, g_MidiEvt->pitch_bend);
-        }
+        g_synth.pitchBend(g_MidiEvt->channel, g_MidiEvt->pitch_bend);
         break;
       case TML_CONTROL_CHANGE:
-        if (g_FluidSynth) {
-          if (g_MidiEvt->control == 91) break; // ignore reverb CC from MIDI files
-          fluid_synth_cc(g_FluidSynth, g_MidiEvt->channel, g_MidiEvt->control, g_MidiEvt->control_value);
-        } else {
-          tsf_channel_midi_control(g_TSF, g_MidiEvt->channel, g_MidiEvt->control, g_MidiEvt->control_value);
-        }
+        if (g_MidiEvt->control == 91) break; // ignore reverb CC from MIDI files
+        g_synth.controlChange(g_MidiEvt->channel, g_MidiEvt->control, g_MidiEvt->control_value);
         break;
       default:
         break;
@@ -197,53 +264,19 @@ extern void tp_set_speed(float speed) {
 }
 
 extern void tp_stop() {
-  if (g_FluidSynth) {
-    for (int i = 0; i < 16; i++)
-      fluid_synth_all_notes_off(g_FluidSynth, i);
-  } else {
-    tsf_reset(g_TSF);
-  }
+  g_synth.panic();
   g_MidiEvt = NULL;
 }
 
 extern void tp_restart() {
-  if (g_FluidSynth) {
-    for (int i = 0; i < 16; i++)
-      fluid_synth_all_notes_off(g_FluidSynth, i);
-  } else {
-    tsf_reset(g_TSF);
-  }
+  g_synth.panic();
   g_MidiEvt = g_FirstEvt;
 }
 
-// TODO: TinySoundFont not currently used
-extern void tp_open_tsf(tsf *t, const void *data, int length, int sampleRate) {
-  g_MidiTimeMs = 0;
-  g_DurationMs = 0;
-  g_TSF = t;
-  g_FluidSynth = NULL;
-  g_SampleRate = sampleRate;
-
-  tsf_set_output(g_TSF, TSF_STEREO_INTERLEAVED, sampleRate, 0.0);
-  // Channel 10 uses percussion sound bank
-  tsf_channel_set_bank_preset(g_TSF, 9, 128, 0);
-  g_MidiEvt = tml_load_memory(data, length);
-  g_FirstEvt = g_MidiEvt;
-
-  // Skip to first note to eliminate silence
-  unsigned int firstNoteTimeMs;
-  tml_get_info(g_FirstEvt, NULL, NULL, NULL, &firstNoteTimeMs, NULL);
-  g_MidiTimeMs = (double)firstNoteTimeMs;
-
-  EM_ASM_({ console.log('Tiny MIDI Player loaded %d bytes.', $0); }, length);
-  EM_ASM_({ console.log('First note appears at %d ms.', $0); }, g_MidiTimeMs);
-}
-
 extern void tp_open(const void *data, int length) {
+  g_synth.reset();
   g_MidiTimeMs = 0;
   g_DurationMs = 0;
-  fluid_synth_system_reset(g_FluidSynth);
-  fluid_synth_program_reset(g_FluidSynth);
   g_MidiEvt = tml_load_memory(data, length);
   g_FirstEvt = g_MidiEvt;
   memset(g_ChannelsInUse, 0, sizeof g_ChannelsInUse);
@@ -253,7 +286,7 @@ extern void tp_open(const void *data, int length) {
   // Skip to first note to eliminate silence
   unsigned int firstNoteTimeMs;
   tml_get_info(g_FirstEvt, NULL, NULL, NULL, &firstNoteTimeMs, NULL);
-  g_MidiTimeMs = (double)firstNoteTimeMs;
+  g_MidiTimeMs = (double) firstNoteTimeMs;
 
   EM_ASM_({ console.log('Tiny MIDI Player loaded %d bytes.', $0); }, length);
   EM_ASM_({ console.log('First note appears at %d ms.', $0); }, g_MidiTimeMs);
@@ -268,7 +301,6 @@ extern void tp_unload_soundfont() {
   }
 }
 
-// TODO: TinySoundFont not implemented
 extern int tp_load_soundfont(const char *filename) {
   tp_unload_soundfont();
   return fluid_synth_sfload(g_FluidSynth, filename, 1);
@@ -303,8 +335,25 @@ extern int tp_get_channel_program(int chan) {
 extern void tp_set_channel_mute(int chan, char isMuted) {
   g_ChannelsMuted[chan] = isMuted;
   if (isMuted) {
-    fluid_synth_all_notes_off(g_FluidSynth, chan);
+    g_synth.panicChannel(chan);
   }
+}
+
+extern int tp_set_bank(int bank) {
+  return adl_setBank(g_adlSynth, bank);
+}
+
+extern int tp_set_synth_engine(int synthId) {
+  if (g_synthId == synthId) return 0;
+  if (synthId < 0 || synthId > NUM_SYNTHS) return -1;
+  g_synth.panic();
+  g_synth.reset();
+  g_synthId = synthId;
+  g_synth = g_Synths[g_synthId];
+  // restore state
+  if (g_MidiEvt != NULL)
+    tp_seek((int)tp_get_position_ms() - 1);
+  return 0;
 }
 
 #ifdef __cplusplus
