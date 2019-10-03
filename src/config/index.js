a6b37dba19b3acadaa1d56981ab2fe7ba86f33ff
soltune <k@psycube.com>
2019-10-03 16:21:00 +0900

    added .env sample for user-specific environments

--- src/config/index.js
+++ src/config/index.js
@@ -1,9 +1,7 @@
-export const API_BASE = 'https://gifx.co/chip';
-export const CATALOG_PREFIX = 'https://gifx.co/music/';
-export const SOUNDFONT_URL_PATH = 'https://gifx.co/soundfonts/';
-// export const API_BASE = 'http://localhost:8080';
-// export const CATALOG_PREFIX = 'http://localhost:3000/catalog/';
-// export const SOUNDFONT_URL_PATH = 'http://localhost:3000/soundfonts/';
+export const API_BASE = process.env.REACT_APP_API_BASE;
+export const CATALOG_PREFIX = process.env.REACT_APP_CATALOG_PREFIX;
+export const SOUNDFONT_URL_PATH = process.env.REACT_APP_SOUNDFONT_URL_PATH;
+
 export const MAX_VOICES = 64;
 export const REPLACE_STATE_ON_SEEK = false;
 export const FORMATS =  [
