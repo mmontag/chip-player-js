979c1feea924fb6cd9959c3d506896401ff76e7c
Matt Montag <matt.montag@gmail.com>
2020-03-21 19:20:08 -0700

    Revert "Use module import for encoding-japanese"
    This reverts commit 64e330e4a24d7ac8f387cf3c8251808b307e85e3.

--- src/players/GMEPlayer.js
+++ src/players/GMEPlayer.js
@@ -1,7 +1,7 @@
 import Player from "./Player.js";
 import App from "../App";
 import SubBass from "../effects/SubBass";
-import encoding from "encoding-japanese";
+const encoding = require('encoding-japanese');
 
 let emu = null;
 let libgme = null;
