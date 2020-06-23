447e6587b1e792f555293d00319263fb294116c3
Matt Montag <matt.montag@gmail.com>
2020-06-22 20:13:25 -0700

    Support high sample rates in libxmp

--- libxmp/include/xmp.h
+++ libxmp/include/xmp.h
@@ -106,7 +106,7 @@ extern "C" {
 #define XMP_MAX_ENV_POINTS	32	/* Max number of envelope points */
 #define XMP_MAX_MOD_LENGTH	256	/* Max number of patterns in module */
 #define XMP_MAX_CHANNELS	64	/* Max number of channels in module */
-#define XMP_MAX_SRATE		49170	/* max sampling rate (Hz) */
+#define XMP_MAX_SRATE		192000	/* max sampling rate (Hz) */
 #define XMP_MIN_SRATE		4000	/* min sampling rate (Hz) */
 #define XMP_MIN_BPM		20	/* min BPM */
 /* frame rate = (50 * bpm / 125) Hz */
