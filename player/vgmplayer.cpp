mmontag/chip-player-js

commit 5c412b2b95e80038f6269de212acde34e84fb674

VGMPlayer: access SetPanning via rwFuncs

player/vgmplayer.cpp
@@ -1136,6 +1136,7 @@ void VGMPlayer::InitDevices(void)
 		CHIP_DEVICE chipDev;
 		DEV_INFO* devInf;
 		PLR_DEV_OPTS* devOpts;
+		DEVFUNC_PANALL funcPan;
 		UINT8 retVal;
 		
 		memset(&chipDev, 0x00, sizeof(CHIP_DEVICE));
@@ -1337,24 +1338,25 @@ void VGMPlayer::InitDevices(void)
 				devInf->devDef->SetOptionBits(devInf->dataPtr, devOpts->coreOpts);
 			RefreshMuting(chipDev, devOpts->muteOpts);
 		}
-		if (devInf->devDef->SetPanning != NULL)
+		retVal = SndEmu_GetDeviceFunc(devInf->devDef, RWF_CHN_PAN | RWF_WRITE, DEVRW_ALL, 0, (void**)&funcPan);
+		if (funcPan != NULL)
 		{
 			if (chipType == DEVID_SN76496)
 			{
 				INT16 panPos[4] = {0x00, -0x80, +0x80, 0x00};
-				devInf->devDef->SetPanning(devInf->dataPtr, panPos);
+				funcPan(devInf->dataPtr, panPos);
 			}
 			else if (chipType == DEVID_YM2413)
 			{
 				INT16 panPos[14] = {
 					-0x100, +0x100, -0x80, +0x80, -0x40, +0x40, -0xC0, +0xC0, 0x00,
 					-0x60, +0x60, 0x00, -0xC0, +0xC0};
-				devInf->devDef->SetPanning(devInf->dataPtr, panPos);
+				funcPan(devInf->dataPtr, panPos);
 			}
 			else if (chipType == DEVID_AY8910)
 			{
 				INT16 panPos[3] = {-0x80, +0x80, 0x00};
-				devInf->devDef->SetPanning(devInf->dataPtr, panPos);
+				funcPan(devInf->dataPtr, panPos);
 			}
 		}
 		

vgmtest.c
@@ -480,6 +480,7 @@ static void InitVGMChips(void)
 	VGM_LINKCDEV* clDev;
 	DEV_INFO* devInf;
 	UINT8 retVal;
+	DEVFUNC_PANALL funcPan;
 	
 	memset(&VGMChips, 0x00, sizeof(VGMChips));
 	vgmHdrArr = (UINT8*)&VGMHdr;	// can't use VGMData due to the data offset
@@ -532,10 +533,11 @@ static void InitVGMChips(void)
 					break;
 				SndEmu_GetDeviceFunc(cDev->defInf.devDef, RWF_REGISTER | RWF_WRITE, DEVRW_A8D8, 0, (void**)&cDev->write8);
 				
-				if (cDev->defInf.devDef->SetPanning != NULL)
+				SndEmu_GetDeviceFunc(cDev->defInf.devDef, RWF_CHN_PAN | RWF_WRITE, DEVRW_ALL, 0, (void**)&funcPan);
+				if (funcPan != NULL)
 				{
 					INT16 panPos[4] = {0x00, -0x80, +0x80, 0x00};
-					cDev->defInf.devDef->SetPanning(cDev->defInf.dataPtr, panPos);
+					funcPan(cDev->defInf.dataPtr, panPos);
 				}
 			}
 			break;
@@ -626,10 +628,11 @@ static void InitVGMChips(void)
 					break;
 				SndEmu_GetDeviceFunc(cDev->defInf.devDef, RWF_REGISTER | RWF_WRITE, DEVRW_A8D8, 0, (void**)&cDev->write8);
 				
-				if (cDev->defInf.devDef->SetPanning != NULL)
+				SndEmu_GetDeviceFunc(cDev->defInf.devDef, RWF_CHN_PAN | RWF_WRITE, DEVRW_ALL, 0, (void**)&funcPan);
+				if (funcPan != NULL)
 				{
 					INT16 panPos[3] = {-0x80, +0x80, 0x00};
-					cDev->defInf.devDef->SetPanning(cDev->defInf.dataPtr, panPos);
+					funcPan(cDev->defInf.dataPtr, panPos);
 				}
 			}
 			break;

emu/EmuStructs.h
@@ -90,7 +90,7 @@ struct _device_definition
 	
 	DEVFUNC_OPTMASK SetOptionBits;
 	DEVFUNC_OPTMASK SetMuteMask;
-	DEVFUNC_PANALL SetPanning;
+	DEVFUNC_PANALL SetPanning;		// **NOTE: deprecated, moved to rwFuncs**
 	DEVFUNC_SRCCB SetSRateChgCB;	// used to set callback function for realtime sample rate changes
 	DEVFUNC_LINKDEV LinkDevice;		// used to link multiple devices together
 	

commit 16b4ca5df943e3927e5a8bc3e15c487a0dbfe7b0

fix crash introduced in 5c412b2

player/vgmplayer.cpp
@@ -1347,7 +1347,8 @@ void VGMPlayer::InitDevices(void)
 				devInf->devDef->SetOptionBits(devInf->dataPtr, devOpts->coreOpts);
 			RefreshMuting(chipDev, devOpts->muteOpts);
 		}
-		retVal = SndEmu_GetDeviceFunc(devInf->devDef, RWF_CHN_PAN | RWF_WRITE, DEVRW_ALL, 0, (void**)&funcPan);
+		funcPan = NULL;
+		SndEmu_GetDeviceFunc(devInf->devDef, RWF_CHN_PAN | RWF_WRITE, DEVRW_ALL, 0, (void**)&funcPan);
 		if (funcPan != NULL)
 		{
 			if (chipType == DEVID_SN76496)

vgmtest.c
@@ -533,6 +533,7 @@ static void InitVGMChips(void)
 					break;
 				SndEmu_GetDeviceFunc(cDev->defInf.devDef, RWF_REGISTER | RWF_WRITE, DEVRW_A8D8, 0, (void**)&cDev->write8);
 				
+				funcPan = NULL;
 				SndEmu_GetDeviceFunc(cDev->defInf.devDef, RWF_CHN_PAN | RWF_WRITE, DEVRW_ALL, 0, (void**)&funcPan);
 				if (funcPan != NULL)
 				{
@@ -628,6 +629,7 @@ static void InitVGMChips(void)
 					break;
 				SndEmu_GetDeviceFunc(cDev->defInf.devDef, RWF_REGISTER | RWF_WRITE, DEVRW_A8D8, 0, (void**)&cDev->write8);
 				
+				funcPan = NULL;
 				SndEmu_GetDeviceFunc(cDev->defInf.devDef, RWF_CHN_PAN | RWF_WRITE, DEVRW_ALL, 0, (void**)&funcPan);
 				if (funcPan != NULL)
 				{