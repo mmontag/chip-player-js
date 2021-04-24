769ebe0ff94c56c74f0f42b9cb1e3ef7a5c98692
Matt Montag <matt.montag@gmail.com>
2021-04-23 20:38:06 -1000

    Update local development paths in config/index.js
    Flew to Hawaii today, updated stuff to work on the plane.

--- src/config/index.js
+++ src/config/index.js
@@ -1,9 +1,9 @@
 const API_BASE = 'https://gifx.co/chip';
 const CATALOG_PREFIX = 'https://gifx.co/music/';
 const SOUNDFONT_URL_PATH = 'https://gifx.co/soundfonts/';
-// const API_BASE = 'http://localhost:8080';
-// const CATALOG_PREFIX = 'http://localhost:9000/catalog/';
-// const SOUNDFONT_URL_PATH = 'http://localhost:3000/soundfonts/';
+// const API_BASE = 'http://localhost:8080';                       // npm run server - Node.js server on port 8080
+// const CATALOG_PREFIX = 'http://localhost:8000/catalog/';        // python scripts/httpserver.py - Python file server
+// const SOUNDFONT_URL_PATH = 'http://localhost:3000/soundfonts/'; // Webpack dev server
 const MAX_VOICES = 64;
 const REPLACE_STATE_ON_SEEK = false;
 const FORMATS =  [
