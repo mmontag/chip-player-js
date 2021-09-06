c54c8bd00b9478fd90d9d4f4bd00fdbdbdfda430
Matt Montag <matt.montag@gmail.com>
2021-09-06 16:46:52 -0700

    Move DirectoryLink inline styles to css classes

--- src/DirectoryLink.js
+++ src/DirectoryLink.js
@@ -5,8 +5,8 @@ import React from "react";
 import queryString from 'querystring';
 
 export default function DirectoryLink(props) {
-  const linkStyle = props.dim ? css.linkDim : null;
-  const folderStyle = props.dim ? css.folderImgDim : css.folderImg;
+  const linkClassName = props.dim ? 'DirectoryLink-dim' : null;
+  const folderClassName = props.dim ? 'DirectoryLink-folderIconDim' : 'DirectoryLink-folderIcon';
   const urlParams = queryString.parse(window.location.search.substr(1));
   delete urlParams.q;
   // Double encode % because react-router will decode this into history.
@@ -15,23 +15,10 @@ export default function DirectoryLink(props) {
   // ...is not released in react-router-dom 5.2.0 which uses history 4.10
   const to = props.to.replace('%25', '%2525');
   const search = queryString.stringify(urlParams);
-  return <Link to={{ pathname: to, search: search }} style={linkStyle}>
-    <img alt='folder' style={folderStyle} src={folder}/>{props.children}
+  return <Link to={{ pathname: to, search: search }} className={linkClassName}>
+    <img alt='folder' className={folderClassName} src={folder}/>{props.children}
   </Link>;
 }
-
-const css = {
-  folderImg: {
-    verticalAlign: 'bottom',
-  },
-  folderImgDim: {
-    verticalAlign: 'bottom',
-    filter: 'grayscale() brightness(0.75)',
-  },
-  linkDim: {
-    color: 'var(--neutral2)',
-  },
-};
 
 DirectoryLink.propTypes = {
   to: PropTypes.string.isRequired,
