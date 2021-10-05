mmontag/chip-player-js

commit 41320b21e45375011b125e1e08b5a61fc4790a67

update some sound cores to current version
- NSFPlay NES cores
- C140/C219 (minor register readback fix from MAME)
- Nuked OPLL

emu/cores/np_nes_apu.c
@@ -29,6 +29,7 @@ enum
 	OPT_NONLINEAR_MIXER,
 	OPT_PHASE_REFRESH,
 	OPT_DUTY_SWAP,
+	OPT_NEGATE_SWEEP_INIT,
 	OPT_END
 };
 
@@ -54,6 +55,7 @@ struct _NES_APU
 	UINT32 rate, clock;
 
 	INT32 square_table[32];		// nonlinear mixer
+	INT32 square_linear;		// linear mix approximation
 
 	int scounter[2];			// frequency divider
 	int sphase[2];				// phase counter
@@ -156,7 +158,6 @@ void NES_APU_np_FrameSequence(void* chip, int s)
 				if (apu->freq[i] >= 8 && apu->sfreq[i] < 0x800 && apu->sweep_amount[i] > 0) // update frequency if appropriate
 				{
 					apu->freq[i] = apu->sfreq[i] < 0 ? 0 : apu->sfreq[i];
-					if (apu->scounter[i] > apu->freq[i]) apu->scounter[i] = apu->freq[i];
 				}
 				apu->sweep_div[i] = apu->sweep_div_period[i] + 1;
 
@@ -181,16 +182,16 @@ static INT32 calc_sqr(NES_APU* apu, int i, UINT32 clocks)
 		{0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0},
 		{1, 1, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1}
 	};
-	INT32 ret = 0;
+	INT32 ret;
 
-	apu->scounter[i] += clocks;
-	while (apu->scounter[i] > apu->freq[i])
+	apu->scounter[i] -= clocks;
+	while (apu->scounter[i] < 0)
 	{
 		apu->sphase[i] = (apu->sphase[i] + 1) & 15;
-		apu->scounter[i] -= (apu->freq[i] + 1);
+		apu->scounter[i] += apu->freq[i] + 1;
 	}
 
-	//INT32 ret = 0;
+	ret = 0;
 	if (apu->length_counter[i] > 0 &&
 		apu->freq[i] >= 8 &&
 		apu->sfreq[i] < 0x800
@@ -244,10 +245,8 @@ UINT32 NES_APU_np_Render(void* chip, INT32 b[2])
 
 	if(apu->option[OPT_NONLINEAR_MIXER])
 	{
-		INT32 voltage;
 		INT32 ref;
-		
-		voltage = apu->square_table[apu->out[0] + apu->out[1]];
+		INT32 voltage = apu->square_table[apu->out[0] + apu->out[1]];
 		m[0] = apu->out[0] << 6;
 		m[1] = apu->out[1] << 6;
 		ref = m[0] + m[1];
@@ -264,8 +263,8 @@ UINT32 NES_APU_np_Render(void* chip, INT32 b[2])
 	}
 	else
 	{
-		m[0] = apu->out[0] << 6;
-		m[1] = apu->out[1] << 6;
+		m[0] = (apu->out[0] * apu->square_linear) / 15;
+		m[1] = (apu->out[1] * apu->square_linear) / 15;
 	}
 
 	// Shifting is (x-2) to match the volume of MAME's NES APU sound core
@@ -295,10 +294,13 @@ void* NES_APU_np_Create(UINT32 clock, UINT32 rate)
 	apu->option[OPT_PHASE_REFRESH] = true;
 	apu->option[OPT_NONLINEAR_MIXER] = true;
 	apu->option[OPT_DUTY_SWAP] = false;
+	apu->option[OPT_NEGATE_SWEEP_INIT] = false;
 
 	apu->square_table[0] = 0;
 	for(i=1;i<32;i++)
 		apu->square_table[i]=(INT32)((8192.0*95.88)/(8128.0/i+100));
+
+	apu->square_linear = apu->square_table[15]; // match linear scale to one full volume square of nonlinear
 
 	for(c=0;c<2;++c)
 		for(t=0;t<2;++t)
@@ -341,6 +343,11 @@ void NES_APU_np_Reset(void* chip)
 	NES_APU_np_Write(apu, 0x4015, 0);
 	if (apu->option[OPT_UNMUTE_ON_RESET])
 		NES_APU_np_Write(apu, 0x4015, 0x0f);
+	if (apu->option[OPT_NEGATE_SWEEP_INIT])
+	{
+		NES_APU_np_Write(apu, 0x4001, 0x08);
+		NES_APU_np_Write(apu, 0x4005, 0x08);
+	}
 
 	for (i = 0; i < 2; i++)
 		apu->out[i] = 0;
@@ -448,13 +455,11 @@ bool NES_APU_np_Write(void* chip, UINT16 adr, UINT8 val)
 		case 0x6:
 			apu->freq[ch] = val | (apu->freq[ch] & 0x700) ;
 			sweep_sqr(apu, ch);
-			if (apu->scounter[ch] > apu->freq[ch]) apu->scounter[ch] = apu->freq[ch];
 			break;
 
 		case 0x3:
 		case 0x7:
 			apu->freq[ch] = (apu->freq[ch] & 0xFF) | ((val & 0x7) << 8) ;
-
 			if (apu->option[OPT_PHASE_REFRESH])
 				apu->sphase[ch] = 0;
 			apu->envelope_write[ch] = true;
@@ -463,7 +468,6 @@ bool NES_APU_np_Write(void* chip, UINT16 adr, UINT8 val)
 				apu->length_counter[ch] = length_table[(val >> 3) & 0x1f];
 			}
 			sweep_sqr(apu, ch);
-			if (apu->scounter[ch] > apu->freq[ch]) apu->scounter[ch] = apu->freq[ch];
 			break;
 
 		default:

emu/cores/np_nes_dmc.c
@@ -33,6 +33,8 @@ enum
 	OPT_RANDOMIZE_NOISE,
 	OPT_TRI_MUTE,
 	OPT_TRI_NULL,
+	OPT_RANDOMIZE_TRI,
+	OPT_DPCM_REVERSE,
 	OPT_END
 };
 
@@ -57,8 +59,9 @@ struct _NES_DMC
 	const UINT8* memory;
 	UINT32 out[3];
 	UINT32 daddress;
-	UINT32 length;
+	UINT32 dlength;
 	UINT32 data;
+	bool empty;
 	INT16 damp;
 	int dac_lsb;
 	bool dmc_pop;
@@ -69,7 +72,6 @@ struct _NES_DMC
 	int pal;
 	int mode;
 	bool irq;
-	bool active;
 
 	UINT32 counter[3];	// frequency dividers
 	int tphase;			// triangle phase
@@ -93,7 +95,7 @@ struct _NES_DMC
 	int envelope_div;
 	int envelope_counter;
 
-	bool enable[3];
+	bool enable[2];	// tri/noise enable
 	int length_counter[2];	// 0=tri, 1=noise
 
 	// frame sequencer
@@ -132,6 +134,25 @@ static const UINT32 freq_table[2][16] = {
 	398, 354, 316, 298, 276, 236, 210, 198, 176, 148, 132, 118,  98,  78,  66,  50
 }};
 
+static const UINT32 BITREVERSE[256] = {
+	0x00, 0x80, 0x40, 0xC0, 0x20, 0xA0, 0x60, 0xE0, 0x10, 0x90, 0x50, 0xD0, 0x30, 0xB0, 0x70, 0xF0,
+	0x08, 0x88, 0x48, 0xC8, 0x28, 0xA8, 0x68, 0xE8, 0x18, 0x98, 0x58, 0xD8, 0x38, 0xB8, 0x78, 0xF8,
+	0x04, 0x84, 0x44, 0xC4, 0x24, 0xA4, 0x64, 0xE4, 0x14, 0x94, 0x54, 0xD4, 0x34, 0xB4, 0x74, 0xF4,
+	0x0C, 0x8C, 0x4C, 0xCC, 0x2C, 0xAC, 0x6C, 0xEC, 0x1C, 0x9C, 0x5C, 0xDC, 0x3C, 0xBC, 0x7C, 0xFC,
+	0x02, 0x82, 0x42, 0xC2, 0x22, 0xA2, 0x62, 0xE2, 0x12, 0x92, 0x52, 0xD2, 0x32, 0xB2, 0x72, 0xF2,
+	0x0A, 0x8A, 0x4A, 0xCA, 0x2A, 0xAA, 0x6A, 0xEA, 0x1A, 0x9A, 0x5A, 0xDA, 0x3A, 0xBA, 0x7A, 0xFA,
+	0x06, 0x86, 0x46, 0xC6, 0x26, 0xA6, 0x66, 0xE6, 0x16, 0x96, 0x56, 0xD6, 0x36, 0xB6, 0x76, 0xF6,
+	0x0E, 0x8E, 0x4E, 0xCE, 0x2E, 0xAE, 0x6E, 0xEE, 0x1E, 0x9E, 0x5E, 0xDE, 0x3E, 0xBE, 0x7E, 0xFE,
+	0x01, 0x81, 0x41, 0xC1, 0x21, 0xA1, 0x61, 0xE1, 0x11, 0x91, 0x51, 0xD1, 0x31, 0xB1, 0x71, 0xF1,
+	0x09, 0x89, 0x49, 0xC9, 0x29, 0xA9, 0x69, 0xE9, 0x19, 0x99, 0x59, 0xD9, 0x39, 0xB9, 0x79, 0xF9,
+	0x05, 0x85, 0x45, 0xC5, 0x25, 0xA5, 0x65, 0xE5, 0x15, 0x95, 0x55, 0xD5, 0x35, 0xB5, 0x75, 0xF5,
+	0x0D, 0x8D, 0x4D, 0xCD, 0x2D, 0xAD, 0x6D, 0xED, 0x1D, 0x9D, 0x5D, 0xDD, 0x3D, 0xBD, 0x7D, 0xFD,
+	0x03, 0x83, 0x43, 0xC3, 0x23, 0xA3, 0x63, 0xE3, 0x13, 0x93, 0x53, 0xD3, 0x33, 0xB3, 0x73, 0xF3,
+	0x0B, 0x8B, 0x4B, 0xCB, 0x2B, 0xAB, 0x6B, 0xEB, 0x1B, 0x9B, 0x5B, 0xDB, 0x3B, 0xBB, 0x7B, 0xFB,
+	0x07, 0x87, 0x47, 0xC7, 0x27, 0xA7, 0x67, 0xE7, 0x17, 0x97, 0x57, 0xD7, 0x37, 0xB7, 0x77, 0xF7,
+	0x0F, 0x8F, 0x4F, 0xCF, 0x2F, 0xAF, 0x6F, 0xEF, 0x1F, 0x9F, 0x5F, 0xDF, 0x3F, 0xBF, 0x7F, 0xFF,
+};
+
 void* NES_DMC_np_Create(UINT32 clock, UINT32 rate)
 {
 	NES_DMC* dmc;
@@ -149,8 +170,10 @@ void* NES_DMC_np_Create(UINT32 clock, UINT32 rate)
 	dmc->option[OPT_DPCM_ANTI_CLICK] = 0;
 	dmc->option[OPT_NONLINEAR_MIXER] = 1;
 	dmc->option[OPT_RANDOMIZE_NOISE] = 1;
+	dmc->option[OPT_RANDOMIZE_TRI] = 1;
 	dmc->option[OPT_TRI_MUTE] = 1;
 	dmc->option[OPT_TRI_NULL] = 0;
+	dmc->option[OPT_DPCM_REVERSE] = 0;
 	dmc->tnd_table[0][0][0][0] = 0;
 	dmc->tnd_table[1][0][0][0] = 0;
 
@@ -211,12 +234,13 @@ static void FrameSequence(NES_DMC* dmc, int s)
 
 	if (s == 0 && (dmc->frame_sequence_steps == 4))
 	{
-		dmc->frame_irq = true;
+		if (dmc->frame_irq_enable) dmc->frame_irq = true;
+		//cpu->UpdateIRQ(NES_CPU::IRQD_FRAME, dmc->frame_irq & dmc->frame_irq_enable);
 	}
 
 	// 240hz clock
 	{
-		bool divider = false;
+		bool divider;
 		
 		// triangle linear counter
 		if (dmc->linear_counter_halt)
@@ -233,7 +257,7 @@ static void FrameSequence(NES_DMC* dmc, int s)
 		}
 
 		// noise envelope
-		//bool divider = false;
+		divider = false;
 		if (dmc->envelope_write)
 		{
 			dmc->envelope_write = false;
@@ -254,7 +278,7 @@ static void FrameSequence(NES_DMC* dmc, int s)
 			if (dmc->envelope_loop && dmc->envelope_counter == 0)
 				dmc->envelope_counter = 15;
 			else if (dmc->envelope_counter > 0)
-				--dmc->envelope_counter;	// TODO: Make this work.
+				--dmc->envelope_counter;
 		}
 	}
 
@@ -286,11 +310,11 @@ UINT32 calc_tri(NES_DMC* dmc, UINT32 clocks)
 	if (dmc->linear_counter > 0 && dmc->length_counter[0] > 0
 		&& (!dmc->option[OPT_TRI_MUTE] || dmc->tri_freq > 0))
 	{
-		dmc->counter[0] += clocks;
-		while (dmc->counter[0] > dmc->tri_freq)
+		dmc->counter[0] -= clocks;
+		while (dmc->counter[0] < 0)
 		{
 			dmc->tphase = (dmc->tphase + 1) & 31;
-			dmc->counter[0] -= (dmc->tri_freq + 1);
+			dmc->counter[0] += (dmc->tri_freq + 1);
 		}
 	}
 	// additional option to prevent even more clicks -Valley Bell
@@ -299,16 +323,16 @@ UINT32 calc_tri(NES_DMC* dmc, UINT32 clocks)
 		if (dmc->tphase && dmc->tphase < 31)
 		{
 			// Finish the Triangle wave to prevent clicks.
-			dmc->counter[0] += clocks;
-			while(dmc->counter[0] > dmc->tri_freq && dmc->tphase)
+			dmc->counter[0] -= clocks;
+			while(dmc->counter[0] < 0 && dmc->tphase < 31)
 			{
 				dmc->tphase = (dmc->tphase + 1) & 31;
-				dmc->counter[0] -= (dmc->tri_freq + 1);
+				dmc->counter[0] += (dmc->tri_freq + 1);
 			}
 		}
 	}
 
-	//UINT32 ret = tritbl[tphase];
+	//UINT32 ret = tritbl[dmc->tphase];
 	//return ret;
 	return tritbl[dmc->tphase];
 }
@@ -319,33 +343,41 @@ UINT32 calc_tri(NES_DMC* dmc, UINT32 clocks)
 // 変換を行っている。
 UINT32 calc_noise(NES_DMC* dmc, UINT32 clocks)
 {
-	UINT32 env, last, count, accum, clocks_accum;
-	
-	env = dmc->envelope_disable ? dmc->noise_volume : dmc->envelope_counter;
+	UINT32 last, count, accum, accum_clocks, average;
+	UINT32 env = dmc->envelope_disable ? dmc->noise_volume : dmc->envelope_counter;
 	if (dmc->length_counter[1] < 1) env = 0;
 
-	last = (dmc->noise & 0x4000) ? env : 0;
+	last = (dmc->noise & 0x4000) ? 0 : env;
 	if (clocks < 1) return last;
 
 	// simple anti-aliasing (noise requires it, even when oversampling is off)
 	count = 0;
-	accum = 0;
+	accum = dmc->counter[1] * last; // samples pending from previous calc
+	accum_clocks = dmc->counter[1];
+	#ifdef _DEBUG_NSFP
+		INT32 start_clocks = dmc->counter[1];
+	#endif
+	if (dmc->counter[1] < 0) // only happens on startup when using the randomize noise option
+	{
+		accum = 0;
+		accum_clocks = 0;
+	}
 
-	dmc->counter[1] += clocks;
+	dmc->counter[1] -= clocks;
 //	assert(dmc->nfreq > 0);	// prevent infinite loop
 	if (dmc->nfreq <= 0)	// prevent infinite loop -VB
 		return last;
-	while (dmc->counter[1] >= dmc->nfreq)
+	while (dmc->counter[1] < 0)
 	{
 		// tick the noise generator
 		UINT32 feedback = (dmc->noise&1) ^ ((dmc->noise&dmc->noise_tap)?1:0);
 		dmc->noise = (dmc->noise>>1) | (feedback<<14);
 
+		last = (dmc->noise & 0x4000) ? 0 : env;
+		accum += (last * dmc->nfreq);
+		dmc->counter[1] += dmc->nfreq;
 		++count;
-		accum += last;
-		last = (dmc->noise & 0x4000) ? env : 0;
-
-		dmc->counter[1] -= dmc->nfreq;
+		accum_clocks += dmc->nfreq;
 	}
 
 	if (count < 1) // no change over interval, don't anti-alias
@@ -353,14 +385,15 @@ UINT32 calc_noise(NES_DMC* dmc, UINT32 clocks)
 		return last;
 	}
 
-	clocks_accum = clocks - dmc->counter[1];
-	// count = number of samples in accum
-	// counter[1] = number of clocks since last sample
+	accum -= (last * dmc->counter[1]); // remove these samples which belong in the next calc
+	accum_clocks -= dmc->counter[1];
+	#ifdef _DEBUG_NSFP
+		if (start_clocks >= 0) assert(accum_clocks == clocks); // these should be equal
+	#endif
 
-	accum = (accum * clocks_accum) + (last * dmc->counter[1] * count);
-	// note accum as an average is already premultiplied by count
-
-	return accum / (clocks * count);
+	average = accum / accum_clocks;
+//	assert(average <= 15); // above this would indicate overflow
+	return average;
 }
 
 // DMCチャンネルの計算 戻り値は0-127
@@ -370,44 +403,56 @@ UINT32 calc_dmc(NES_DMC* dmc, UINT32 clocks)
 //	assert(dmc->dfreq > 0);	// prevent infinite loop
 	if (dmc->dfreq <= 0)	// prevent infinite loop -VB
 		return (dmc->damp<<1) + dmc->dac_lsb;
-	while (dmc->counter[2] >= dmc->dfreq)
+	while (dmc->counter[2] < 0)
 	{
-		if ( dmc->data != 0x100 )	// data = 0x100 は EMPTY を意味する。
+		dmc->counter[2] += dmc->dfreq;
+
+		if ( dmc->data > 0x100 ) // data = 0x100 when shift register is empty
 		{
-			if ((dmc->data & 1) && (dmc->damp < 63))
-				dmc->damp++;
-			else if (!(dmc->data & 1) && (0 < dmc->damp))
-				dmc->damp--;
+			if (!dmc->empty)
+			{
+				if ((dmc->data & 1) && (dmc->damp < 63))
+					dmc->damp++;
+				else if (!(dmc->data & 1) && (0 < dmc->damp))
+					dmc->damp--;
+			}
 			dmc->data >>=1;
 		}
 
-		if ( dmc->data == 0x100 && dmc->active )
+		if ( dmc->data <= 0x100 ) // shift register is empty
 		{
-			//dmc->memory->Read(dmc->daddress, dmc->data);
-			dmc->data = dmc->memory[dmc->daddress];
-			dmc->data |= (dmc->data&0xFF)|0x10000;	// 8bitシフトで 0x100 になる
-			if ( dmc->length > 0 )
+			if (dmc->dlength > 0)
 			{
+				//dmc->memory->Read (dmc->daddress, dmc->data);
+				dmc->data = dmc->memory[dmc->daddress];
+				//cpu->StealCycles(4); // DMC read takes 3 or 4 CPU cycles, usually 4
+				// (checking for the 3-cycle case would require sub-instruction emulation)
+				dmc->data &= 0xFF; // read 8 bits
+				if (dmc->option[OPT_DPCM_REVERSE]) dmc->data = BITREVERSE[dmc->data];
+				dmc->data |= 0x10000; // use an extra bit to signal end of data
+				dmc->empty = false;
 				dmc->daddress = ((dmc->daddress+1)&0xFFFF)|0x8000 ;
-				dmc->length --;
-			}
-		}
-		
-		if ( dmc->length == 0 )	// 最後のフェッチが終了したら(再生完了より前に)即座に終端処理
-		{
-			if (dmc->mode & 1)
-			{
-				dmc->daddress = ((dmc->adr_reg<<6)|0xC000);
-				dmc->length = (dmc->len_reg<<4)+1;
+				--dmc->dlength;
+				if (dmc->dlength == 0)
+				{
+					if (dmc->mode & 1) // looped DPCM = auto-reload
+					{
+						dmc->daddress = ((dmc->adr_reg<<6)|0xC000);
+						dmc->dlength = (dmc->len_reg<<4)+1;
+					}
+					else if (dmc->mode & 2) // IRQ and not looped
+					{
+						dmc->irq = true;
+						//cpu->UpdateIRQ(NES_CPU::IRQD_DMC, true);
+					}
+				}
 			}
 			else
 			{
-				dmc->irq = (dmc->mode==2&&dmc->active)?1:0;	// 直前がactiveだったときはIRQ発行
-				dmc->active = false;
+				dmc->data = 0x10000; // DMC will do nothing
+				dmc->empty = true;
 			}
 		}
-
-		dmc->counter[2] -= dmc->dfreq;
 	}
 
 	return (dmc->damp<<1) + dmc->dac_lsb;
@@ -480,7 +525,6 @@ UINT32 NES_DMC_np_Render(void* chip, INT32 b[2])
 			dmc->dmc_pop = false;
 
 			// prevent overflow, keep headspace at edges
-			//const INT32 OFFSET_MAX = (1 << 30) - (4 << 16);
 #define OFFSET_MAX	((1 << 30) - (4 << 16))
 			if (dmc->dmc_pop_offset >  OFFSET_MAX) dmc->dmc_pop_offset =  OFFSET_MAX;
 			if (dmc->dmc_pop_offset < -OFFSET_MAX) dmc->dmc_pop_offset = -OFFSET_MAX;
@@ -549,11 +593,13 @@ void NES_DMC_np_SetAPU(void* chip, void* apu_)
 // Initializing TRI, NOISE, DPCM mixing table
 static void InitializeTNDTable(NES_DMC* dmc, double wt, double wn, double wd)
 {
-	// volume adjusted by 0.75 based on empirical measurements
-	const double MASTER = 8192.0 * 0.75;
+	// volume adjusted by 0.95 based on empirical measurements
+	static const double MASTER = 8192.0 * 0.95;
 	// truthfully, the nonlinear curve does not appear to match well
-	// with my tests, triangle in particular seems too quiet relatively.
-	// do more testing of the APU/DMC DAC later
+	// with my tests. Do more testing of the APU/DMC DAC later.
+	// this value keeps the triangle consistent with measured levels,
+	// but not necessarily the rest of this APU channel,
+	// because of the lack of a good DAC model, currently.
 
 	int t, n, d;
 
@@ -594,45 +640,65 @@ void NES_DMC_np_Reset(void* chip)
 	dmc->tphase = 0;
 	dmc->nfreq = wavlen_table[0][0];
 	dmc->dfreq = freq_table[0][0];
-
+	dmc->tri_freq = 0;
+	dmc->linear_counter = 0;
+	dmc->linear_counter_reload = 0;
+	dmc->linear_counter_halt = 0;
+	dmc->linear_counter_control = 0;
+	dmc->noise_volume = 0;
+	dmc->noise = 0;
+	dmc->noise_tap = 0;
+	dmc->envelope_loop = 0;
+	dmc->envelope_disable = 0;
+	dmc->envelope_write = 0;
+	dmc->envelope_div_period = 0;
 	dmc->envelope_div = 0;
+	dmc->envelope_counter = 0;
+	dmc->enable[0] = 0;
+	dmc->enable[1] = 0;
 	dmc->length_counter[0] = 0;
 	dmc->length_counter[1] = 0;
-	dmc->linear_counter = 0;
-	dmc->envelope_counter = 0;
-
 	dmc->frame_irq = false;
 	dmc->frame_irq_enable = false;
 	dmc->frame_sequence_count = 0;
 	dmc->frame_sequence_steps = 4;
 	dmc->frame_sequence_step = 0;
+	//cpu->UpdateIRQ(NES_CPU::IRQD_FRAME, false);
 
-	for (i = 0; i < 0x10; i++)
-		NES_DMC_np_Write(dmc, 0x4008 + i, 0);
+	for (i = 0; i < 0x0F; i++)
+		NES_DMC_np_Write (dmc, 0x4008 + i, 0);
+	NES_DMC_np_Write (dmc, 0x4017, 0x40);
 
 	dmc->irq = false;
-	NES_DMC_np_Write(dmc, 0x4015, 0x00);
+	NES_DMC_np_Write (dmc, 0x4015, 0x00);
 	if (dmc->option[OPT_UNMUTE_ON_RESET])
-		NES_DMC_np_Write(dmc, 0x4015, 0x0f);
+		NES_DMC_np_Write (dmc, 0x4015, 0x0f);
+	//cpu->UpdateIRQ(NES_CPU::IRQD_DMC, false);
 
 	dmc->out[0] = dmc->out[1] = dmc->out[2] = 0;
-	dmc->tri_freq = 0;
 	dmc->damp = 0;
 	dmc->dmc_pop = false;
 	dmc->dmc_pop_offset = 0;
 	dmc->dmc_pop_follow = 0;
 	dmc->dac_lsb = 0;
 	dmc->data = 0x100;
+	dmc->empty = true;
 	dmc->adr_reg = 0;
-	dmc->active = false;
-	dmc->length = 0;
+	dmc->dlength = 0;
 	dmc->len_reg = 0;
 	dmc->daddress = 0;
 	dmc->noise = 1;
 	dmc->noise_tap = (1<<1);
+
 	if (dmc->option[OPT_RANDOMIZE_NOISE])
 	{
 		dmc->noise |= rand();
+		dmc->counter[1] = -(rand() & 511);
+	}
+	if (dmc->option[OPT_RANDOMIZE_TRI])
+	{
+		dmc->tphase = rand() & 31;
+		dmc->counter[0] = -(rand() & 2047);
 	}
 
 	NES_DMC_np_SetRate(dmc, dmc->rate);
@@ -694,17 +760,18 @@ bool NES_DMC_np_Write(void* chip, UINT16 adr, UINT8 val)
 			dmc->length_counter[1] = 0;
 		}
 
-		if ((val & 16)&&!dmc->active)
+		if ((val & 16) && dmc->dlength == 0)
 		{
-			dmc->enable[2] = dmc->active = true;
 			dmc->daddress = (0xC000 | (dmc->adr_reg << 6));
-			dmc->length = (dmc->len_reg << 4) + 1;
-			dmc->irq = 0;
+			dmc->dlength = (dmc->len_reg << 4) + 1;
 		}
 		else if (!(val & 16))
 		{
-			dmc->enable[2] = dmc->active = false;
+			dmc->dlength = 0;
 		}
+
+		dmc->irq = false;
+		//cpu->UpdateIRQ(NES_CPU::IRQD_DMC, false);
 
 		dmc->reg[adr-0x4008] = val;
 		return true;
@@ -714,7 +781,9 @@ bool NES_DMC_np_Write(void* chip, UINT16 adr, UINT8 val)
 	{
 		//DEBUG_OUT("4017 = %02X\n", val);
 		dmc->frame_irq_enable = ((val & 0x40) == 0x40);
-		dmc->frame_irq = (dmc->frame_irq_enable ? dmc->frame_irq : 0);
+		if (dmc->frame_irq_enable) dmc->frame_irq = false;
+		//cpu->UpdateIRQ(NES_CPU::IRQD_FRAME, false);
+
 		dmc->frame_sequence_count = 0;
 		if (val & 0x80)
 		{
@@ -752,12 +821,10 @@ bool NES_DMC_np_Write(void* chip, UINT16 adr, UINT8 val)
 
 	case 0x400a:
 		dmc->tri_freq = val | (dmc->tri_freq & 0x700) ;
-		if (dmc->counter[0] > dmc->tri_freq) dmc->counter[0] = dmc->tri_freq;
 		break;
 
 	case 0x400b:
 		dmc->tri_freq = (dmc->tri_freq & 0xff) | ((val & 0x7) << 8) ;
-		if (dmc->counter[0] > dmc->tri_freq) dmc->counter[0] = dmc->tri_freq;
 		dmc->linear_counter_halt = true;
 		if (dmc->enable[0])
 		{
@@ -783,7 +850,6 @@ bool NES_DMC_np_Write(void* chip, UINT16 adr, UINT8 val)
 		else
 			dmc->noise_tap = (1<<1);
 		dmc->nfreq = wavlen_table[dmc->pal][val&15];
-		if (dmc->counter[1] > dmc->nfreq) dmc->counter[1] = dmc->nfreq;
 		break;
 
 	case 0x400f:
@@ -798,8 +864,12 @@ bool NES_DMC_np_Write(void* chip, UINT16 adr, UINT8 val)
 
 	case 0x4010:
 		dmc->mode = (val >> 6) & 3;
+		if (!(dmc->mode & 2))
+		{
+			dmc->irq = false;
+			//cpu->UpdateIRQ(NES_CPU::IRQD_DMC, false);
+		}
 		dmc->dfreq = freq_table[dmc->pal][val&15];
-		if (dmc->counter[2] > dmc->dfreq) dmc->counter[2] = dmc->dfreq;
 		break;
 
 	case 0x4011:
@@ -834,14 +904,15 @@ bool NES_DMC_np_Read(void* chip, UINT16 adr, UINT8* val)
 
 	if (adr == 0x4015)
 	{
-		*val |= (dmc->irq?128:0)
-			 | (dmc->frame_irq ? 0x40 : 0)
-			 | (dmc->active?16:0)
-			 | (dmc->length_counter[1]?8:0)
-			 | (dmc->length_counter[0]?4:0)
+		*val |=(dmc->irq               ? 0x80 : 0)
+			 | (dmc->frame_irq         ? 0x40 : 0)
+			 | ((dmc->dlength>0)       ? 0x10 : 0)
+			 | (dmc->length_counter[1] ? 0x08 : 0)
+			 | (dmc->length_counter[0] ? 0x04 : 0)
 			 ;
 
 		dmc->frame_irq = false;
+		//cpu->UpdateIRQ(NES_CPU::IRQD_FRAME, false);
 		return true;
 	}
 	else if (0x4008<=adr&&adr<=0x4014)

emu/cores/c140.c
@@ -177,10 +177,25 @@ static UINT32 find_sample(c140_state *info, UINT32 adrs, UINT8 bank, int voice)
 	return 0;
 }
 
+INLINE UINT8 c140_keyon_status_read(c140_state *info, UINT16 offset)
+{
+	//m_stream->update();
+	C140_VOICE const *v = &info->voi[offset >> 4];
+
+	// suzuka 8 hours and final lap games read from here, expecting bit 6 to be an in-progress sample flag.
+	// four trax also expects bit 4 high for some specific channels to make engine noises to work properly
+	// (sounds kinda bogus when player crashes in an object and jump spin, needs real HW verification)
+	return (v->key ? 0x40 : 0x00) | (info->REG[offset] & 0x3f);
+}
+
 static UINT8 c140_r(void *chip, UINT16 offset)
 {
 	c140_state *info = (c140_state *)chip;
 	offset&=0x1ff;
+
+	if ((offset & 0xf) == 0x5 && offset < 0x180)
+		return c140_keyon_status_read(info, offset);
+
 	return info->REG[offset];
 }
 

emu/cores/c219.c
@@ -144,6 +144,7 @@ typedef struct
 	UINT32  sample_start;
 	UINT32  sample_end;
 	UINT32  sample_loop;
+	UINT8   key;
 	UINT8   Muted;
 } C219_VOICE;
 
@@ -183,12 +184,29 @@ static UINT32 find_sample(c219_state *info, UINT32 adrs, int voice)
 	return ((bank << 17) | adrs) & info->pRomMask;
 }
 
+INLINE UINT8 c219_keyon_status_read(c219_state *info, UINT16 offset)
+{
+	//m_stream->update();
+	C219_VOICE const *v = &info->voi[offset >> 4];
+
+	// suzuka 8 hours and final lap games read from here, expecting bit 6 to be an in-progress sample flag.
+	// four trax also expects bit 4 high for some specific channels to make engine noises to work properly
+	// (sounds kinda bogus when player crashes in an object and jump spin, needs real HW verification)
+	return (v->key ? 0x40 : 0x00) | (info->REG[offset] & 0x3f);
+}
+
 static UINT8 c219_r(void *chip, UINT16 offset)
 {
 	c219_state *info = (c219_state *)chip;
 	offset &= 0x1ff;
 	if (offset >= 0x1f8 && (offset & 0x001))
 		offset &= ~0x008;
+
+	// assume same as c140
+	// TODO: what happens here on reading unmapped voice regs?
+	if ((offset & 0xf) == 0x5 && offset < 0x100)
+		return c219_keyon_status_read(info, offset);
+
 	return info->REG[offset];
 }
 
@@ -216,6 +234,7 @@ static void c219_w(void *chip, UINT16 offset, UINT8 data)
 				v->sample_loop = ((vreg->loop_msb<<8) | vreg->loop_lsb)*2;
 				v->sample_start = ((vreg->start_msb<<8) | vreg->start_lsb)*2;
 				v->sample_end = ((vreg->end_msb<<8) | vreg->end_lsb)*2;
+				v->key=1;
 				v->pos = v->sample_start;
 				v->pofs = 0xFFFF;
 				v->sample = 0;
@@ -228,6 +247,10 @@ static void c219_w(void *chip, UINT16 offset, UINT8 data)
 					find_sample(info, v->sample_loop, v->bank, offset>>4),
 					find_sample(info, v->sample_end, v->bank, offset>>4));
 				#endif
+			}
+			else
+			{
+				v->key=0;
 			}
 		}
 	}
@@ -263,7 +286,7 @@ static void c219_fetch_sample(c219_state *chip, UINT32 vid)
 			}
 			else
 			{
-				vreg->mode &= ~C219_MODE_KEYON;
+				v->key=0;
 				v->sample = 0;
 			}
 		}
@@ -291,7 +314,7 @@ static void c219_update(void *param, UINT32 samples, DEV_SMPL **outputs)
 			C219_VOICE* v = &chip->voi[j];
 			const C219_VREGS* vreg = (C219_VREGS*)&chip->REG[j*16];
 
-			if ((vreg->mode & C219_MODE_KEYON) && ! v->Muted)
+			if (v->key && ! v->Muted)
 			{
 				UINT32 frequency = (vreg->frequency_msb<<8) | vreg->frequency_lsb;
 				INT32 newofs;

emu/cores/nesintf.h
@@ -16,25 +16,28 @@
 
 // [NSFPlay core] APU/DMC options
 #define OPT_NES_UNMUTE_ON_RESET		0x0001	// enable all channels by default after reset (default: enabled)
-#define OPT_NES_NONLINEAR_MIXER		0x0002	// (default: enabled)
+#define OPT_NES_NONLINEAR_MIXER		0x0002	// nonlinear mixing (default: enabled)
 // [NSFPlay core] APU options
 #define OPT_NES_PHASE_REFRESH		0x0004	// writing 4003/4007 resets the square's waveform (default: enabled)
-#define OPT_NES_DUTY_SWAP		0x0008	// swap 25:75 and 50:50 duty cycles (behaviour of some Famiclones)
-						// (default: disabled)
+#define OPT_NES_DUTY_SWAP			0x0008	// swap 25:75 and 50:50 duty cycles (behaviour of some Famiclones)
+											// (default: disabled)
+//#define OPT_NES_NEGATE_SWEEP_INIT	0x00##	// initialize sweep unit unmuted (default: disabled)
 // [NSFPlay core] DMC options
-#define OPT_NES_ENABLE_4011		0x0010	// enable register 4011 writes (default: enabled)
+#define OPT_NES_ENABLE_4011			0x0010	// enable register 4011 writes (default: enabled)
 #define OPT_NES_ENABLE_PNOISE		0x0020	// enable "loop noise" (disable for early Famicom consoles + Arcade boards)
-						// (default: enabled)
+											// (default: enabled)
 #define OPT_NES_DPCM_ANTI_CLICK		0x0040	// nullify register 4011 writes, but preserve nonlinearity
-						// (default: disabled)
+											// (default: disabled)
 #define OPT_NES_RANDOMIZE_NOISE		0x0080	// randomize noise at device reset (default: enabled)
-#define OPT_NES_TRI_MUTE		0x0100	// stops Triangle wave if set to freq = 0,
-						// processes it at a very high rate else (default: enabled)
-#define OPT_NES_TRI_NULL		0x0200	// always make Triangle return to null-level when stopping,
-						// prevents clicking (Valley Bell mod) (default: disabled)
+#define OPT_NES_TRI_MUTE			0x0100	// stops Triangle wave if set to freq = 0,
+											// processes it at a very high rate else (default: enabled)
+#define OPT_NES_TRI_NULL			0x0200	// always make Triangle return to null-level when stopping,
+											// prevents clicking (Valley Bell mod) (default: disabled)
+//#define OPT_NES_RANDOMIZE_TRI		0x0###	// randomize triangle on reset (default: enabled)
+//#define OPT_NES_DPCM_REVERSE		0x0###	// reverse bits of DPCM sample bytes (default: disabled)
 // [NSFPlay FDS core] options
-#define OPT_NES_4085_RESET		0x0400	// OPT_4085_RESET (default: disabled)
-#define OPT_NES_FDS_DISABLE		0x0800	// OPT_WRITE_PROTECT (default: disabled)
+#define OPT_NES_4085_RESET			0x0400	// OPT_4085_RESET (default: disabled)
+#define OPT_NES_FDS_DISABLE			0x0800	// OPT_WRITE_PROTECT (default: disabled)
 
 // default option bitmask: 0x01B7
 

emu/cores/np_nes_fds.c
@@ -226,14 +226,14 @@ void NES_FDS_Reset(void* chip)
 	fds->master_env_speed = 0xFF;
 
 	// NOTE: the FDS BIOS reset only does the following related to audio:
-	//   $4023 = $00
-	//   $4023 = $83 enables master_io
-	//   $4080 = $80 output volume = 0, envelope disabled
-	//   $408A = $FF master envelope speed set to slowest
+	//	 $4023 = $00
+	//	 $4023 = $83 enables master_io
+	//	 $4080 = $80 output volume = 0, envelope disabled
+	//	 $408A = $E8 master envelope speed
 	NES_FDS_Write(fds, 0x4023, 0x00);
 	NES_FDS_Write(fds, 0x4023, 0x83);
 	NES_FDS_Write(fds, 0x4080, 0x80);
-	NES_FDS_Write(fds, 0x408A, 0xFF);
+	NES_FDS_Write(fds, 0x408A, 0xE8);
 
 	// reset other stuff
 	NES_FDS_Write(fds, 0x4082, 0x00);	// wav freq 0
@@ -486,9 +486,9 @@ bool NES_FDS_Write(void* chip, UINT16 adr, UINT8 val)
 		if (fds->mod_halt)
 		{
 			// writes to current playback position (there is no direct way to set phase)
-			fds->wave[TMOD][(fds->phase[TMOD] >> 16) & 0x3F] = val & 0x7F;
+			fds->wave[TMOD][(fds->phase[TMOD] >> 16) & 0x3F] = val & 0x07;
 			fds->phase[TMOD] = (fds->phase[TMOD] + 0x010000) & 0x3FFFFF;
-			fds->wave[TMOD][(fds->phase[TMOD] >> 16) & 0x3F] = val & 0x7F;
+			fds->wave[TMOD][(fds->phase[TMOD] >> 16) & 0x3F] = val & 0x07;
 			fds->phase[TMOD] = (fds->phase[TMOD] + 0x010000) & 0x3FFFFF;
 			fds->mod_write_pos = fds->phase[TMOD] >> 16;	// used by OPT_4085_RESET
 		}
@@ -515,7 +515,7 @@ bool NES_FDS_Read(void* chip, UINT16 adr, UINT8* val)
 {
 	NES_FDS* fds = (NES_FDS*)chip;
 
-	if (adr >= 0x4040 && adr < 0x407F)
+	if (adr >= 0x4040 && adr <= 0x407F)
 	{
 		// TODO: if wav_write is not enabled, the
 		// read address may not be reliable? need

commit 8fa374ee7593d36a2b5dd33376e6d4668a78933e

fix NSFPlay NES DPCM not playing
The bug was introduced with 41320b2.

emu/cores/np_nes_apu.c
@@ -323,19 +323,29 @@ void NES_APU_np_Reset(void* chip)
 	apu->gclock = 0;
 	//apu->mask = 0;
 
-	apu->scounter[0] = 0;
-	apu->scounter[1] = 0;
-	apu->sphase[0] = 0;
-	apu->sphase[0] = 0;
-
-	apu->sweep_div[0] = 1;
-	apu->sweep_div[1] = 1;
-	apu->envelope_div[0] = 0;
-	apu->envelope_div[1] = 0;
-	apu->length_counter[0] = 0;
-	apu->length_counter[1] = 0;
-	apu->envelope_counter[0] = 0;
-	apu->envelope_counter[1] = 0;
+	for (i=0; i<2; ++i)
+	{
+		apu->scounter[i] = 0;
+		apu->sphase[i] = 0;
+		apu->duty[i] = 0;
+		apu->volume[i] = 0;
+		apu->freq[i] = 0;
+		apu->sfreq[i] = 0;
+		apu->sweep_enable[i] = 0;
+		apu->sweep_mode[i] = 0;
+		apu->sweep_write[i] = 0;
+		apu->sweep_div_period[i] = 0;
+		apu->sweep_div[i] = 1;
+		apu->sweep_amount[i] = 0;
+		apu->envelope_disable[i] = 0;
+		apu->envelope_loop[i] = 0;
+		apu->envelope_write[i] = 0;
+		apu->envelope_div_period[i] = 0;
+		apu->envelope_div[i] = 0;
+		apu->envelope_counter[i] = 0;
+		apu->length_counter[i] = 0;
+		apu->enable[i] = 0;
+	}
 
 	for (i = 0x4000; i < 0x4008; i++)
 		NES_APU_np_Write(apu, i, 0);

emu/cores/np_nes_dmc.c
@@ -399,7 +399,7 @@ UINT32 calc_noise(NES_DMC* dmc, UINT32 clocks)
 // DMCチャンネルの計算 戻り値は0-127
 UINT32 calc_dmc(NES_DMC* dmc, UINT32 clocks)
 {
-	dmc->counter[2] += clocks;
+	dmc->counter[2] -= clocks;
 //	assert(dmc->dfreq > 0);	// prevent infinite loop
 	if (dmc->dfreq <= 0)	// prevent infinite loop -VB
 		return (dmc->damp<<1) + dmc->dac_lsb;