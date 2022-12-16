2a98834ae7069dcdf6f3a623939d62c8d5f5025c
Matt Montag <matt.montag@gmail.com>
2022-12-16 12:00:00 -0700

    Add list of N64 games that don't use drums on channel 10

--- src/players/MIDIPlayer.js
+++ src/players/MIDIPlayer.js
@@ -321,22 +321,32 @@ export default class MIDIPlayer extends Player {
     // Load custom Soundfont if present in the metadata response.
     if (this.getParameter('synthengine') === MIDI_ENGINE_LIBFLUIDLITE) {
       const metadataUrl = getMetadataUrlForFilepath(filepath);
+      let useMelodicChannel10 = false;
       // This will most certainly be cached by a preceding fetch in App.js.
       const { soundfont: soundfontUrl } = await requestCache.fetchCached(metadataUrl);
       if (soundfontUrl) {
-        const sf2Path = `user/${path.basename(getFilepathFromUrl(soundfontUrl))}`;
+        const soundfontBasename = path.basename(getFilepathFromUrl(soundfontUrl));
+        const sf2Path = `user/${soundfontBasename}`;
         newTransientParams['soundfont'] = sf2Path;
         if (this.getParameter('soundfont') !== sf2Path) {
           await ensureEmscFileWithUrl(lib, `${SOUNDFONT_MOUNTPOINT}/${sf2Path}`, soundfontUrl);
           this.updateSoundfontParamDefs();
         }
-        // Disabled due to Kirby 64 tracks. CH 10 needs to be a drum channel.
-        // TODO: Maybe this is no longer needed at all. Delete after 2022-12-25.
-        //   lib._tp_set_ch10_melodic(1);
-        // } else {
-        //   lib._tp_set_ch10_melodic(0);
+        // Melodic mode mean CH 10 becomes like any other channel, using SoundFont bank 0 by default.
+        // The MIDI file *must* do an explicit bank select to get bank 128 on CH 10 for drums.
+        // This is list is a quick hack for N64 games that don't treat channel 10 like drums.
+        // The more correct alternative would be to make sure the MIDI files contain a reliable signal
+        // for melodic CH 10; perhaps borrowed from GS or XG standard.
+        const melodicDrumSoundfonts = [
+          'Centre Court Tennis', 'Goemon', 'Bomberman', 'GoldenEye',
+          'Perfect Dark', 'Banjo Kazooie', 'Diddy Kong', 'Zelda'
+        ];
+        if (melodicDrumSoundfonts.some(sf => soundfontUrl.includes(sf))) {
+          console.debug('MIDI channel 10 melodic mode enabled for %s.', soundfontBasename);
+          useMelodicChannel10 = true;
+        }
       }
-      lib._tp_set_ch10_melodic(0);
+      lib._tp_set_ch10_melodic(useMelodicChannel10);
     }
 
     // Apply transient params. Avoid thrashing of params that haven't changed.
