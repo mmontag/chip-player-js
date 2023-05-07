1f85f562248f4100e1b60eb656c4aef2413ec989
Matt Montag <matt.montag@gmail.com>
2023-05-07 02:02:05 -0700

    Simplify AppHeader

--- src/components/AppHeader.js
+++ src/components/AppHeader.js
@@ -1,33 +1,28 @@
 import React from "react";
-import { NavLink } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 export default class AppHeader extends React.PureComponent {
   render() {
     return (
       <header className="AppHeader">
-        <NavLink className="AppHeader-title" to={{ pathname: "/" }}>Chip Player JS</NavLink>{' '}
+        <Link className="AppHeader-title" to={{ pathname: "/" }}>Chip Player JS</Link>
         {this.props.user ?
-          <span>
-              • Logged in as {this.props.user.displayName}.{' '}
+          <>
+            {' • '}
+            Logged in as {this.props.user.displayName}.
+            {' '}
             <a href="#" onClick={this.props.handleLogout}>Logout</a>
-            </span>
+          </>
           :
-          <span>
-              • <a href="#" onClick={this.props.handleLogin}>Login/Sign Up</a> to Save Favorites
-            </span>
+          <>
+            {' • '}
+            <a href="#" onClick={this.props.handleLogin}>Login/Sign Up</a> to Save Favorites
+          </>
         }
-        {!this.props.isPhone &&
-        <p className="AppHeader-subtitle">
-            <span className="AppHeader-byline">Feedback:{' '}
-              <a href="https://twitter.com/matthewmontag" target="_blank" rel="noopener noreferrer">
-                @matthewmontag
-              </a>
-            </span>
-          Powered by{' '}
-          <a href="https://bitbucket.org/mpyne/game-music-emu/wiki/Home">Game Music Emu</a>,{' '}
-          <a href="https://github.com/cmatsuoka/libxmp">LibXMP</a>, and{' '}
-          <a href="https://github.com/divideconcept/FluidLite">FluidLite</a>.
-        </p>}
+        {' • '}
+        <a href="https://twitter.com/messages/compose?recipient_id=587634572" target="_blank" rel="noopener noreferrer">
+          Feedback
+        </a>
       </header>
     );
   }
