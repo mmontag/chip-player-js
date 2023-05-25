588b4312d05b6bf7822ba7aba1e7fb8fc46d1e25
Matt Montag <matt.montag@gmail.com>
2023-05-25 00:32:01 -0700

    Convert VolumeSlider.js to function component

--- src/components/VolumeSlider.js
+++ src/components/VolumeSlider.js
@@ -1,20 +1,18 @@
-import React, {Component} from "react";
+import React from "react";
 
-export class VolumeSlider extends Component {
-  render() {
-    return <div className="VolumeSlider">
-      <input type='range'
-             title={"Volume"}
-             min={0} max={150} step={1}
-             onChange={this.props.onChange}
-             onDoubleClick={this.props.handleReset}
-             onContextMenu={this.props.handleReset}
-             value={this.props.value}>
-      </input>
-      <div className="VolumeSlider-labels">
-        <div>Volume</div>
-        <div>{this.props.value}%</div>
-      </div>
-    </div>;
-  }
+export function VolumeSlider(props) {
+  return <div className="VolumeSlider">
+    <input type='range'
+           title={"Volume"}
+           min={0} max={150} step={1}
+           onChange={props.onChange}
+           onDoubleClick={props.handleReset}
+           onContextMenu={props.handleReset}
+           value={props.value}>
+    </input>
+    <div className="VolumeSlider-labels">
+      <div>Volume</div>
+      <div>{props.value}%</div>
+    </div>
+  </div>;
 }
