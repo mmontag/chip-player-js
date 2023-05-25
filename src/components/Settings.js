a43c9fae1da217a4d0b15260b0b3eb19e0a49ca5
Matt Montag <matt.montag@gmail.com>
2023-05-24 23:37:38 -0700

    Convert Settings.js to function component

--- src/components/Settings.js
+++ src/components/Settings.js
@@ -1,36 +1,34 @@
-import React, { PureComponent } from 'react';
+import React from 'react';
 import PlayerParams from './PlayerParams';
 
-export default class Settings extends PureComponent {
-  render() {
-    const {
-      ejected,
-      tempo,
-      currentSongNumVoices,
-      voiceMask,
-      voiceNames,
-      handleSetVoiceMask,
-      handleTempoChange,
-      sequencer,
-    } = this.props;
-    return (
-      <div className='Settings'>
-        <h3>{sequencer?.getPlayer()?.name || 'Player'} Settings</h3>
-        {sequencer?.getPlayer() ?
-          <PlayerParams
-            ejected={ejected}
-            tempo={tempo}
-            numVoices={currentSongNumVoices}
-            voiceMask={voiceMask}
-            voiceNames={voiceNames}
-            handleTempoChange={handleTempoChange}
-            handleSetVoiceMask={handleSetVoiceMask}
-            getParameter={sequencer.getPlayer().getParameter}
-            setParameter={sequencer.getPlayer().setParameter}
-            paramDefs={sequencer.getPlayer().getParamDefs()}/>
-          :
-          <div>(No active player)</div>}
-      </div>
-    );
-  }
+export default function Settings(props) {
+  const {
+    ejected,
+    tempo,
+    currentSongNumVoices,
+    voiceMask,
+    voiceNames,
+    handleSetVoiceMask,
+    handleTempoChange,
+    sequencer,
+  } = props;
+  return (
+    <div className='Settings'>
+      <h3>{sequencer?.getPlayer()?.name || 'Player'} Settings</h3>
+      {sequencer?.getPlayer() ?
+        <PlayerParams
+          ejected={ejected}
+          tempo={tempo}
+          numVoices={currentSongNumVoices}
+          voiceMask={voiceMask}
+          voiceNames={voiceNames}
+          handleTempoChange={handleTempoChange}
+          handleSetVoiceMask={handleSetVoiceMask}
+          getParameter={sequencer.getPlayer().getParameter}
+          setParameter={sequencer.getPlayer().setParameter}
+          paramDefs={sequencer.getPlayer().getParamDefs()}/>
+        :
+        <div>(No active player)</div>}
+    </div>
+  );
 }
