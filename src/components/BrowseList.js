f1e71a372d97ecc1bcfb4cb70258346077d1e0da
Matt Montag <matt.montag@gmail.com>
2023-05-27 18:35:53 -0700

    Parent directory link opportunistically navigates back (#135)
    So that scroll state can be restored.
    
    This also includes perf optimizations for DirectoryLink. memoization, avoid creating functions on every render, etc.

--- src/components/BrowseList.js
+++ src/components/BrowseList.js
@@ -1,27 +1,39 @@
 import React, { memo } from 'react';
 import bytes from 'bytes';
+import trimEnd from 'lodash/trimEnd';
 import { CATALOG_PREFIX } from '../config';
 import DirectoryLink from './DirectoryLink';
 import FavoriteButton from './FavoriteButton';
+import queryString from 'querystring';
+import { useHistory } from 'react-router-dom';
 
 export default memo(BrowseList);
 function BrowseList({ virtual, ...props }) {
-    const {
-      currContext,
-      currIdx,
-      favorites,
-      toggleFavorite,
-      handleSongClick,
-      browsePath,
-      playContext,
-    } = props;
+  const {
+    currContext,
+    currIdx,
+    favorites,
+    toggleFavorite,
+    handleSongClick,
+    browsePath,
+    playContext,
+  } = props;
 
-    // Scroll Into View
-    // ----------------
-    // Note this does not work for virtual list, since the playing item might not be in the DOM.
-    // See also https://medium.com/@teh_builder/ref-objects-inside-useeffect-hooks-eb7c15198780
+  // Scroll Into View
+  // ----------------
+  // Note this does not work for virtual list, since the playing item might not be in the DOM.
+  // See also https://medium.com/@teh_builder/ref-objects-inside-useeffect-hooks-eb7c15198780
+  const urlParams = queryString.parse(window.location.search.substring(1));
+  delete urlParams.q;
+  const search = queryString.stringify(urlParams);
 
-    return (
+  // Check if previous page url is the parent directory of current page url.
+  const history = useHistory();
+  const prevPath = trimEnd(history.location.state?.prevPathname, '/');
+  const currPath = trimEnd(window.location.pathname, '/');
+  const prevPageIsParentDir = prevPath === currPath.substring(0, currPath.lastIndexOf('/'));
+
+  return (
       <div style={{ position: 'relative' }}>
         <div style={virtual.style}>
           {virtual.items.map(item => {
@@ -32,11 +44,13 @@ function BrowseList({ virtual, ...props }) {
               item.path.replace('%', '%25').replace('#', '%23').replace(/^\//, '');
             const name = item.path.split('/').pop();
             const isPlaying = currContext === playContext && currIdx === item.idx;
+            const isBackLink = item.path === '..' && prevPageIsParentDir;
+
             if (item.type === 'directory') {
               return (
                 <div key={name} className="BrowseList-row">
                   <div className="BrowseList-colName">
-                    <DirectoryLink to={'/browse/' + path}>{name}</DirectoryLink>
+                    <DirectoryLink to={'/browse/' + path} search={search} isBackLink={isBackLink} history={history}>{name}</DirectoryLink>
                   </div>
                   <div className="BrowseList-colDir">
                     &lt;DIR&gt;
--- src/components/DirectoryLink.js
+++ src/components/DirectoryLink.js
@@ -1,26 +1,48 @@
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
 import folder from "../images/folder.png";
 import * as PropTypes from "prop-types";
-import React from "react";
+import React, { memo } from "react";
 import queryString from 'querystring';
 
-export default function DirectoryLink(props) {
+function getSearch() {
+  const urlParams = queryString.parse(window.location.search.substring(1));
+  delete urlParams.q;
+  return queryString.stringify(urlParams);
+}
+
+export default memo(DirectoryLink);
+
+function DirectoryLink(props) {
   const linkClassName = props.dim ? 'DirectoryLink-dim' : null;
   const folderClassName = props.dim ? 'DirectoryLink-folderIconDim' : 'DirectoryLink-folderIcon';
-  const urlParams = queryString.parse(window.location.search.substr(1));
-  delete urlParams.q;
   // Double encode % because react-router will decode this into history.
   // See https://github.com/ReactTraining/history/issues/505
   // The fix https://github.com/ReactTraining/history/pull/656
   // ...is not released in react-router-dom 5.2.0 which uses history 4.10
   const to = props.to.replace('%25', '%2525');
-  const search = queryString.stringify(urlParams);
-  return <Link to={{ pathname: to, search: search }} className={linkClassName}>
-    <img alt='folder' className={folderClassName} src={folder}/>{props.children}
-  </Link>;
+  const search = props.search || getSearch();
+
+  let toObj = { pathname: to, search: search, state: { prevPathname: window.location.pathname } };
+  let onClick = null;
+
+  if (props.isBackLink) {
+    onClick = function goBack(e) {
+      e.preventDefault();
+      // history.back();
+      props.history.goBack();
+    }
+  }
+
+  return (
+    <Link to={toObj} className={linkClassName} onClick={onClick}>
+      <img alt='folder' className={folderClassName} src={folder}/>{props.children}
+    </Link>
+  );
 }
 
 DirectoryLink.propTypes = {
   to: PropTypes.string.isRequired,
   dim: PropTypes.bool,
+  search: PropTypes.string,
+  isBackLink: PropTypes.bool,
 };
