00348f98c678cdf4e4a9567707f4522de2779269
Matt Montag <matt.montag@gmail.com>
2025-04-01 12:00:00 -0700

    Add "Global Settings" section to Settings panel
    This is a bit rough for now...

--- src/components/Settings.js
+++ src/components/Settings.js
@@ -1,7 +1,20 @@
-import React, { memo } from 'react';
+import React, { memo, useCallback, useContext } from 'react';
 import PlayerParams from './PlayerParams';
+import { UserContext } from "./UserProvider";
+
+const themes = [
+  {
+    value: 'msdos',
+    label: 'MS-DOS',
+  },
+  {
+    value: 'winamp',
+    label: 'Winamp',
+  }
+];
 
 export default memo(Settings);
+
 function Settings(props) {
   const {
     ejected,
@@ -14,6 +27,14 @@ function Settings(props) {
     handleTempoChange,
     sequencer,
   } = props;
+
+  const { settings, updateSettings } = useContext(UserContext);
+  const theme = settings?.theme; // Extract only what we need
+
+  const handleThemeChange = useCallback((e) => {
+    updateSettings({ theme: e.target.value });
+  }, [updateSettings]);
+
   return (
     <div className='Settings'>
       <h3>{sequencer?.getPlayer()?.name || 'Player'} Settings</h3>
@@ -32,6 +53,20 @@ function Settings(props) {
           paramDefs={sequencer.getPlayer().getParamDefs()}/>
         :
         <div>(No active player)</div>}
+      <h3>Global Settings</h3>
+      <span className='PlayerParams-param'>
+        <label htmlFor='theme' className="PlayerParams-label">
+          Theme:{' '}
+        </label>
+        <select
+          id='theme'
+          onChange={handleThemeChange}
+          value={theme}>
+          {themes.map(option =>
+            <option key={option.value} value={option.value}>{option.label}</option>
+          )}
+        </select>
+      </span>
     </div>
   );
 }
