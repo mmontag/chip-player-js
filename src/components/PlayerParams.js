mmontag/chip-player-js

commit 1c9311e337856f6e8ccf1cc308e5358b875902c1

Move param values into playerState and React state
This replaces the getParameter/setParameter function props passed down to PlayerParams.

src/components/PlayerParams.js
@@ -1,7 +1,7 @@
-import React, { PureComponent } from 'react';
+import React from 'react';
 import autoBindReact from 'auto-bind/react';
 
-export default class PlayerParams extends PureComponent {
+export default class PlayerParams extends React.PureComponent {
   constructor(props) {
     super(props);
     autoBindReact(this);
@@ -45,26 +45,38 @@ export default class PlayerParams extends PureComponent {
   // }
 
   handleVoiceToggle(e, index) {
-    const voiceMask = this.props.voiceMask;
+    const { onVoiceMaskChange } = this.props;
+    if (!onVoiceMaskChange) return;
+
+    const voiceMask = [...this.props.voiceMask];
     e = e.nativeEvent || {};
     if (e.altKey || e.shiftKey || e.metaKey) {
-      // Behaves like Photoshop (toggling layer visibility with alt+click)
       if (voiceMask.every((enabled, i) => (i === index) === enabled)) {
-        // Alt-click on a single enabled channel unmutes everything
         voiceMask.fill(true);
       } else {
-        // Solo the channel
         voiceMask.fill(false);
         voiceMask[index] = true;
       }
     } else {
-      // Regular toggle behavior
       voiceMask[index] = !voiceMask[index];
     }
-    this.props.handleSetVoiceMask(voiceMask);
+    onVoiceMaskChange(voiceMask);
   }
 
   render() {
+    const {
+      paramDefs,
+      paramValues,
+      onParamChange,
+      tempo,
+      onTempoChange,
+      ejected,
+      voiceGroups,
+      numVoices,
+      voiceMask,
+      voiceNames,
+    } = this.props;
+
     return (
       <div className='PlayerParams'>
         <span className='PlayerParams-param PlayerParams-group'>
@@ -73,15 +85,15 @@ export default class PlayerParams extends PureComponent {
           </label>
           <input
             id='tempo'
-            disabled={this.props.ejected}
-            type='range' value={this.props.tempo}
+            disabled={ejected}
+            type='range' value={tempo}
             min='0.3' max='2.0' step='0.05'
-            onInput={this.props.handleTempoChange}
-            onChange={this.props.handleTempoChange}/>{' '}
-          {this.props.tempo.toFixed(2)}
+            onInput={onTempoChange}
+            onChange={onTempoChange}/>{' '}
+          {tempo.toFixed(2)}
         </span>
-        {this.props.voiceGroups.length > 0 ?
-          this.props.voiceGroups.map((voiceGroup, i) => {
+        {voiceGroups.length > 0 ?
+          voiceGroups.map((voiceGroup, i) => {
             return (
               <span className='PlayerParams-param PlayerParams-group' key={voiceGroup.name}>
                   <label className="PlayerParams-group-title" title="Sound chip">
@@ -95,7 +107,7 @@ export default class PlayerParams extends PureComponent {
                         type='checkbox'
                         id={'v_'+i+j}
                         onChange={(e) => this.handleVoiceToggle(e, voice.idx)}
-                        checked={this.props.voiceMask[voice.idx]}/>
+                        checked={voiceMask[voice.idx]}/>
                       <label htmlFor={'v_'+i+j}>
                         {voice.name}
                       </label></div>
@@ -105,22 +117,22 @@ export default class PlayerParams extends PureComponent {
             )
           })
           :
-          this.props.numVoices > 1 &&
+          numVoices > 1 &&
           <span className='PlayerParams-param PlayerParams-group'>
             <label className="PlayerParams-group-title">
               Voices:
             </label>
             <div className="PlayerParams-voiceList">
-              {[...Array(this.props.numVoices)].map((_, i) => {
+              {[...Array(numVoices)].map((_, i) => {
                 return (
                   <div key={i} className='App-voice-label'><input
                     title='Alt+click to solo. Alt+click again to unmute all.'
                     type='checkbox'
                     id={'v_'+i}
                     onChange={(e) => this.handleVoiceToggle(e, i)}
-                    checked={this.props.voiceMask[i]}/>
+                    checked={voiceMask[i]}/>
                   <label htmlFor={'v_'+i}>
-                    {this.props.voiceNames[i]}
+                    {voiceNames[i]}
                   </label></div>
                 )
               })}
@@ -128,10 +140,10 @@ export default class PlayerParams extends PureComponent {
           </span>
         }
 
-        {this.props.paramDefs.map(param => {
-          const value = this.props.getParameter(param.id);
+        {paramDefs && paramValues && paramDefs.map(param => {
+          const value = paramValues[param.id];
           const dependsOn = param.dependsOn;
-          if (dependsOn && this.props.getParameter(dependsOn.param) !== dependsOn.value) {
+          if (dependsOn && paramValues[dependsOn.param] !== dependsOn.value) {
             return null;
           }
           switch (param.type) {
@@ -143,10 +155,7 @@ export default class PlayerParams extends PureComponent {
                 </label>
                   <select
                     id={param.id}
-                    onChange={(e) => {
-                      this.props.setParameter(param.id, e.target.value);
-                      this.forceUpdate();
-                    }}
+                    onChange={(e) => onParamChange(param.id, e.target.value)}
                     value={value}>
                     {param.options.map(optgroup =>
                       <optgroup key={optgroup.label} label={optgroup.label}>
@@ -168,10 +177,7 @@ export default class PlayerParams extends PureComponent {
                          type='range'
                          title={param.hint}
                          min={param.min} max={param.max} step={param.step}
-                         onChange={(e) => {
-                           this.props.setParameter(param.id, e.target.value);
-                           this.forceUpdate();
-                         }}
+                         onChange={(e) => onParamChange(param.id, parseFloat(e.target.value))}
                          value={value}>
                   </input>{' '}
                   {value !== undefined && param.step >= 1 ? value : value.toFixed(2)}
@@ -182,10 +188,7 @@ export default class PlayerParams extends PureComponent {
                 <span key={param.id} className='PlayerParams-param'>
                   <input type='checkbox'
                          id={param.id}
-                         onChange={(e) => {
-                           this.props.setParameter(param.id, e.target.checked);
-                           this.forceUpdate();
-                         }}
+                         onChange={(e) => onParamChange(param.id, e.target.checked)}
                          checked={value}/>
                   <label htmlFor={param.id} title={param.hint}>
                     {param.label}
@@ -194,9 +197,7 @@ export default class PlayerParams extends PureComponent {
               );
             case 'button':
               return (
-                <button key={param.id} title={param.hint} className="box-button" onClick={() => {
-                  this.props.setParameter(param.id, true);
-                }}>
+                <button key={param.id} title={param.hint} className="box-button" onClick={() => onParamChange(param.id, true)}>
                   {param.label}
                 </button>
               );

src/components/App.js
@@ -117,6 +117,7 @@ class App extends React.Component {
       tempo: 1,
       voiceMask: Array(MAX_VOICES).fill(true),
       voiceNames: Array(MAX_VOICES).fill(''),
+      voiceGroups: [],
       imageUrl: null,
       infoTexts: [],
       showInfo: false,
@@ -127,6 +128,7 @@ class App extends React.Component {
       directories: {},
       hasPlayer: false,
       paramDefs: [],
+      paramValues: {},
       // Special playable contexts
       localFiles: [],
     };
@@ -252,8 +254,9 @@ class App extends React.Component {
       voiceGroups: 'voiceGroups',
       songUrl: 'url',
       hasPlayer: 'hasPlayer',
-      // TODO: add param values? move to paramStateUpdate?
+      // TODO: Move to a separate paramStateUpdate?
       paramDefs: 'paramDefs',
+      paramValues: 'paramValues',
       infoTexts: 'infoTexts',
     };
     const appState = {};
@@ -511,6 +514,14 @@ class App extends React.Component {
     this.setState({
       tempo: tempo
     });
+  }
+
+  handleParamChange(id, value) {
+    if (!this.sequencer.getPlayer()) return;
+    this.sequencer.getPlayer().setParameter(id, value);
+    this.setState(prevState => ({
+      paramValues: { ...prevState.paramValues, [id]: value },
+    }));
   }
 
   setSpeedRelative(delta) {
@@ -809,18 +820,6 @@ class App extends React.Component {
                       currIdx={currIdx}
                       listing={this.state.localFiles}/>
                   )}/>>
-                  <Route path="/settings" render={() => (
-                    <Settings
-                      ejected={this.state.ejected}
-                      tempo={this.state.tempo}
-                      currentSongNumVoices={this.state.currentSongNumVoices}
-                      voiceMask={this.state.voiceMask}
-                      voiceNames={this.state.voiceNames}
-                      handleSetVoiceMask={this.handleSetVoiceMask}
-                      handleTempoChange={this.handleTempoChange}
-                      sequencer={this.sequencer}
-                      />
-                  )}/>
                   {/* Catch-all route */}
                   <Route render={({history}) => (
                     this.contentAreaRef.current &&
@@ -843,12 +842,15 @@ class App extends React.Component {
                   <Settings
                     ejected={this.state.ejected}
                     tempo={this.state.tempo}
-                    currentSongNumVoices={this.state.currentSongNumVoices}
+                    numVoices={this.state.currentSongNumVoices}
                     voiceMask={this.state.voiceMask}
                     voiceNames={this.state.voiceNames}
                     voiceGroups={this.state.voiceGroups}
-                    handleSetVoiceMask={this.handleSetVoiceMask}
-                    handleTempoChange={this.handleTempoChange}
+                    onVoiceMaskChange={this.handleSetVoiceMask}
+                    onTempoChange={this.handleTempoChange}
+                    paramDefs={this.state.paramDefs}
+                    paramValues={this.state.paramValues}
+                    onParamChange={this.handleParamChange}
                     sequencer={this.sequencer}
                   />
                 </div>

src/components/Settings.js
@@ -13,23 +13,24 @@ const themes = [
   }
 ];
 
-export default memo(Settings);
-
 function Settings(props) {
   const {
     ejected,
     tempo,
-    currentSongNumVoices,
+    numVoices,
     voiceMask,
     voiceNames,
     voiceGroups,
-    handleSetVoiceMask,
-    handleTempoChange,
+    onVoiceMaskChange,
+    onTempoChange,
+    paramDefs,
+    paramValues,
+    onParamChange,
     sequencer,
   } = props;
 
   const { settings, updateSettings } = useContext(UserContext);
-  const theme = settings?.theme; // Extract only what we need
+  const theme = settings?.theme;
 
   const handleThemeChange = useCallback((e) => {
     updateSettings({ theme: e.target.value });
@@ -42,15 +43,16 @@ function Settings(props) {
         <PlayerParams
           ejected={ejected}
           tempo={tempo}
-          numVoices={currentSongNumVoices}
+          numVoices={numVoices}
           voiceMask={voiceMask}
           voiceNames={voiceNames}
           voiceGroups={voiceGroups}
-          handleTempoChange={handleTempoChange}
-          handleSetVoiceMask={handleSetVoiceMask}
-          getParameter={sequencer.getPlayer().getParameter}
-          setParameter={sequencer.getPlayer().setParameter}
-          paramDefs={sequencer.getPlayer().getParamDefs()}/>
+          onTempoChange={onTempoChange}
+          onVoiceMaskChange={onVoiceMaskChange}
+          paramDefs={paramDefs}
+          paramValues={paramValues}
+          onParamChange={onParamChange}
+        />
         :
         <div>(No active player)</div>}
       <h3>Global Settings</h3>
@@ -70,3 +72,5 @@ function Settings(props) {
     </div>
   );
 }
+
+export default memo(Settings);

src/players/Player.js
@@ -203,6 +203,16 @@ export default class Player extends EventEmitter {
     return this.paramDefs;
   }
 
+  getParamValues() {
+    const paramValues = {};
+    if (this.getParameter) {
+      for (const def of this.paramDefs) {
+        paramValues[def.id] = this.getParameter(def.id);
+      }
+    }
+    return paramValues;
+  }
+
   getBasePlayerState() {
     return {
       metadata: this.getMetadata(),
@@ -212,6 +222,7 @@ export default class Player extends EventEmitter {
       numSubtunes: this.getNumSubtunes(),
       subtune: this.getSubtune(),
       paramDefs: this.getParamDefs(),
+      paramValues: this.getParamValues(),
       tempo: this.getTempo(),
       voiceMask: this.getVoiceMask(),
       voiceNames: this.getVoiceNames(),

commit ab834c2c4b7484c843a661fc0453863f24381f09

Fix dependent params regression from 1c9311e3

src/components/PlayerParams.js
@@ -181,7 +181,7 @@ export default class PlayerParams extends React.PureComponent {
                 </label>
                   <select
                     id={param.id}
-                    onChange={(e) => onParamChange(param.id, e.target.value)}
+                    onChange={(e) => onParamChange(param.id, parseInt(e.target.value))}
                     value={value}>
                     {param.options.map(optgroup =>
                       <optgroup key={optgroup.label} label={optgroup.label}>